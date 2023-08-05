13eb4ab734e9464aee39d0de275431df82bfe398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package tinygp/0.0.1rc1/tinygp-0.0.1rc1.tar.gz

---
+++ packages/tinygp/tinygp-0.0.1rc1.tar.gz/tinygp-0.0.1rc1/src/tinygp/kernels.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    "Sum",
+    "Product",
+    "Constant",
+    "DotProduct",
+    "Polynomial",
+    "Linear",
+    "Exp",
+    "ExpSquared",
+    "Matern32",
+    "Matern52",
+    "Cosine",
+    "RationalQuadratic",
+]
+
+from typing import Tuple, Union
+
+import jax
+import jax.numpy as jnp
+
+from .metrics import Metric, diagonal_metric
+
+Axis = Union[int, Tuple[int], jnp.ndarray]
+
+
+class Kernel:
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        raise NotImplementedError()
+
+    def evaluate_diag(self, X: jnp.ndarray) -> jnp.ndarray:
+        return jax.vmap(self)(X, X)
+
+    def evaluate(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        return jax.vmap(lambda _X1: jax.vmap(lambda _X2: self(_X1, _X2))(X2))(
+            X1
+        )
+
+    def __add__(self, other: Union["Kernel", jnp.ndarray]) -> "Kernel":
+        if isinstance(other, Kernel):
+            return Sum(self, other)
+        return Sum(self, Constant(other))
+
+    def __radd__(self, other: Union["Kernel", jnp.ndarray]) -> "Kernel":
+        if isinstance(other, Kernel):
+            return Sum(other, self)
+        return Sum(Constant(other), self)
+
+    def __mul__(self, other: Union["Kernel", jnp.ndarray]) -> "Kernel":
+        if isinstance(other, Kernel):
+            return Product(self, other)
+        return Product(self, Constant(other))
+
+    def __rmul__(self, other: Union["Kernel", jnp.ndarray]) -> "Kernel":
+        if isinstance(other, Kernel):
+            return Product(other, self)
+        return Product(Constant(other), self)
+
+
+class Sum(Kernel):
+    def __init__(self, kernel1: Kernel, kernel2: Kernel):
+        self.kernel1 = kernel1
+        self.kernel2 = kernel2
+
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        return self.kernel1(X1, X2) + self.kernel2(X1, X2)
+
+
+class Product(Kernel):
+    def __init__(self, kernel1: Kernel, kernel2: Kernel):
+        self.kernel1 = kernel1
+        self.kernel2 = kernel2
+
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        return self.kernel1(X1, X2) * self.kernel2(X1, X2)
+
+
+class Constant(Kernel):
+    def __init__(self, value: jnp.ndarray):
+        self.value = jnp.asarray(value)
+
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        return self.value
+
+
+class SubspaceKernel(Kernel):
+    def __init__(self, *, axis: Axis = None):
+        self.axis = axis
+
+    def evaluate_subspace(
+        self, X1: jnp.ndarray, X2: jnp.ndarray
+    ) -> jnp.ndarray:
+        raise NotImplementedError()
+
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        if self.axis is None:
+            return self.evaluate_subspace(X1, X2)
+        return self.evaluate_subspace(X1[self.axis], X2[self.axis])
+
+
+class DotProduct(SubspaceKernel):
+    def evaluate_subspace(
+        self, X1: jnp.ndarray, X2: jnp.ndarray
+    ) -> jnp.ndarray:
+        return X1 @ X2
+
+
+class Polynomial(SubspaceKernel):
+    def __init__(self, *, order: int, sigma: jnp.ndarray, axis: Axis = None):
+        self.order = float(order)
+        self.sigma2 = jnp.asarray(sigma) ** 2
+        super().__init__(axis=axis)
+
+    def evaluate_subspace(
+        self, X1: jnp.ndarray, X2: jnp.ndarray
+    ) -> jnp.ndarray:
+        return (X1 @ X2 + self.sigma2) ** self.order
+
+
+class Linear(SubspaceKernel):
+    def __init__(self, *, order: int, sigma: jnp.ndarray, axis: Axis = None):
+        self.order = float(order)
+        self.sigma2 = jnp.asarray(sigma) ** 2
+        super().__init__(axis=axis)
+
+    def evaluate_subspace(
+        self, X1: jnp.ndarray, X2: jnp.ndarray
+    ) -> jnp.ndarray:
+        return (X1 @ X2 / self.sigma2) ** self.order
+
+
+class MetricKernel(Kernel):
+    def __init__(self, metric: Union[Metric, jnp.ndarray]):
+        if callable(metric):
+            self.metric = metric
+        else:
+            self.metric = diagonal_metric(metric)
+
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        raise NotImplementedError()
+
+    def __call__(self, X1: jnp.ndarray, X2: jnp.ndarray) -> jnp.ndarray:
+        r2 = self.metric(X1 - X2)
+        return self.evaluate_radial(jnp.where(jnp.isclose(r2, 0.0), 0.0, r2))
+
+
+class Exp(MetricKernel):
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        return jnp.exp(-jnp.sqrt(r2))
+
+
+class ExpSquared(MetricKernel):
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        return jnp.exp(-0.5 * r2)
+
+
+class Matern32(MetricKernel):
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        arg = jnp.sqrt(3.0 * r2)
+        return (1.0 + arg) * jnp.exp(-arg)
+
+
+class Matern52(MetricKernel):
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        arg1 = 5.0 * r2
+        arg2 = jnp.sqrt(arg1)
+        return (1.0 + arg2 + arg1 / 3.0) * jnp.exp(-arg2)
+
+
+class Cosine(MetricKernel):
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        return jnp.cos(2 * jnp.pi * jnp.sqrt(r2))
+
+
+class ExpSineSquared(MetricKernel):
+    def __init__(self, metric: Metric, *, gamma: jnp.ndarray):
+        self.gamma = jnp.asarray(gamma)
+        super().__init__(metric)
+
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        return jnp.exp(
+            -self.gamma * jnp.square(jnp.sin(jnp.pi * jnp.sqrt(r2)))
+        )
+
+
+class RationalQuadratic(MetricKernel):
+    def __init__(self, metric: Metric, *, alpha: jnp.ndarray):
+        self.alpha = jnp.asarray(alpha)
+        super().__init__(metric)
+
+    def evaluate_radial(self, r2: jnp.ndarray) -> jnp.ndarray:
+        return (1.0 + 0.5 * r2 / self.alpha) ** -self.alpha
---
+++ packages/tinygp/tinygp-0.0.1rc1.tar.gz/tinygp-0.0.1rc1/tests/test_george_compat.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pytest
+from jax.config import config
+
+from tinygp import GaussianProcess, kernels, metrics
+
+george = pytest.importorskip("george")
+
+config.update("jax_enable_x64", True)
+
+
+@pytest.fixture
+def random():
+    return np.random.default_rng(1058390)
+
+
+@pytest.fixture(
+    scope="module",
+    params=["Constant", "DotProduct", "Polynomial", "Linear"],
+)
+def kernel(request):
+    return {
+        "Constant": (
+            kernels.Constant(value=1.5),
+            george.kernels.ConstantKernel(
+                log_constant=np.log(1.5 / 5), ndim=5
+            ),
+        ),
+        "DotProduct": (
+            kernels.DotProduct(),
+            george.kernels.DotProductKernel(ndim=3),
+        ),
+        "Polynomial": (
+            kernels.Polynomial(order=2.5, sigma=1.3),
+            george.kernels.PolynomialKernel(
+                order=2.5, log_sigma2=2 * np.log(1.3), ndim=1
+            ),
+        ),
+        "Linear": (
+            kernels.Linear(order=2.5, sigma=1.3),
+            george.kernels.LinearKernel(
+                order=2.5, log_gamma2=2.5 * 2 * np.log(1.3), ndim=1
+            ),
+        ),
+    }[request.param]
+
+
+@pytest.fixture(scope="module", params=["Cosine", "ExpSineSquared"])
+def periodic_kernel(request):
+    return {
+        "Cosine": (
+            kernels.Cosine(2.3),
+            george.kernels.CosineKernel(log_period=np.log(2.3)),
+        ),
+        "ExpSineSquared": (
+            kernels.ExpSineSquared(2.3, gamma=1.3),
+            george.kernels.ExpSine2Kernel(gamma=1.3, log_period=np.log(2.3)),
+        ),
+    }[request.param]
+
+
+@pytest.fixture(
+    scope="module",
+    params=["unit", "diagonal1", "diagonal2", "cholesky", "dense"],
+)
+def metric(request):
+    random = np.random.default_rng(5968)
+    L = np.tril(random.standard_normal((5, 5)))
+    L[np.diag_indices_from(L)] = np.exp(
+        L[np.diag_indices_from(L)]
+    ) + random.uniform(1.5, 2.5, len(L))
+    return {
+        "unit": (metrics.unit_metric, dict(metric=1.0, ndim=2)),
+        "diagonal1": (
+            metrics.diagonal_metric(0.5),
+            dict(metric=0.5 ** 2, ndim=3),
+        ),
+        "diagonal2": (
+            metrics.diagonal_metric(np.array([0.5, 0.3])),
+            dict(metric=[0.5 ** 2, 0.3 ** 2], ndim=2),
+        ),
+        "cholesky": (
+            metrics.cholesky_metric(L, lower=True),
+            dict(metric=L @ L.T, ndim=len(L)),
+        ),
+        "dense": (
+            metrics.dense_metric(L @ L.T),
+            dict(metric=L @ L.T, ndim=len(L)),
+        ),
+    }[request.param]
+
+
+@pytest.fixture(
+    scope="module",
+    params=["Exp", "ExpSquared", "Matern32", "Matern52", "RationalQuadratic"],
+)
+def metric_kernel(request, metric):
+    tiny_metric, george_metric_args = metric
+    return {
+        "Exp": (
+            kernels.Exp(tiny_metric),
+            george.kernels.ExpKernel(**george_metric_args),
+        ),
+        "ExpSquared": (
+            kernels.ExpSquared(tiny_metric),
+            george.kernels.ExpSquaredKernel(**george_metric_args),
+        ),
+        "Matern32": (
+            kernels.Matern32(tiny_metric),
+            george.kernels.Matern32Kernel(**george_metric_args),
+        ),
+        "Matern52": (
+            kernels.Matern52(tiny_metric),
+            george.kernels.Matern52Kernel(**george_metric_args),
+        ),
+        "RationalQuadratic": (
+            kernels.RationalQuadratic(tiny_metric, alpha=1.5),
+            george.kernels.RationalQuadraticKernel(
+                log_alpha=np.log(1.5), **george_metric_args
+            ),
+        ),
+    }[request.param]
+
+
+def compare_kernel_value(random, tiny_kernel, george_kernel):
+    x1 = np.sort(random.uniform(0, 10, (50, george_kernel.ndim)))
+    x2 = np.sort(random.uniform(0, 10, (45, george_kernel.ndim)))
+    np.testing.assert_allclose(
+        tiny_kernel.evaluate(x1, x2),
+        george_kernel.get_value(x1, x2),
+    )
+    np.testing.assert_allclose(
+        tiny_kernel.evaluate(x1, x1),
+        george_kernel.get_value(x1),
+    )
+    np.testing.assert_allclose(
+        tiny_kernel.evaluate_diag(x1),
+        george_kernel.get_value(x1, diag=True),
+    )
+
+
+def compare_gps(random, tiny_kernel, george_kernel):
+    x = np.sort(random.uniform(0, 10, (50, george_kernel.ndim)))
+    t = np.sort(random.uniform(0, 10, (12, george_kernel.ndim)))
+    y = np.sin(x[:, 0])
+    diag = random.uniform(0.01, 0.1, 50)
+
+    # Set up the GPs
+    george_gp = george.GP(george_kernel)
+    george_gp.compute(x, np.sqrt(diag))
+    tiny_gp = GaussianProcess(tiny_kernel, x, diag=diag)
+
+    # Likelihood
+    np.testing.assert_allclose(
+        tiny_gp.condition(y), george_gp.log_likelihood(y)
+    )
+
+    # Filtering
+    np.testing.assert_allclose(
+        tiny_gp.predict(),
+        george_gp.predict(y, x, return_var=False, return_cov=False),
+    )
+
+    # Filtering with explicit value
+    np.testing.assert_allclose(
+        tiny_gp.predict(x),
+        george_gp.predict(y, x, return_var=False, return_cov=False),
+    )
+    np.testing.assert_allclose(
+        tiny_gp.predict(t),
+        george_gp.predict(y, t, return_var=False, return_cov=False),
+    )
+
+    # Variance
+    np.testing.assert_allclose(
+        tiny_gp.predict(return_var=True)[1],
+        george_gp.predict(y, x, return_var=True, return_cov=False)[1],
+        rtol=1e-5,
+    )
+    np.testing.assert_allclose(
+        tiny_gp.predict(t, return_var=True)[1],
+        george_gp.predict(y, t, return_var=True, return_cov=False)[1],
+        rtol=1e-5,
+    )
+
+    # Covariance
+    np.testing.assert_allclose(
+        tiny_gp.predict(return_cov=True)[1],
+        george_gp.predict(y, x, return_var=False, return_cov=True)[1],
+        rtol=1e-5,
+    )
+    np.testing.assert_allclose(
+        tiny_gp.predict(t, return_cov=True)[1],
+        george_gp.predict(y, t, return_var=False, return_cov=True)[1],
+        rtol=1e-5,
+    )
+
+
+def test_kernel_value(random, kernel):
+    tiny_kernel, george_kernel = kernel
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+    tiny_kernel *= 0.3
+    george_kernel *= 0.3
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+
+
+def test_periodic_kernel_value(random, periodic_kernel):
+    tiny_kernel, george_kernel = periodic_kernel
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+    tiny_kernel *= 0.3
+    george_kernel *= 0.3
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+
+
+def test_metric(metric):
+    tiny_metric, george_metric_args = metric
+    george_metric = george.metrics.Metric(**george_metric_args)
+    for n in range(george_metric.ndim):
+        e = np.zeros(george_metric.ndim)
+        e[n] = 1.0
+        np.testing.assert_allclose(
+            tiny_metric(e), e.T @ np.linalg.solve(george_metric.to_matrix(), e)
+        )
+
+
+def test_metric_kernel_value(random, metric_kernel):
+    tiny_kernel, george_kernel = metric_kernel
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+    tiny_kernel *= 0.3
+    george_kernel *= 0.3
+    compare_kernel_value(random, tiny_kernel, george_kernel)
+
+
+def test_gp(random, kernel):
+    tiny_kernel, george_kernel = kernel
+    compare_gps(random, tiny_kernel, george_kernel)
+
+
+def test_periodic_gp(random, periodic_kernel):
+    tiny_kernel, george_kernel = periodic_kernel
+    compare_gps(random, tiny_kernel, george_kernel)
+
+
+def test_metric_gp(random, metric_kernel):
+    tiny_kernel, george_kernel = metric_kernel
+    compare_gps(random, tiny_kernel, george_kernel)
