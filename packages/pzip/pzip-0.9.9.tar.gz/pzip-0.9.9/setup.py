e54118b976e8f28b7e46d818e7c37ffcef7a2229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:06 +0000

    Add package pzip/0.9.9/pzip-0.9.9.tar.gz

---
+++ packages/pzip/pzip-0.9.9.tar.gz/pzip-0.9.9/setup.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open(os.path.join("pzip", "__init__.py"), "r") as src:
+    version = re.match(r'.*__version__ = "(.*?)"', src.read(), re.S).group(1)
+
+setup(
+    name="pzip",
+    version=version,
+    description="Crytographically secure file compression.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dan Watson",
+    author_email="watsond@imsweb.com",
+    url="https://github.com/imsweb/pzip",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["cryptography", "tqdm"],
+    extras_require={"deflate": ["deflate"]},
+    entry_points={"console_scripts": ["pzip=pzip.cli:main"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: System :: Archiving :: Compression",
+    ],
+)
