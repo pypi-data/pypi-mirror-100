834034d7d29bb7c0958cdc74341098afac23a43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:29 +0000

    Add package sa_up_test/1.0.1/sa_up_test-1.0.1.tar.gz

---
+++ packages/sa_up_test/sa_up_test-1.0.1.tar.gz/sa_up_test-1.0.1/sa_up_test/__init__.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+#@Author  :ku sa sa
+#@Time    :2021/3/26
+
+
---
+++ packages/sa_up_test/sa_up_test-1.0.1.tar.gz/sa_up_test-1.0.1/sa_up_test/hello_sa.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+#@Author  :ku sa sa
+#@Time    :2021/2/4
+
+
+class Sa(object):
+	
+	def hello_sa(self):
+		print('你好，我的名字是sa!!')
