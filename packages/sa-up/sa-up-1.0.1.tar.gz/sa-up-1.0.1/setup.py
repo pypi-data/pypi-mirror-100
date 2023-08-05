dad7a7e9beb25d6c8a4b02992effa9233b5ef628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package sa-up/1.0.1/sa-up-1.0.1.tar.gz

---
+++ packages/sa-up/sa-up-1.0.1.tar.gz/sa-up-1.0.1/setup.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+#@Author  :ku sa sa
+#@Time    :2021/2/4
+
+
+from setuptools import setup
+import setuptools
+
+setup(
+	name = 'sa-up',#包名
+	version = '1.0.1',
+	description = 'Tools used by hy~',
+	url = 'https://pypi.org/project/sa-up_test/',#包的主页
+	packages = setuptools.find_packages(),
+	platforms = 'any'
+	
+	
+	
+)
