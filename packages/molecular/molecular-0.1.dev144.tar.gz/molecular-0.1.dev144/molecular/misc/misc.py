3196617098c81f61623aa0202b7f5b6b3cbda0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:56 +0000

    Add package molecular/0.1.dev144/molecular-0.1.dev144.tar.gz

---
+++ packages/molecular/molecular-0.1.dev144.tar.gz/molecular-0.1.dev144/molecular/misc/misc.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+from typelike import ArrayLike
+
+
+# Assert that an array is incremental
+def assert_incremental(a, increment=1):
+    """
+    Assert that something like an array has entries that increment by a specific value.
+
+    Parameters
+    ----------
+    a : ArrayLike
+        Array to check if entries increment by specific value.
+    increment : int
+        Increment value.
+
+    Raises
+    ------
+    AssertionError
+        If `a` does not have entries that increment by `increment`
+    """
+
+    assert is_incremental(a, increment=increment)
+
+
+# Test if an array is incremental
+def is_incremental(a, increment=1):
+    return (np.diff(a) == increment).all()
+
+
+# Convenience zfill function
+def zfill(a, width=None):
+    if width is None:
+        return a
+    elif hasattr(a, '__getitem__'):
+        return np.char.zfill(list(map(str, a)), width)
+    else:
+        return str(a).zfill(width)
