1734de7d016eb0cb301182db5debc9e83a4a12d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:18 +0000

    Add package uquake/0.4.10/uquake-0.4.10-py3-none-any.whl

---
+++ packages/uquake/uquake-0.4.10-py3-none-any.whl/uquake/project/settings.py
@@ -0,0 +1,49 @@
+import os
+from dynaconf import LazySettings
+
+
+class Settings(LazySettings):
+    def __init__(self, settings_location):
+        """
+        Init function currently just initializes the object allowing
+        """
+        if "SPP_CONFIG" in os.environ:
+            # keep this as legacy behavior
+            config_dir = os.environ['UQUAKE_CONFIG']
+        else:
+            config_dir = os.getcwd()
+
+        dconf = {}
+        dconf.setdefault('ENVVAR_PREFIX_FOR_DYNACONF', 'UQUAKE')
+
+        env_prefix = '{0}_ENV'.format(
+            dconf['ENVVAR_PREFIX_FOR_DYNACONF']
+        )  # SPP_ENV
+
+        dconf.setdefault(
+            'ENV_FOR_DYNACONF',
+            os.environ.get(env_prefix, 'DEV').upper()
+        )
+
+        # This was an incredibly odd fix, the base settings_template.toml needs to be on
+        # top of the list otherwise you will not be able to modify the settings
+        # downstream
+        # default_paths = (
+        #     str(settings_location) +
+        #     "settings.py,.secrets.py,"
+        #     "settings_template.toml,settings.tml,.secrets.toml,.secrets.tml,"
+        #     "settings.yaml,settings.yml,.secrets.yaml,.secrets.yml,"
+        #     "settings.ini,settings.conf,settings.properties,"
+        #     "connectors.toml,connectors.tml,.connectors.toml,.connectors.tml,"
+        #     "connectors.json,"
+        #     ".secrets.ini,.secrets.conf,.secrets.properties,"
+        #     "settings.json,.secrets.json"
+        # )
+
+        default_paths = str(settings_location)
+
+        dconf['SETTINGS_FILE_FOR_DYNACONF'] = default_paths
+        dconf['ROOT_PATH_FOR_DYNACONF'] = config_dir
+
+        super().__init__(**dconf)
+
