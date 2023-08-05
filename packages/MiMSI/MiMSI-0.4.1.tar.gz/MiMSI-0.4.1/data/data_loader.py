4ec96d88473fe26315b6af5ef3eff02b486c8b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:22 +0000

    Add package MiMSI/0.4.1/MiMSI-0.4.1.tar.gz

---
+++ packages/MiMSI/MiMSI-0.4.1.tar.gz/MiMSI-0.4.1/data/data_loader.py
@@ -0,0 +1,170 @@
+"""
+MiMSI Data Loader
+
+Torch Data Loader for MiMSI
+
+@author: John Ziegler
+Memorial Sloan Kettering Cancer Center 
+Nov. 2018
+
+zieglerj@mskcc.org
+
+(c) 2018 Memorial Sloan Kettering Cancer Center.  This program is free software: you may use, redistribute, 
+and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 or later. See the LICENSE file for details
+
+"""
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+from sklearn.preprocessing import normalize
+from torchvision import transforms
+
+
+class MSIBags(Dataset):
+    def __init__(
+        self,
+        root_dir,
+        coverage=100,
+        include_locations=True,
+        labeled=True,
+        num_repeats=10,
+    ):
+        self.root_dir = root_dir
+        self.file_list = os.listdir(root_dir)
+        self.is_labeled = labeled
+        self.coverage = coverage
+        self.include_locations = include_locations
+        self.num_repeats = num_repeats
+        self.data_file_list = [
+            x for x in self.file_list if x.endswith("_data.npy")
+        ]  # parse out only samples
+
+        self.loc_file_list = []
+        if self.include_locations:
+            self.loc_file_list = [
+                x for x in self.file_list if x.endswith("_locations.npy")
+            ]  # parse out location files
+
+    def __len__(self):
+        return len(self.data_file_list)
+
+    def __parsefilename__(self, filename):
+        """format of the vector filename is:
+            {sample_id}_{label}_{type_of_file}.npy
+            where the label is either -1 or 1 and the type of file is either "data" or "locations"
+
+            This helper method parses out the sample_id and label, and takes into account 
+            corner cases like the client using "_" in their sample name.
+        """
+
+        split_filename = filename.split("_")
+        label = split_filename[-2]  # -1 index is data/locations, -2 is label
+        sample_portion = split_filename[:-2]
+        if len(sample_portion) > 1:
+            # underscore must have been used since total length is over 3
+            # need to join the sample_id back together
+            sample_id = "_".join(sample_portion)
+        else:
+            # underscore wasn't used, just grab the first (and only) element
+            sample_id = sample_portion[0]
+
+        return sample_id, label
+
+    def _preprocess(self, np_from_disk):
+        bag_repeats = []
+        for i in range(self.num_repeats):
+            bag = []
+            for tumor_normal_microsatellite in np_from_disk:
+                tumor = tumor_normal_microsatellite[0]
+                normal = tumor_normal_microsatellite[1]
+
+                # downsample to the required coverage
+                downsampled_t = tumor[
+                    np.random.choice(tumor.shape[0], self.coverage), :, :
+                ]
+                downsampled_n = normal[
+                    np.random.choice(normal.shape[0], self.coverage), :, :
+                ]
+
+                bag.append(np.concatenate((downsampled_t, downsampled_n), axis=0))
+
+            bag_repeats.append(
+                bag
+            )  # List (length N) of lists, each list is of len 10 (10 copies of each vector)
+
+        return bag_repeats  # Return 10 X (N x (200 x L x 3)0
+
+    def __getitem__(self, idx):
+        data_file = os.path.join(self.root_dir, self.data_file_list[idx])
+        loc_file = os.path.join(self.root_dir, self.data_file_list[idx]).replace(
+            "data", "locations"
+        )
+
+        sample_id, label = self.__parsefilename__(self.data_file_list[idx])
+        final_bags = []
+        bags = self._preprocess(np.load(data_file))
+        for bag in bags:
+
+            bag = [
+                np.concatenate(
+                    (entry, np.zeros((self.coverage * 2, 40 - entry.shape[1], 3))),
+                    axis=1,
+                )
+                for entry in bag
+            ]
+            bag = np.array(bag)
+
+            bag_mean = bag.mean(axis=(0, 1, 2), keepdims=True)
+            bag_std = bag.std(axis=(0, 1, 2), keepdims=True)
+
+            # Normalize bag
+            bag = (bag - bag_mean) / (bag_std)
+
+            # Setup bag into Torch Tensor-Style Dimensions
+            bag = np.swapaxes(bag, 2, 3)
+            bag = np.swapaxes(bag, 1, 2)
+            bag = torch.from_numpy(bag).float()
+            final_bags.append(bag)
+
+        if self.include_locations:
+            locations = np.load(loc_file)
+            return final_bags, label, locations, sample_id
+
+        return final_bags, label, 0, sample_id
+
+
+if __name__ == "__main__":
+    """
+    This is a simple test you can run to verify the training
+    data is successfully loaded from the filesystem using the
+    DataLoader above. Prints out a success message as well as 
+    the dataset mean and standard deviation upon success
+    """
+    train_loader = DataLoader(MSIBags("./data", True, True), batch_size=1, shuffle=True)
+
+    bag_means = []
+    bag_stds = []
+    for batch_idx, (data, label, locations, sample) in enumerate(train_loader):
+        data = data.squeeze(0)
+
+        bag_mean = data.numpy().mean(axis=(0, 2, 3))
+        bag_std = data.numpy().std(axis=(0, 2, 3))
+
+        bag_means.append(bag_mean)
+        bag_stds.append(bag_std)
+
+    means = np.array(bag_means)
+
+    stds = np.array(bag_stds)
+
+    final_mean = means.mean(axis=0)
+    final_std = stds.mean(axis=0)
+
+    print("Data from training loaded successfully. \n")
+    print("Final Mean: \n")
+    print(final_mean)
+    print("Final Standard Deviation: \n")
+    print(final_std)
---
+++ packages/MiMSI/MiMSI-0.4.1.tar.gz/MiMSI-0.4.1/main/mi_msi_train_test.py
@@ -0,0 +1,275 @@
+'''
+MiMSI Train/Test
+
+Used to train and validate a MiMSI model from scratch
+
+@author: John Ziegler
+Memorial Sloan Kettering Cancer Center 
+May 2020
+
+zieglerj@mskcc.org
+
+(c) 2018 Memorial Sloan Kettering Cancer Center.  This program is free software: you may use, redistribute, 
+and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 or later. See the LICENSE file for details
+
+'''
+
+
+import numpy as np
+import os
+import sys
+import argparse
+import torch
+import torch.utils.data as data_utils
+import torch.optim as optim
+from torch.autograd import Variable
+from sklearn import metrics
+from copy import deepcopy
+from .data.data_loader import MSIBags
+from .model.mi_msi_model import MSIModel
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='MiMSI - A Multiple Instance Learning Model for detecting microsatellite instability in NGS data')
+    parser.add_argument('--epochs', type=int, default=40, metavar='N',
+                        help='Number of epochs to train (default: 40)')
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='Learning rate used in training (default: 0.0001)')
+    parser.add_argument('--reg', type=float, default=5e-4, metavar='R',
+                        help='Weight decay used in training (default: 5e-4)')
+    parser.add_argument('--seed', type=int, default=2,
+                        metavar='S', help='Random Seed (default: 2)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='Disables CUDA training for use off GPU, if this is not specified the utility will check availability of torch.cuda')
+    parser.add_argument('--name', default="mi_msi_1",
+                        help='Name of the model, ')
+    parser.add_argument('--train-location', default="./main",
+                        help='Directory Location for Training Data')
+    parser.add_argument('--test-location', default="./main",
+                        help='Directory Location for Testing Data')
+    parser.add_argument('--save', default=False,
+                        help='Save the model weights to disk after training')
+
+
+    args = parser.parse_args()
+    # Training settings
+    epochs, lr, reg, seed, name, train_location, test_location, save = args.epochs, args.lr, args.reg, args.seed, args.name, args.train_location, args.test_location, args.save
+    cuda = not args.no_cuda and torch.cuda.is_available()
+    generate_model(seed, cuda, epochs, lr, reg, train_location, test_location, save, name)
+
+
+def train(epoch, model, optimizer, train_loader, cuda):
+    model.train()
+    train_loss = 0.
+    train_error = 0.
+    for batch_idx, (data, label, locations, sample_id) in enumerate(train_loader):
+        bag_label = torch.tensor(int(label[0]))
+
+        # reset gradients
+        optimizer.zero_grad()
+
+        # we're not doing the 10x repeats by default, but this could 
+        # be modified to handle that by looping this section for
+        # every repeat
+        data = data[0]
+        if cuda:
+            data, bag_label = data.cuda(), bag_label.cuda()
+        data, bag_label = Variable(data), Variable(bag_label)
+
+        # calculate loss and error
+        loss, Y_prob, Y_hat = model.calculate_objective(data, bag_label)
+        train_loss += loss.item()
+        
+        error = model.calculate_classification_error(Y_hat, bag_label)
+        train_error += error
+
+        # back prop
+        loss.backward()
+
+        # step
+        optimizer.step()
+        data = None
+        label = None
+        locations = None
+        sample_id = None
+        bag_label = None
+        torch.cuda.empty_cache()
+
+    # calculate loss and error for epoch
+    train_loss /= len(train_loader)
+    train_error /= len(train_loader)
+
+    print('Epoch: {}, Loss: {:.4f}, Train error: {:.4f}'.format(epoch, train_loss, train_error))
+    return train_loss, train_error
+
+
+def test(test_loader, model, cuda, save, name):
+    model.eval()
+    test_loss = 0.
+    test_error = 0.
+    
+    tp = 0.
+    fp = 0.
+    tn = 0.
+    fn = 0.
+    
+    labels = []
+    preds = []
+
+    incorrect = []
+
+    if len(test_loader) == 0:
+        print('No testing data supplied! Please indicate a directory containing generated NGS vectors in .npy format.')
+        return
+        
+    with torch.no_grad():
+        for batch_idx, (data, label, locations, sample_id) in enumerate(test_loader):
+            bag_label = torch.tensor(int(label[0]))
+            
+            data = data[0] # again, because we're only doing one run
+
+            if cuda:
+                data, bag_label = data.cuda(), bag_label.cuda()
+            data, bag_label = Variable(data), Variable(bag_label)
+            
+            # Do our prediction
+            loss, Y_prob, Y_hat = model.calculate_objective(data, bag_label)
+            test_loss += loss.item()
+        
+            # save prediction and labels for analysis, if required
+            preds.append(Y_prob.item())
+            labels.append(bag_label.item())
+ 
+            # calculate error
+            error = model.calculate_classification_error(Y_hat, bag_label)
+            
+            if error == 0. and Y_hat == 1.:
+                tp += 1
+            elif error == 0. and Y_hat == 0.:
+                tn += 1
+            elif error == 1. and Y_hat == 1.:
+                fp += 1
+                # save the incorrect cases for further analysis
+                incorrect.append((sample_id, Y_prob.item()))
+            else:
+                fn += 1
+                # save the incorrect cases for further analysis
+                incorrect.append((sample_id, Y_prob.item()))
+
+            test_error += error
+
+
+    test_error /= len(test_loader)
+    test_loss /= len(test_loader)
+
+    fpr, tpr, thresholds = metrics.roc_curve(labels, preds)
+    auroc = metrics.auc(fpr, tpr)
+    if save:
+        np.save('./' + name + '_fpr.npy', fpr)
+        np.save('./' + name + '_tpr.npy', tpr)
+        np.save('./' + name + '_thres.npy', thresholds)
+    
+    
+    print('Incorrect cases (based on provided labels): \n')
+    print(incorrect)
+
+    print('Test Set, Loss: {:.4f}, Test error: {:.4f}\n'.format(test_loss, test_error))
+    print('TP, FP, TN, FN: {:.4f},{:.4f},{:.4f},{:.4f}\n'.format(tp, fp, tn, fn))
+    print('AUROC: {:.4f}\n'.format(auroc))
+    return test_loss, test_error
+
+def generate_model(seed, cuda, epochs, lr, reg, train_location, test_location, save, name):
+    print('Lets Go!!!\n')
+    train_loss_list = []
+    train_error_list = []
+
+    # Checkpoint for our model so that we can save the best
+    # performing model during training
+    best_checkpoint = {
+        'epoch': 0,
+        'loss': 99999,
+        'error': 999999,
+        'state_dict': {},
+        'optimizer': {}
+    }
+
+    torch.manual_seed(seed)
+    loader_kwargs = {}
+
+    if cuda:
+        torch.cuda.manual_seed(seed)
+        loader_kwargs = {'num_workers': 1, 'pin_memory': True}
+        print('\nGPU is enabled!')
+
+    print('Loading Training and Test Set\n')
+
+    # We set the number of repeats param in the loader to 1 for training and testing
+    # by default. To enable repeatitions/augmentation (for downsampling) you can change
+    # 1 back to the default of 10 as in evalute_sample.py, just keep in mind the
+    # two train/test methods below will need to be looped
+    train_loader = data_utils.DataLoader(MSIBags(train_location, 100, False, True, 1),
+                                         batch_size=1,
+                                         shuffle=True,
+                                         **loader_kwargs)
+
+    test_loader = data_utils.DataLoader(MSIBags(test_location, 100, False, True, 1),
+                                        batch_size=1,
+                                        shuffle=False,
+                                        **loader_kwargs)
+
+    if len(train_loader) == 0:
+        print('No training data supplied! Please indicate a directory containing generated NGS vectors in .npy format.')
+        sys.exit()
+
+    # initialize model class
+    model = MSIModel()
+
+    if cuda:
+        model.cuda()
+
+    optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=reg)
+
+    print('Training the Model... \n')
+    for epoch in range(1, epochs + 1):
+        train_loss, train_error = train(
+            epoch, model, optimizer, train_loader, cuda)
+        train_loss_list.append(train_loss)
+        train_error_list.append(train_error)
+
+        test_loss, test_error = test(test_loader, model, cuda, False, name)
+
+        # If this set of weights beats our best test loss thus far, set the checkpoint
+        if  test_loss < best_checkpoint['loss']:
+            best_checkpoint = {
+                'epoch': epoch,
+                'loss': test_loss,
+                'error': test_error,
+                'state_dict': deepcopy(model.state_dict()),
+                'optimizer': deepcopy(optimizer.state_dict()),
+            }
+
+        test_loss = None
+        test_error = None
+
+    if save:
+        np.save('./' + name + '_trainerror.npy', train_error_list)
+        np.save('./' + name + '_trainloss.npy', train_loss_list)
+        train_loss_list = None
+        train_error_list = None
+
+    print('Training Complete... \n')
+    
+    # Load and test model
+    print('Testing the Model... \n')
+    model.load_state_dict(best_checkpoint['state_dict'])
+    final_loss, final_err = test(test_loader, model, cuda, save, name)
+
+    if save:
+        model.cpu()
+        torch.save(model.state_dict(), './' + name + '.model')
+
+
+if __name__ == "__main__":
+    main()
