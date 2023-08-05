756f5b2d7087e2e1f60ef0f0cb6e9a2b0ef2de00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:38 +0000

    Add package ddddkkkk/1.0/ddddkkkk-1.0.tar.gz

---
+++ packages/ddddkkkk/ddddkkkk-1.0.tar.gz/ddddkkkk-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# coding:utf-8
+
+from distutils.core import  setup
+
+setup(
+    name="ddddkkkk",
+    version='1.0',
+    description="这是第一个程序",
+    author='fj',
+    author_email= '1172376339@qq.com',
+    py_modules=['ddddkkkk.newsa','ddddkkkk.salary']
+)
