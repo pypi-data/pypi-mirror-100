3764820751934213f200f01a5c027dcd71d79eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:52 +0000

    Add package npc-shopify-cli/0.0.7/npc-shopify-cli-0.0.7.tar.gz

---
+++ packages/npc-shopify-cli/npc-shopify-cli-0.0.7.tar.gz/npc-shopify-cli-0.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="npc-shopify-cli",  # Replace with your own username
+    version="0.0.7",
+    author="Newspaper Club Ltd",
+    author_email="services@newspaperclub.com",
+    description="A cli for working with Shopify",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/newspaperclub/npc-shop-cli",
+    project_urls={
+        "Bug Tracker": "https://github.com/newspaperclub/npc-shop-cli/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    executables=[
+        "npc-theme"
+    ]
+)
