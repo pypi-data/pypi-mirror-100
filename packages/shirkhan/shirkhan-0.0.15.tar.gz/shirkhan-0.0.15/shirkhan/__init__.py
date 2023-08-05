86f4812c508a69bf4f1f8e199dc2c6afe5e8cd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:23 +0000

    Add package shirkhan/0.0.15/shirkhan-0.0.15.tar.gz

---
+++ packages/shirkhan/shirkhan-0.0.15.tar.gz/shirkhan-0.0.15/shirkhan/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.15"
+__author__ = "shirkhan"
+
+from shirkhan.shir_syllable import *
+
+# utils
+from shirkhan.shir_encode_decode import *
+from shirkhan.shir_file import *
+from shirkhan.shir_string import *
+from shirkhan.shir_encryption import *
+from shirkhan import shir_csv as csv
+from shirkhan import shir_excel as excel
+from shirkhan import shir_wav as wave
+from shirkhan import shir_mp3 as mp3
+
+if __name__ == '__main__':
+    pass
