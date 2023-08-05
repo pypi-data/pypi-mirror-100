022d65c3a40459284e2e8f4ba48a711cc7c34c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package pyimporters-csv/0.0.4/pyimporters-csv-0.0.4.tar.gz

---
+++ packages/pyimporters-csv/pyimporters-csv-0.0.4.tar.gz/pyimporters-csv-0.0.4/test/test_excel.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from progress.bar import Bar
+
+from pyimporters_plugins.base import Term
+from pyimporters_csv.csv import CSVKnowledgeParser, CSVOptions
+from pyimporters_csv.excel import ExcelKnowledgeParser, ExcelOptions
+
+
+def test_xls():
+    source = Path('data/Digestion.xls')
+    parser = ExcelKnowledgeParser()
+    options = ExcelOptions(encoding="utf-8", identifier_col="ID", prefLabel_col="prefLabel_en", altLabel_cols="altLabel_en", multivalue_separator="|")
+    concepts = list(parser.parse(source, options.dict(), Bar('Processing')))
+    assert len(concepts) == 92
+    c7 : Term = concepts[7]
+    assert c7.identifier == 'https://opendata.inra.fr/EMTD/8'
+    assert c7.prefLabel == 'specific pathogen-free animal'
+    assert len(c7.altLabel) == 2
+    assert set(c7.altLabel) == set(['SPF animal', 'specific pathogen free animal'])
+
+def test_xlsx():
+    source = Path('data/Digestion.xlsx')
+    parser = ExcelKnowledgeParser()
+    options = ExcelOptions(encoding="utf-8", identifier_col="ID", prefLabel_col="prefLabel_en", altLabel_cols="altLabel_en", multivalue_separator="|")
+    concepts = list(parser.parse(source, options.dict(), Bar('Processing')))
+    assert len(concepts) == 92
+    c7 : Term = concepts[7]
+    assert c7.identifier == 'https://opendata.inra.fr/EMTD/8'
+    assert c7.prefLabel == 'specific pathogen-free animal'
+    assert len(c7.altLabel) == 2
+    assert set(c7.altLabel) == set(['SPF animal', 'specific pathogen free animal'])
