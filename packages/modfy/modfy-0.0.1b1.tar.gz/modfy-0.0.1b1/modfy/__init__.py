283a6767a1ebeb07ae65050dd3bc57a3b3bcd8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package modfy/0.0.1b1/modfy-0.0.1b1.tar.gz

---
+++ packages/modfy/modfy-0.0.1b1.tar.gz/modfy-0.0.1b1/modfy/__init__.py
@@ -0,0 +1,6 @@
+from modfy.ProcessServer import ProcessServer
+from modfy.auth import Auth
+from modfy.operations.OperationClass import OperationClass
+from modfy.operations.Compress import Compress
+from modfy.main import Modfy
+from modfy.config import Config
