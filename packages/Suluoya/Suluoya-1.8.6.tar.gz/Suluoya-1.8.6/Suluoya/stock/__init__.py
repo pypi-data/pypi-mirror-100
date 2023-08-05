b82b499f7e79ae4c62dd72c7e35a38236c729fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:45 +0000

    Add package Suluoya/1.8.6/Suluoya-1.8.6.tar.gz

---
+++ packages/Suluoya/Suluoya-1.8.6.tar.gz/Suluoya-1.8.6/Suluoya/stock/__init__.py
@@ -0,0 +1,8 @@
+__name__ = "Suluoya"
+__author__ = 'Suluoya'
+__all__ = ['Markowit','GetGoodStock','GetData','GetDate','picture']
+from .Markowit import *
+from .GetGoodStock import *
+from .GetData import *
+from .GetDate import *
+from .picture import *
