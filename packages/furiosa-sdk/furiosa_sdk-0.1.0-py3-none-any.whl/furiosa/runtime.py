87fd6a8c6a2b029fd87cd21986f85b4c1ae738d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package furiosa-sdk/0.1.0/furiosa_sdk-0.1.0-py3-none-any.whl

---
+++ packages/furiosa-sdk/furiosa_sdk-0.1.0-py3-none-any.whl/furiosa/runtime.py
@@ -0,0 +1,8 @@
+import importlib.util
+
+_furiosa_sdk_runtime = importlib.util.find_spec("furiosa_sdk_runtime")
+
+if _furiosa_sdk_runtime is not None:
+    from furiosa_sdk_runtime import *
+    from furiosa_sdk_runtime import __version__
+    from furiosa_sdk_runtime import __full_version__
