da80966db614993778f2a978749ae5802d575c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package xyz-distributions/0.1/xyz_distributions-0.1.tar.gz

---
+++ packages/xyz-distributions/xyz_distributions-0.1.tar.gz/xyz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xyz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['xyz_distributions'],
+      author = 'Codeninja',
+      author_email = 'student@udacity.com',
+      zip_safe=False)
