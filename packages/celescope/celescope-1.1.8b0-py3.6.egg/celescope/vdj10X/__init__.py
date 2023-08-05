224f79a7a3395666ad801e7bff590ac9256363c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:05 +0000

    Add package celescope/1.1.8b0/celescope-1.1.8b0-py3.6.egg

---
+++ packages/celescope/celescope-1.1.8b0-py3.6.egg/celescope/vdj10X/__init__.py
@@ -0,0 +1,13 @@
+__STEPS__ = ['sample', 'convert', 'vdj_10X', ]
+__ASSAY__ = 'vdj10X'
+
+ref_dict = {
+    'hs': "/SGRNJ/Database/script/genome/10X/vdj/refdata-cellranger-vdj-GRCh38-alts-ensembl-2.0.0",
+    'mmu': "/SGRNJ/Database/script/genome/10X/vdj/refdata-cellranger-vdj-GRCm38-alts-ensembl-2.2.0",
+}
+
+soft_dict = {
+    '3.0': '/SGRNJ/Database/script/soft/cellranger/cellranger-3.0.2/cellranger',
+    '3.1': '/SGRNJ/Database/script/soft/cellranger/cellranger-3.1.0/cellranger',
+    '6.0': '/SGRNJ/Database/script/soft/cellranger/cellranger-6.0.0/cellranger'
+}
