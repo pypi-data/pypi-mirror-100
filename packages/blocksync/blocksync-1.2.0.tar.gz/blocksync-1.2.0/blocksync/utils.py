a1bf26ce1cee100cafece0aeda1de101e38a2fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:19 +0000

    Add package blocksync/1.2.0/blocksync-1.2.0.tar.gz

---
+++ packages/blocksync/blocksync-1.2.0.tar.gz/blocksync-1.2.0/blocksync/utils.py
@@ -0,0 +1,19 @@
+import random
+import string
+from inspect import signature
+from typing import Callable
+
+__all__ = ["validate_callback", "generate_random_data"]
+
+
+def validate_callback(c: Callable, n: int = 0) -> bool:
+    if callable(c):
+        sig = signature(c)
+        if len(sig.parameters) < n:
+            raise ValueError(f"{c} least required {n} arguments, but {len(sig.parameters)}")
+        return True
+    raise TypeError("{} is not callable".format(c))
+
+
+def generate_random_data(size: int) -> str:
+    return "".join(random.choices(string.ascii_letters + string.digits, k=size))
