bd1269bf878290472a944f729ca28963e5cf7d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package modfy/0.0.1/modfy-0.0.1-py3-none-any.whl

---
+++ packages/modfy/modfy-0.0.1-py3-none-any.whl/modfy/__init__.py
@@ -0,0 +1,5 @@
+from modfy.ProcessServer import ProcessServer
+from modfy.auth import Auth
+from modfy.operations.OperationClass import OperationClass
+from modfy.operations.Compress import Compress
+from modfy.main import Modfy
