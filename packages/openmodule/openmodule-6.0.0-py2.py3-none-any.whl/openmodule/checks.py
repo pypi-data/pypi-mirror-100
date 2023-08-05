46b91ec5c44f0287ec15e4d1bf9e2301deb2baf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package openmodule/6.0.0/openmodule-6.0.0-py2.py3-none-any.whl

---
+++ packages/openmodule/openmodule-6.0.0-py2.py3-none-any.whl/openmodule/checks.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from pydantic.main import BaseModel
+from sqlalchemy import DateTime
+
+from openmodule import config
+
+if TYPE_CHECKING:
+    from openmodule.core import OpenModuleCore
+    from openmodule.database.database import Database
+
+assert config.debug() or config.testing(), (
+    "checks module must not be used or imported in production environments"
+)
+
+
+class CheckingOpenModuleModel(type(BaseModel), type):
+    def __new__(mcs, name, bases, dct):
+        cls = super().__new__(mcs, name, bases, dct)
+
+        # noinspection PyUnresolvedReferences
+        for field_name, field in cls.__fields__.items():
+            if field.type_ == datetime:
+                assert "_timezone_validator" in field.class_validators, (
+                    "datetime fields must use the timezone_validator to ensure all datetime values "
+                    "are always naive. Otherwise runtime errors may occur depending on the isoformat used.\n"
+                    f"In class {name}, please add:\n"
+                    f'  _tz_{field_name} = timezone_validator("{field_name}")'
+                )
+
+        return cls
+
+
+def check_invalid_database_column_type(typ):
+    if config.run_checks():
+        assert not isinstance(typ, DateTime), "Do NOT use DateTime fields, use TZDateTime fields instead"
