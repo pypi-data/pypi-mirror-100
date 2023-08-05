a8106ce3833edd439b5966dbfa8a189ea1625a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package lumispy/0.1.0/lumispy-0.1.0.tar.gz

---
+++ packages/lumispy/lumispy-0.1.0.tar.gz/lumispy-0.1.0/README.md
@@ -0,0 +1,29 @@
+# LumiSpy
+
+[![Build Status](https://dev.azure.com/Lumispy/lumispy/_apis/build/status/LumiSpy.lumispy?branchName=master)](https://dev.azure.com/Lumispy/lumispy/_build/latest?definitionId=3&branchName=master)
+![Tests](https://github.com/lumispy/lumispy/workflows/Tests/badge.svg)
+[![Coverage Status](https://coveralls.io/repos/github/LumiSpy/lumispy/badge.svg?branch=master)](https://coveralls.io/github/LumiSpy/lumispy?branch=master)
+
+LumiSpy is an extension Python package for multi-dimensional data analysis 
+provided by the [HyperSpy](http://hyperspy.org) library. It is aimed at helping 
+with the analysis of luminescence spectroscopy data (cathodoluminescence, 
+photoluminescence, electroluminescence, Raman, SNOM).
+
+LumiSpy is released under the GPL v3 license. 
+
+If analysis using LumiSpy forms a part of published work please consider 
+recognising the code development by citing the
+[github repository](www.github.com/lumispy/lumispy).
+
+Instructions on how to install LumiSpy and get started: [installation guide](www.github.com/lumispy/lumispy/blob/master/INSTALLATION.md).
+
+[Tutorials and example workflows](https://github.com/lumispy/lumispy-demos)
+have been curated as a series of Jupyter notebooks that you can work through 
+and modify to perform many common analyses. Simply:
+
+1. Download the `lumispy_demos` repository in your desired folder
+2. Load lumispy (as shown above)
+3. In Jupyter lab, navigate to the folder and start using the notebook
+
+Everyone is welcome to contribute. Please read our
+[contributing guidelines](https://github.com/lumispy/lumispy/blob/master/.github/CONTRIBUTING.md) and get started!
---
+++ packages/lumispy/lumispy-0.1.0.tar.gz/lumispy-0.1.0/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The LumiSpy developers
+#
+# This file is part of LumiSpy.
+#
+# LumiSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LumiSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LumiSpy.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+exec(open('lumispy/release_info.py').read())  # grab version info
+
+
+setup(
+    name='lumispy',
+    version='0.1.0',
+    description='Luminescence spectroscopy data analysis with HyperSpy.',
+    author='The LumiSpy Developers',
+    #author_email=email,
+    license=license,
+    url="https://github.com/lumispy/lumispy",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+
+    packages=find_packages(),
+    # adjust the tabbing
+    install_requires=[
+        'numpy',
+        'scipy',
+        'hyperspy >= 1.5.2',        # earlier versions incompatible with numpy >= 1.17.0
+    ],
+    extras_require={"tests": ["pytest>=5.0"],
+                    "coverage": ["pytest-cov>=2.8.1", "coveralls>=1.10", "coverage>=5.0"]},
+    package_data={
+        "lumispy": ["*.py", "hyperspy_extension.yaml"],
+    },
+    entry_points={'hyperspy.extensions': ['lumispy = lumispy']},
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/lumispy/lumispy/issues',
+        'Source': 'https://github.com/lumispy/lumispy',
+    },
+    )
