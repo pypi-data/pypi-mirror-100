11784d0cbbf8755a641b2cf52c0b9b2982d59e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package shirkhan/0.0.14/shirkhan-0.0.14.tar.gz

---
+++ packages/shirkhan/shirkhan-0.0.14.tar.gz/shirkhan-0.0.14/shirkhan/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.0.14"
+__author__ = "shirkhan"
+
+from shirkhan.shir_syllable import *
+
+# utils
+from shirkhan.shir_encode_decode import *
+from shirkhan.shir_file import *
+from shirkhan.shir_string import *
+from shirkhan.shir_encryption import *
+from shirkhan import shir_csv as csv
+from shirkhan import shir_excel as excel
+
+if __name__ == '__main__':
+    pass
