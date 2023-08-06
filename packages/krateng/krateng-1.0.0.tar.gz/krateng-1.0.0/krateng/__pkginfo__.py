a7f71706e441086b69fdda74d47d38bda5aedae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:25 +0000

    Add package krateng/1.0.0/krateng-1.0.0.tar.gz

---
+++ packages/krateng/krateng-1.0.0.tar.gz/krateng-1.0.0/krateng/__pkginfo__.py
@@ -0,0 +1,36 @@
+name = "krateng"
+desc = "Metapackage for my system setup"
+author = {
+	"name":"Johannes Krattenmacher",
+	"email":"misc@dev.krateng.ch",
+	"github": "krateng"
+}
+version = 1,0,0
+versionstr = ".".join(str(n) for n in version)
+links = {
+	"pypi":"krateng",
+	"github":"kratengpy"
+}
+python_version = ">=3.6"
+requires = [
+	"borgbackup",
+	"doreah",
+	"jinja2",
+	"lxml",
+	"MechanicalSoup",
+	"PyYAML",
+	"requests",
+	"s3cmd",
+	"selenium"
+	"Send2Trash",
+	"tabulate",
+	"waitress",
+	"yeonji",
+	"youtube-dl",
+	
+]
+resources = [
+]
+
+commands = {
+}
