e7d33b25d9bdc85e0f819839e6542262fc7e1299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:36 +0000

    Add package odoo12-addon-pattern-import-export/12.0.2.4.0/odoo12_addon_pattern_import_export-12.0.2.4.0-py3-none-any.whl

---
+++ packages/odoo12-addon-pattern-import-export/odoo12_addon_pattern_import_export-12.0.2.4.0-py3-none-any.whl/odoo/addons/pattern_import_export/models/__init__.py
@@ -0,0 +1,7 @@
+from . import ir_exports
+from . import ir_exports_line
+from . import ir_actions
+from . import base
+from . import ir_fields
+from . import patterned_import_export
+from . import ir_attachment
