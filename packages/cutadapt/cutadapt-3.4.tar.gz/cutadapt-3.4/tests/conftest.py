df70a48da016b4782b5c1d32e28c489198a2ef29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:16 +0000

    Add package cutadapt/3.4/cutadapt-3.4.tar.gz

---
+++ packages/cutadapt/cutadapt-3.4.tar.gz/cutadapt-3.4/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from utils import assert_files_equal, datapath, cutpath
+from cutadapt.__main__ import main
+from cutadapt.report import Statistics
+
+
+@pytest.fixture(params=[1, 2])
+def cores(request):
+    return request.param
+
+
+@pytest.fixture
+def run(tmp_path):
+    def _run(params, expected, inpath) -> Statistics:
+        if type(params) is str:
+            params = params.split()
+        tmp_fastaq = tmp_path / expected
+        params += ['-o', tmp_fastaq]
+        params += [datapath(inpath)]
+        stats = main([str(p) for p in params])
+        # TODO redirect standard output
+        assert_files_equal(cutpath(expected), tmp_fastaq)
+        return stats
+
+    return _run
