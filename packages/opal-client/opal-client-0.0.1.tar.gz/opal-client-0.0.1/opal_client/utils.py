a3f6d5d08af32633ebbc460e6a6202fb5d18c19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:54 +0000

    Add package opal-client/0.0.1/opal-client-0.0.1.tar.gz

---
+++ packages/opal-client/opal-client-0.0.1.tar.gz/opal-client-0.0.1/opal_client/utils.py
@@ -0,0 +1,20 @@
+import asyncio
+import aiohttp
+import threading
+
+from typing import Tuple, Dict
+from fastapi import Response
+
+
+async def proxy_response(response: aiohttp.ClientResponse):
+    content = await response.text()
+    return Response(
+        content=content,
+        status_code=response.status,
+        headers=dict(response.headers),
+        media_type="application/json",
+    )
+
+
+def tuple_to_dict(tup: Tuple[str, str]) -> Dict[str, str]:
+    return dict([tup])
