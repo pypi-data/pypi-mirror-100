41fb43f2bfcda2e790ddfb9956a366c567f33ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:15 +0000

    Add package kinnaird-utils/0.0.1/kinnaird-utils-0.0.1.tar.gz

---
+++ packages/kinnaird-utils/kinnaird-utils-0.0.1.tar.gz/kinnaird-utils-0.0.1/test/test_text.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+import logging
+from kinnaird_utils.text.text import strip_special_characters, print_warning
+
+logger = logging.getLogger(__name__)
+
+
+def get_input(text):
+    return input(text)
+
+
+def answer():
+    ans = get_input('enter yes or no')
+    if ans == 'yes':
+        return 'you entered yes'
+    if ans == 'no':
+        return 'you entered no'
+
+
+class TextTestCase(unittest.TestCase):
+    def test_strip_special_characters(self):
+        input_string = "Special $#! characters   spaces 888323"
+        result = strip_special_characters(input_string)
+        expected_output = "Specialcharactersspaces888323"
+        self.assertEqual(result, expected_output)
