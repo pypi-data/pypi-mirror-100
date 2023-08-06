8d1ff95b124a2bab41cdd7827817e584b640d63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:02 +0000

    Add package dyndnsc/0.6.1/dyndnsc-0.6.1-py2.py3-none-any.whl

---
+++ packages/dyndnsc/dyndnsc-0.6.1-py2.py3-none-any.whl/dyndnsc/updater/dummy.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Module providing a dummy updater."""
+
+from .base import UpdateProtocol
+
+
+class UpdateProtocolDummy(UpdateProtocol):
+    """The dummy update protocol."""
+
+    _updateurl = "http://localhost.nonexistant/nic/update"
+    configuration_key = "dummy"
+
+    def __init__(self, hostname, **kwargs):
+        """
+        Initialize.
+
+        :param hostname: string hostname
+        """
+        self.hostname = hostname
+        super(UpdateProtocolDummy, self).__init__()
+
+    def update(self, ip):
+        """Pretend to update the IP on the remote service."""
+        return ip
