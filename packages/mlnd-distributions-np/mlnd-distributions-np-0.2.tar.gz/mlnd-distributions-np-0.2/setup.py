12f57f8d99cb8d8e7717648b43db6f893e2a457c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:22 +0000

    Add package mlnd-distributions-np/0.2/mlnd-distributions-np-0.2.tar.gz

---
+++ packages/mlnd-distributions-np/mlnd-distributions-np-0.2.tar.gz/mlnd-distributions-np-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlnd-distributions-np',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['mlnd-distributions-np'],
+      author='Nidhi P',
+      author_email='nidhipansuriya17@gmail.com',
+      zip_safe=False)
