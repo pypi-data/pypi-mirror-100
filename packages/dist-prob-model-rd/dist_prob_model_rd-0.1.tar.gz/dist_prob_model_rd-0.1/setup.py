507584bf8a3bbab039b639dd67adb317c13a204c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:23 +0000

    Add package dist-prob-model-rd/0.1/dist_prob_model_rd-0.1.tar.gz

---
+++ packages/dist-prob-model-rd/dist_prob_model_rd-0.1.tar.gz/dist_prob_model_rd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_prob_model_rd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_prob_model_rd'],
+      zip_safe=False)
