ceb369e2ce3d820e29792cbe2db36fecb4c29db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package KoksSzachy/0.5.3/KoksSzachy-0.5.3.tar.gz

---
+++ packages/KoksSzachy/KoksSzachy-0.5.3.tar.gz/KoksSzachy-0.5.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os 
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+
+setup(name='KoksSzachy',
+      version='0.5.3',
+      description='Lubisz grać w szachy? Podobał ci się chess.com lub lichess? W takim razie pokochasz KoksSzachy! heart',
+      author='a1eaiactaest, czajaproggramer, AeroRocket, igoy1, Kajtek-creator',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['koksszachy'],
+      classifires=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+      url="https://github.com/a1eaiactaest/KoksSzachy",
+      install_requires=['chess', 'Flask'],
+      python_requires='>=3.6',
+      entry_points={
+        "console_scripts":[
+          "koksszachy=koksszachy.__main__:main",
+          "KoksSzachy=koksszachy.__main__:main",
+        ]
+      },
+      include_package_data=True)
