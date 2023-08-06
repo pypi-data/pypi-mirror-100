4aa5678b247841c5c78622bb861972e11a650968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:49 +0000

    Add package mcanitexgen/1.2.0/mcanitexgen-1.2.0-py3-none-any.whl

---
+++ packages/mcanitexgen/mcanitexgen-1.2.0-py3-none-any.whl/mcanitexgen/integration/beet.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+
+from beet import Context
+from beet.library.resource_pack import Language, TextureMcmeta
+from beet.toolchain.context import Plugin
+
+import mcanitexgen
+
+
+def beet_default(ctx: Context):
+    """ Entry point into beet pipeline. Loads configuration and executes mcanitexgen plugin """
+
+    config = ctx.meta.get("mcanitexgen", {})
+    load = config.get("load", ())
+
+    ctx.require(create_mcanitexgen_plugin(load))
+
+
+def create_mcanitexgen_plugin(load: Iterable[str] = ()):
+    def plugin(ctx: Context):
+        minecraft = ctx.assets["minecraft"]
+
+        for pattern in load:
+            for path in ctx.directory.glob(pattern):
+                animations = mcanitexgen.animation.load_animations(path)
+
+                minecraft.textures_mcmeta.merge(
+                    {
+                        name: TextureMcmeta(texanim.to_mcmeta())
+                        for name, texanim in animations.items()
+                    }
+                )
+
+    return plugin
