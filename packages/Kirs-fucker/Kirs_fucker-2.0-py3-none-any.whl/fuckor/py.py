4c48cab4e9851d91012e6a9cfa4b794f19239018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:57 +0000

    Add package Kirs-fucker/2.0/Kirs_fucker-2.0-py3-none-any.whl

---
+++ packages/Kirs-fucker/Kirs_fucker-2.0-py3-none-any.whl/fuckor/py.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+__author__ = 'KirsWang'
+__mtime__ = '2021/3/29'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+                  
+"""
+
+def func():
+    print("一个好玩神奇的包")
