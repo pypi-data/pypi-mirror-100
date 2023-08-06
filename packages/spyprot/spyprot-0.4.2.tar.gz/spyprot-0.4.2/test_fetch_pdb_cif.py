7a074fbd3217e00b6dab0532dac1b2e796510004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:35 +0000

    Add package spyprot/0.4.2/spyprot-0.4.2.tar.gz

---
+++ packages/spyprot/spyprot-0.4.2.tar.gz/spyprot-0.4.2/test_fetch_pdb_cif.py
@@ -0,0 +1,43 @@
+from spyprot.fetchChainInfo import PdbFile, MMCIFfile
+import tempfile
+import os
+
+
+def test_download_pdb_files():
+    dir = os.path.join(tempfile.gettempdir(), 'test_pdb')
+    os.makedirs(dir, exist_ok=True)
+    p = PdbFile(dir, "1j85", "A").download()
+    p = PdbFile(dir, "1k36", "A").download()
+    p = PdbFile(dir, "1k36", "A", 'CA').download()
+    p = MMCIFfile(dir, "1k36", "A").download()
+
+    # Download and extract chain 5
+    p = PdbFile(dir, "6p5i", "5").download()
+    # Download and extract only C3' atoms from chain 5
+    p = PdbFile(dir, "6p5i", "5", 'C3\'').download()
+
+    m = MMCIFfile(dir, "1k36", "A").download()
+    # Download and extract chain 5
+    m = MMCIFfile(dir, "6p5i", "5").download()
+    # Download and extract only C3' atoms from chain 5
+
+    MMCIFfile(dir, "3bjx", "A").download()
+    MMCIFfile(dir, "1et4", "A").download()
+
+    PdbFile(dir, "4v7m", "B3").download()
+    # Bundle but not translation
+    # PdbFile(dir, "6az3", "2").download()
+    PdbFile(dir, "6az3", "2", 'C3\'').download()
+
+
+#test_download_pdb_files()
+dir = os.path.join(tempfile.gettempdir(), 'test_pdb')
+outf = PdbFile(dir, "4v7m").download()
+print(outf)
+
+PdbFile(dir, "6az3", "2", 'C3\'').download()
+
+
+dir = os.path.join(tempfile.gettempdir(), 'test_pdb')
+#p = PdbFile(dir, "7bl1", "AAA").download()
+p = PdbFile(dir, "7bl1").download()
