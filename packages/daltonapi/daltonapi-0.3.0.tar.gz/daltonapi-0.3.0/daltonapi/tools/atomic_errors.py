a475191f262b587bccb9fbd7594bd582230e156e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:01 +0000

    Add package daltonapi/0.3.0/daltonapi-0.3.0.tar.gz

---
+++ packages/daltonapi/daltonapi-0.3.0.tar.gz/daltonapi-0.3.0/daltonapi/tools/atomic_errors.py
@@ -0,0 +1,50 @@
+"""Atomic Errors
+
+Error Classes for the package"""
+
+
+class RequestFailedError(Exception):
+    """Exception called when an API request fails"""
+
+    def __init__(self):
+        super().__init__(self)
+        self.message = "The request did not succeed."
+
+    def __str__(self):
+        return self.message
+
+
+class AssetIDError(Exception):
+    """Exception called when Atomic ID is invalid"""
+
+    def __init__(self, asset_id):
+        super().__init__(self)
+        self.message = (
+            "Atomic ID %s is invalid. The Atomic ID must be a string integer."
+            % asset_id
+        )
+
+    def __str__(self):
+        return self.message
+
+
+class NoFiltersError(Exception):
+    """Exception called when no filters are provided"""
+
+    def __init__(self):
+        super().__init__(self)
+        self.message = "This method requires at least one argument."
+
+    def __str__(self):
+        return self.message
+
+
+class NoCollectionImageError(Exception):
+    """Exception called when a collection has no image"""
+
+    def __init__(self):
+        super().__init__(self)
+        self.message = "This collection has no image."
+
+    def __str__(self):
+        return self.message
