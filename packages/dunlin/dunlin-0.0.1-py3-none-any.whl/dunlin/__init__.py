88b0ee1a477470a34d11230d8527db324636cfbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:04 +0000

    Add package dunlin/0.0.1/dunlin-0.0.1-py3-none-any.whl

---
+++ packages/dunlin/dunlin-0.0.1-py3-none-any.whl/dunlin/__init__.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from os.path  import dirname, join
+
+#Add path
+_dir = dirname(__file__)
+sys.path.insert(0, _dir)
+
+from .model_handler          import *
+from ._utils_plot.utils_plot import colors, palette_types
+
+styles = {}
+
+_utils_plot_path = join(_dir, '_utils_plot')
+for file in os.listdir(_utils_plot_path):
+    try:
+        name, ext = file.split('.')
+        if ext == 'mplstyle':
+            styles[name] = join(_utils_plot_path, file)
+    except:
+        pass
