3a4e96d0e907a65c43b1c0aae35d537d2e2344e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:44 +0000

    Add package iokobot/0.7/iokobot-0.7.tar.gz

---
+++ packages/iokobot/iokobot-0.7.tar.gz/iokobot-0.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def readme():
+    readme = ''
+    with open("README.md") as files:
+        readme = files.read()
+    return readme
+
+
+setup(
+    name='iokobot',
+    packages=['iokobot'],
+    version='0.7',
+    license='MIT',
+    description='iokobot is a python library to help you make a smart bot, using machine learning algorithm',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Rizki Maulana',
+    author_email='rizkimaulana348@gmail.com',
+    url='https://github.com/rizki4106/iokobot',
+    download_url='https://github.com/rizki4106/iokobot/archive/v_01.tar.gz',
+    keywords=['bot', 'chatbot', 'iokobot', 'machine-learning'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
