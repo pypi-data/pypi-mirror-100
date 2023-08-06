0c6246d8af1cd0ecbeb69ee0d84ba4195e9f8b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:38 +0000

    Add package Sohbet/1.1.1/Sohbet-1.1.1.tar.gz

---
+++ packages/Sohbet/Sohbet-1.1.1.tar.gz/Sohbet-1.1.1/README.txt
@@ -0,0 +1,3 @@
+# Sohbet
+
+[https://sohbet.page](https://sohbet.page) Sohbet odaları chat yaparak yeni kişilerle tanışmanızı sohbet ederek yeni arkadaşlar bulmanızı, mobil sohbet siteleri seçeneği ile farklı ülkelerden ve şehirlerden kişilerle bir araya gelmenizi ve eğlenceli zaman geçirmenizi sağlar.
---
+++ packages/Sohbet/Sohbet-1.1.1.tar.gz/Sohbet-1.1.1/setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'Sohbet',
+    packages = ['sohbet'],
+    version = '1.1.1',
+    description = 'Sohbet odaları chat üyelik gerekmeyen ücretsiz sohbet siteleri mobil chat avanatjları ile arkadaş bul eğlenceli zaman geçir.',
+    long_description = 'https://sohbet.page Sohbet odaları chat üyelik gerekmeyen ücretsiz sohbet siteleri mobil chat avanatjları ile arkadaş bul eğlenceli zaman geçir.',
+    author = 'Sohbet',
+    author_email = 'emailadresi@email.com',
+    url = 'https://github.com/sohbetweb',
+    keywords='bedava sohbet, chat, sohbet odaları',
+    project_urls={
+        "Documentation": "https://sohbet.page",
+},
+)
