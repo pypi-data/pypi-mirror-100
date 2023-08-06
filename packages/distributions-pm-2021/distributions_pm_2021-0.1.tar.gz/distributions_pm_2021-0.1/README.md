9e366eb8956ace7d25c38bddb8c7d07ffd968713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:02 +0000

    Add package distributions-pm-2021/0.1/distributions_pm_2021-0.1.tar.gz

---
+++ packages/distributions-pm-2021/distributions_pm_2021-0.1.tar.gz/distributions_pm_2021-0.1/README.md
@@ -0,0 +1,2 @@
+# Intro
+This is a test README file.
---
+++ packages/distributions-pm-2021/distributions_pm_2021-0.1.tar.gz/distributions_pm_2021-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_pm_2021',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pm_2021'],
+      zip_safe=False)
