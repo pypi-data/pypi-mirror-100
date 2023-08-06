e064a94985dd9171a3e66bcbae4d98e36feff228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:44 +0000

    Add package hashedml/0.0.2/hashedml-0.0.2.tar.gz

---
+++ packages/hashedml/hashedml-0.0.2.tar.gz/hashedml-0.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='hashedml',
+    version='0.0.2',
+    description='Hash based machine learning',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mtingers/hashedml',
+    download_url='https://pypi.python.org/pypi/hashedml',
+    license='MIT',
+    author='Matth Ingersoll',
+    author_email='matth@mtingers.com',
+    maintainer='Matth Ingersoll',
+    maintainer_email='matth@mtingers.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    install_requires=[
+        'textblob',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hashedml=hashedml.hashedml:main',
+        ],
+    },
+    packages=find_packages(exclude=['tests*']),
+)
