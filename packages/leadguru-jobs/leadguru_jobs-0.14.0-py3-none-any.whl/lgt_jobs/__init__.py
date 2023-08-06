51539c526b7a79e46bf927c82785b64f96f23653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:28 +0000

    Add package leadguru-jobs/0.14.0/leadguru_jobs-0.14.0-py3-none-any.whl

---
+++ packages/leadguru-jobs/leadguru_jobs-0.14.0-py3-none-any.whl/lgt_jobs/__init__.py
@@ -0,0 +1,34 @@
+from .basejobs import (BaseBackgroundJobData, BaseBackgroundJob, InvalidJobTypeException)
+from .bots_stats import BotStatsUpdateJob, BotsCredentialsUpdateJob, RestartBotsJob
+from .smtp import SendMailJob
+from .leads import ArchiveLeadsJob
+from .runner import (BackgroundJobRunner)
+from .simple_job import (SimpleTestJob)
+
+jobs_map = {
+    "SimpleTestJob": SimpleTestJob,
+    "BotStatsUpdateJob": BotStatsUpdateJob,
+    "ArchiveLeadsJob": ArchiveLeadsJob,
+    "BotsCredentialsUpdateJob": BotsCredentialsUpdateJob,
+    "RestartBotsJob": RestartBotsJob,
+    "SendMailJob": SendMailJob
+}
+__all__ = [
+    # Jobs
+    SimpleTestJob,
+    BotStatsUpdateJob,
+    ArchiveLeadsJob,
+    BotsCredentialsUpdateJob,
+    RestartBotsJob,
+    SendMailJob,
+    SimpleTestJob,
+
+    # module classes
+    BackgroundJobRunner,
+    BaseBackgroundJobData,
+    BaseBackgroundJob,
+    InvalidJobTypeException,
+
+    # mapping
+    jobs_map
+]
