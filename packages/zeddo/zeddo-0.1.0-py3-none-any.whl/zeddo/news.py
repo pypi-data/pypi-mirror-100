373951aaa9ddb123b4bb5123789dbc57e7207736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:02 +0000

    Add package zeddo/0.1.0/zeddo-0.1.0-py3-none-any.whl

---
+++ packages/zeddo/zeddo-0.1.0-py3-none-any.whl/zeddo/news.py
@@ -0,0 +1,36 @@
+import requests
+
+# Cache list of all news sources
+all_sources = None
+
+
+def get_sources(api_key):
+    url = 'https://newsapi.org/v2/sources?apiKey={}'.format(api_key)
+
+    response = requests.request('GET', url)
+    if response.status_code != 200:
+        raise RuntimeError(response.json()['message'])
+
+    return response.json()['sources']
+
+
+def get_top_news(api_key, language, n):
+    global all_sources
+    if all_sources is None:
+        all_sources = [source['id'] for source in get_sources(api_key)]
+
+    # Since `sources` (or `country` or `q`) is required, provide all sources.
+    url = (
+        'https://newsapi.org/v2/top-headlines?' +
+        'apiKey={}&'.format(api_key) +
+        'language={}&'.format(language) +
+        'sources={}&'.format(','.join(all_sources)) +
+        'pageSize={}'.format(n)  # no need for more than n entries
+    )
+
+    response = requests.request('GET', url)
+    if response.status_code != 200:
+        raise RuntimeError(response.json()['message'])
+
+    articles = response.json()['articles']
+    return articles[:n] if n < len(articles) else articles
