0fd4236bf4c5d694578858cc5e1204329f448611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:27 +0000

    Add package pymosp/0.4.1/pymosp-0.4.1-py3-none-any.whl

---
+++ packages/pymosp/pymosp-0.4.1-py3-none-any.whl/pymosp/mospobject.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+class MOSPObject:
+    def __init__(self):
+        self.name: str
+        self.name: str
+        self.last_updated: datetime
+        self.json_object: dict
