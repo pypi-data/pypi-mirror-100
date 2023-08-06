5e24c306072ca6dbd4915c34286129332ec95fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:37 +0000

    Add package qiskit-nature/0.1.0/qiskit-nature-0.1.0.tar.gz

---
+++ packages/qiskit-nature/qiskit-nature-0.1.0.tar.gz/qiskit-nature-0.1.0/qiskit_nature/mappers/__init__.py
@@ -0,0 +1,47 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Operator to Qubit Mappers (:mod:`qiskit_nature.mappers`)
+========================================================
+
+.. currentmodule:: qiskit_nature.mappers
+
+
+
+Second-Quantization Mappers
++++++++++++++++++++++++++++
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   BravyiKitaevMapper
+   DirectMapper
+   JordanWignerMapper
+   LinearMapper
+   ParityMapper
+
+"""
+
+from .second_quantization import BravyiKitaevMapper
+from .second_quantization import DirectMapper
+from .second_quantization import JordanWignerMapper
+from .second_quantization import LinearMapper
+from .second_quantization import ParityMapper
+
+__all__ = [
+    "BravyiKitaevMapper",
+    "DirectMapper",
+    "JordanWignerMapper",
+    "LinearMapper",
+    "ParityMapper",
+]
---
+++ packages/qiskit-nature/qiskit-nature-0.1.0.tar.gz/qiskit-nature-0.1.0/qiskit_nature/transformers/__init__.py
@@ -0,0 +1,36 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+QMolecule Transformers (:mod:`qiskit_nature.transformers`)
+==========================================================
+
+.. currentmodule:: qiskit_nature.transformers
+
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   ActiveSpaceTransformer
+   FreezeCoreTransformer
+
+"""
+
+from .active_space_transformer import ActiveSpaceTransformer
+from .base_transformer import BaseTransformer
+from .freeze_core_transformer import FreezeCoreTransformer
+
+__all__ = [
+    'ActiveSpaceTransformer',
+    'BaseTransformer',
+    'FreezeCoreTransformer',
+]
