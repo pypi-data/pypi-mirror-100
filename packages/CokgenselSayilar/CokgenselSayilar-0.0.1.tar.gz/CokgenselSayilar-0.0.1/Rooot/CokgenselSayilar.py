f9476d8618ff82a7bee01b1c710df6cd5540373b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:44 +0000

    Add package CokgenselSayilar/0.0.1/CokgenselSayilar-0.0.1.tar.gz

---
+++ packages/CokgenselSayilar/CokgenselSayilar-0.0.1.tar.gz/CokgenselSayilar-0.0.1/Rooot/CokgenselSayilar.py
@@ -0,0 +1,4 @@
+def CokgenselSayilar(num1, num2):
+	for x in range(1, num2+1):
+         sonuc = (2*x+(num1-2)*((x*x)-x))/2
+         print(sonuc)
---
+++ packages/CokgenselSayilar/CokgenselSayilar-0.0.1.tar.gz/CokgenselSayilar-0.0.1/Rooot/__init__.py
@@ -0,0 +1,1 @@
+from CokgenselSayilar import CokgenselSayilar
