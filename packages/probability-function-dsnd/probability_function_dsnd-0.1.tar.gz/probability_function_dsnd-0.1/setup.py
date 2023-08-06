37a585440a6e88a732484f4627b71945316c4fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:36 +0000

    Add package probability-function-dsnd/0.1/probability_function_dsnd-0.1.tar.gz

---
+++ packages/probability-function-dsnd/probability_function_dsnd-0.1.tar.gz/probability_function_dsnd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_function_dsnd',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_function_dsnd'],
+      author = 'Melanija Gerasimovska',
+      author_email = 'melanija_gerasimovska@hotmail.com',
+      zip_safe=False)
