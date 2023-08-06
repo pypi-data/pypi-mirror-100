abb595a3f915c4591afdc1a4b860f1f0bf8c0e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:24 +0000

    Add package wisesight/0.0.2/wisesight-0.0.2.tar.gz

---
+++ packages/wisesight/wisesight-0.0.2.tar.gz/wisesight-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import pathlib
+import os
+from setuptools import setup
+
+# The directory containing this file
+CURRENT_DIR = pathlib.Path(__file__).parent
+
+setup(
+    name='wisesight',
+    version='0.0.2',
+    url='https://github.com/plutonyx/wisesight',
+    author='Thaweesak Chusri',
+    author_email='p4l3k1n6@gmail.com',
+    description='The wisesight social listening project',
+    long_description=open(os.path.join(CURRENT_DIR, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=[],    
+    include_package_data=True,
+    install_requires=[],
+)
