6096479fc080402caf2fe6cedfaf776d65de7c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:17 +0000

    Add package scrapista/0.0.5/scrapista-0.0.5.tar.gz

---
+++ packages/scrapista/scrapista-0.0.5.tar.gz/scrapista-0.0.5/scrapista/helpers/__init__.py
@@ -0,0 +1,6 @@
+from scrapista.helpers import money_string_to_int
+from scrapista.helpers import put_year_limit
+from scrapista.helpers import get_age
+from scrapista.helpers import get_bdate
+from scrapista.helpers import get_count
+from scrapista.helpers import get_word_info
