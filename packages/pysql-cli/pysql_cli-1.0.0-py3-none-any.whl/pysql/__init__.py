9865e95d18057eb3da05ab83a74902aed26c1fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:48 +0000

    Add package pysql-cli/1.0.0/pysql_cli-1.0.0-py3-none-any.whl

---
+++ packages/pysql-cli/pysql_cli-1.0.0-py3-none-any.whl/pysql/__init__.py
@@ -0,0 +1,31 @@
+"""
+module for PySQL wrapper
+functions, for using as a
+library
+"""
+
+__version__ = "1.0.0"
+__author__ = "Devansh Singh"
+__email__ = "devanshamity@gmail.com"
+__license__ = "MIT"
+
+from pysql import *
+
+"""
+classes for functions
+
+for initializing object instances,
+use (username, password) of
+local MySQL server
+"""
+
+from pysql.packages.auth import Database
+from pysql.packages.ddl_commands import DDL, Alter
+from pysql.packages.dml_commands import DML
+from pysql.data.export import Export
+from pysql.data.imports import Import
+
+"""
+PySQL
+Devansh Singh, 2021
+"""
