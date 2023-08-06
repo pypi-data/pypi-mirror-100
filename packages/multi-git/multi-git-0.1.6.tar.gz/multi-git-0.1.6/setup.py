82d3e464ff093009da6862ce0048c5d6bde9261a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:21 +0000

    Add package multi-git/0.1.6/multi-git-0.1.6.tar.gz

---
+++ packages/multi-git/multi-git-0.1.6.tar.gz/multi-git-0.1.6/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Setup script for this package"""
+
+from distutils.core import setup
+import os
+
+## The text of the README file
+readme_text = ""
+if os.path.exists("README.md"):
+    with open("README.md") as fh:
+        readme_text = fh.read()
+
+setup(
+    name = 'multi-git',
+    version = '0.1.6',
+    description = 'Multi-Git',
+    author = 'Daniel Kullmann',
+    author_email = 'python@danielkullmann.de',
+    url = 'https://pypi.org/project/multi-git/',
+    packages = [],
+    scripts = ['multi-git'],
+    requires = ['toml', 'config_path'],
+    license = "MIT",
+    long_description=readme_text,
+    long_description_content_type="text/markdown",
+)
