0e66b6400c8881925c7896ec76ccd2c5da0dbae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:18 +0000

    Add package StoaCalculator/0.0.2/StoaCalculator-0.0.2.tar.gz

---
+++ packages/StoaCalculator/StoaCalculator-0.0.2.tar.gz/StoaCalculator-0.0.2/stoacalculator/__init__.py
@@ -0,0 +1,18 @@
+def add_numbers(num1, num2):
+    print(num1 + num2)
+
+
+def substract_numbers(num1, num2):
+    print(num1 - num2)
+
+
+def multiply_numbers(num1, num2):
+    print(num1 * num2)
+
+
+def divide_numbers(num1, num2):
+    print(num1 / num2)
+
+
+def pow_numbers(num1, num2):
+    print(num1 ** num2)
