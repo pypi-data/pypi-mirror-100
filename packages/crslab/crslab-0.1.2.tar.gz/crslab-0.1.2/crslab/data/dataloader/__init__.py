035a8b0674e4ea1298cb30ccd4fa9fa8078593d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package crslab/0.1.2.0/crslab-0.1.2.tar.gz

---
+++ packages/crslab/crslab-0.1.2.tar.gz/crslab-0.1.2/crslab/data/dataloader/__init__.py
@@ -0,0 +1,6 @@
+from .base import BaseDataLoader
+from .kbrd import KBRDDataLoader
+from .kgsf import KGSFDataLoader
+from .redial import ReDialDataLoader
+from .tgredial import TGReDialDataLoader
+from .inspired import InspiredDataLoader
