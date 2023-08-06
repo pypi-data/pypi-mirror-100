853b770d3e0c5666d8c502a79704a388e54c470c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:16 +0000

    Add package drupal-dockerizer/0.0.1/drupal_dockerizer-0.0.1.tar.gz

---
+++ packages/drupal-dockerizer/drupal_dockerizer-0.0.1.tar.gz/drupal_dockerizer-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drupal_dockerizer",
+    entry_points='''
+        [console_scripts]
+        drupal-dockerizer=drupal_dockerizer.cli:cli
+    ''',
+    version="0.0.1",
+    author="Vladislav Sadretdinov",
+    author_email="svicervlad@gmail.com",
+    description="Cli tool for up drupal projects in docker containers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jet-dev-team/drupal-dockerizer-cli",
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'appdirs',
+        'PyYAML',
+        'wheel'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
