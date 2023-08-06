99d52399dfd0bcdea0d47e3cf637f4b8ddf13b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:09 +0000

    Add package monke/1.0.5/monke-1.0.5-py3-none-any.whl

---
+++ packages/monke/monke-1.0.5-py3-none-any.whl/monke/server.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+import fuid
+from monke import connection
+
+class Server:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connections = []
+        self.listeners = []
+        self.fuid = fuid.Generator()
+
+    def __handler(self):
+        self.server.listen()
+
+        while True:
+            req = self.server.accept()
+
+            conn = connection.Connection(*req, self.fuid.fuid(), self)
+            self.connections.append(conn)
+
+            threading.Thread(target=conn.start)\
+                .start()
+
+    def on(self, event : str = None):
+        def decorator(func):
+            event_name = event
+
+            if event_name == None:
+                event_name = func.__name__
+            
+            self.listeners.append({
+                "event": event_name,
+                "func": func
+            })
+
+        return decorator
+
+    def start(self, host : str = "127.0.0.1", port : int = 6000):
+        self.server.bind((host, port))
+        self.__handler()
