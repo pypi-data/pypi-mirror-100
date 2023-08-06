5f486858de1ae72ec080400a9ece6f0ca51a4387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:49 +0000

    Add package paypal-casinos/0.1.2/paypal_casinos-0.1.2.tar.gz

---
+++ packages/paypal-casinos/paypal_casinos-0.1.2.tar.gz/paypal_casinos-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['paypal_casinos']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'paypal-casinos',
+    'version': '0.1.2',
+    'description': 'PayPal Casinos — List of Online Casinos That Accept PayPal',
+    'long_description': '# Ever wondered what it takes to build the best PayPal Casino website for the UK?\n\nTo many, this website may look like something sketched out and put together on a Friday pub lunch down the local tavern, but that’s not the whole story. Ok, so our investors did indeed conceive the idea while having a bar meeting one Friday afternoon. Since then, however, a considerable amount of expertise has gone into planning and development.\n\n## How it all started\n\nFirst of all, kudos to the creators who understood the market demand for a website that focuses entirely on [PayPal casinos](https://casinos-paypal.net/). You would have to be in the industry to know it. Still, the PayPal e-wallet is the most popular means of making casino deposits and withdrawals outside bank transactions. It is not by chance that PayPal casino(s) is one of the highest-ranking and popular casino related search terms in the UK. \n\nThis PayPal casino guide was created and built by professionals with vast experience in the gambling industry. With several successful casino-related projects under our belt already, we decided our next project should focus on the things that matter most to UK players. Regular gamblers can compromise on many things but rarely compromise on their method to transact with a casino.\n\n## Our data-driven approach\n\nKnowledge is the cornerstone with which our savvy team of experts commenced developing and deploying the PayPal casino guide for the UK. Using a data-driven approach from the ground up, we first mapped out the pages to be included. Starting from the beginning, as you do, our technical team got to work creating a stable and robust site build using esentity elastic search tools and plugins. Our content creators then got down to the business of writing the information players need to know when using the PayPal e-wallet with UK casinos.\n\nOnce complete, we were ready to insert our real expertise, a database of the best casinos in the UK that accept [PayPal](https://www.paypal.com/), passed through an advanced algorithm refined over many years. Unfortunately for you, that’s a closely guarded secret we don’t give away. If we did, we would have a lot of competition on our hands.\n\n## Moving Forward\n\nOur project is still in its infancy, but we believe there’s enough here already to help real money UK players find the best online PayPal casinos. We also hope that our guide contains enough valuable information to help players decide if PayPal is the best option to use compared to the possibilities. But we are never complacent. We know there is much competition in this industry. Our dynamic team hasn’t succeeded where many have failed by having a ‘that’s good enough’ mentality. For this reason, we will continue to develop our platform to bring real players like you the best options around to play casino games in the UK using PayPal.\n\n## We’re on a mission\n\nThe team behind this UK PayPal casino guide is on a mission to bring you the very latest and best sites to play casino games using [PayPal](https://www.paypal.com/). We aim to continue our research and update our lists as we discover new and exciting places to wager. Our team of dedicated professionals will also keep you posted with the latest developments surrounding PayPal and its use to play casino games.\n\nCome back and stay regularly updated on the best places to play online casino games using PayPal in the UK.\n',
+    'author': 'PayPal Casinos',
+    'author_email': 'info@casinos-paypal.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://casinos-paypal.net',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
