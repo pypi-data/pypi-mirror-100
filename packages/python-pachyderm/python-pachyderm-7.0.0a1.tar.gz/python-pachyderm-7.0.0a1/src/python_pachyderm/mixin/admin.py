e36bf4b3f71cac0e8df953c08cac593b18183adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:36 +0000

    Add package python-pachyderm/7.0.0a1/python-pachyderm-7.0.0a1.tar.gz

---
+++ packages/python-pachyderm/python-pachyderm-7.0.0a1.tar.gz/python-pachyderm-7.0.0a1/src/python_pachyderm/mixin/admin.py
@@ -0,0 +1,12 @@
+from python_pachyderm.proto.admin import admin_pb2_grpc as admin_grpc
+from python_pachyderm.proto.pps import pps_pb2 as pps_proto
+from python_pachyderm.service import Service
+
+
+class AdminMixin:
+    
+    def inspect_cluster(self):
+        """
+        Inspects a cluster. Returns a `ClusterInfo` object.
+        """
+        return self._req(Service.ADMIN, "InspectCluster", req=admin_grpc.google_dot_protobuf_dot_empty__pb2.Empty())
