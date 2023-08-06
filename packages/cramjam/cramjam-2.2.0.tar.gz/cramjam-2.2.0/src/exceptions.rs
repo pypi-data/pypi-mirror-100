39f82bbcfe14246bdc3b3422f3004793847d5a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:42 +0000

    Add package cramjam/2.2.0/cramjam-2.2.0.tar.gz

---
+++ packages/cramjam/cramjam-2.2.0.tar.gz/cramjam-2.2.0/src/exceptions.rs
@@ -0,0 +1,8 @@
+#![allow(missing_docs)]
+//! cramjam specific Python exceptions
+use pyo3::create_exception;
+use pyo3::exceptions::PyException;
+
+create_exception!(cramjam, CompressionError, PyException);
+create_exception!(cramjam, DecompressionError, PyException);
+create_exception!(cramjam, CramjamError, PyException);
