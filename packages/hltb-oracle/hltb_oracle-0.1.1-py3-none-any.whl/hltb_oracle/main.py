725a82d2f897dc79239654e931eeb0d7f9c378ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:03 +0000

    Add package hltb-oracle/0.1.1/hltb_oracle-0.1.1-py3-none-any.whl

---
+++ packages/hltb-oracle/hltb_oracle-0.1.1-py3-none-any.whl/hltb_oracle/main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from hltb_oracle.models import HowLongToBeatGame
+from hltb_oracle.parser import HowLongToBeatParser
+from hltb_oracle.website import HowLongToBeatWebsite, SortBy
+
+
+class HowLongToBeatOracle:
+    @staticmethod
+    def get(
+        search_query: Optional[str] = None,
+        page: Optional[int] = None,
+        sort_by: SortBy = SortBy.most_popular,
+        only_recently_updated=False,
+    ):
+        query_string = "recently updated" if only_recently_updated else search_query
+        html = HowLongToBeatWebsite.search_results(
+            query_string=query_string,
+            page=page,
+            sort_by=sort_by,
+        )
+        result = HowLongToBeatParser.parse_game_list(html)
+        return [HowLongToBeatGame(game) for game in result["data"]], result["pages"][
+            "total_pages"
+        ]
