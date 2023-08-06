c112ccf6b02969fdbb27073d18b7e442ba5b64f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:26 +0000

    Add package calculador-de-viga/0.0.3/calculador_de_viga-0.0.3.tar.gz

---
+++ packages/calculador-de-viga/calculador_de_viga-0.0.3.tar.gz/calculador_de_viga-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='calculador_de_viga',
+    version='0.0.3',
+    url='https://github.com/doglinhaskkk/calculador_de_vigas',
+    license='Boost Software License',
+    author='Doglas Rocha',
+    author_email='doglasfmr@gmail.com',
+    keywords='Vigas',
+    description=u'Calculador de Vigas',
+    packages=['calculador_de_viga'],
+    install_requires=['numpy', 'pandas'])
