52efafd922c342067524bd10aeb6c6a421014a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package odoo13-addon-shopinvader-search-engine/13.0.1.1.0/odoo13_addon_shopinvader_search_engine-13.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-search-engine/odoo13_addon_shopinvader_search_engine-13.0.1.1.0-py3-none-any.whl/odoo/addons/shopinvader_search_engine/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Shopinvader Catalog Search Engine Connector",
+    "version": "13.0.1.1.0",
+    "author": "Akretion",
+    "development_status": "Stable/Production",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": ["shopinvader", "connector_search_engine"],
+    "data": [
+        "views/shopinvader_backend_view.xml",
+        "views/product_view.xml",
+        "views/product_category_view.xml",
+        "data/ir_export_product.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
