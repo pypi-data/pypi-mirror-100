b3300b029661b1453314fa6873e04ae57c6f8701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:27 +0000

    Add package pytubekit/0.0.15/pytubekit-0.0.15.tar.gz

---
+++ packages/pytubekit/pytubekit-0.0.15.tar.gz/pytubekit-0.0.15/pytubekit/configs.py
@@ -0,0 +1,31 @@
+"""
+All configurations
+"""
+from pytconf import Config, ParamCreator
+
+
+class ConfigPagination(Config):
+    page_size = ParamCreator.create_int(
+        help_string="What page size to use for pagination (max is 50)",
+        # if you set it for more than 50 it will be 50...
+        default=50,
+    )
+
+
+class ConfigPlaylist(Config):
+    name = ParamCreator.create_str(
+        help_string="What playlist to use",
+    )
+
+
+class ConfigPlaylists(Config):
+    names = ParamCreator.create_list_str(
+        help_string="What playlists to use",
+    )
+
+
+class ConfigDelete(Config):
+    doit = ParamCreator.create_bool(
+        help_string="Really delete?",
+        default=True,
+    )
