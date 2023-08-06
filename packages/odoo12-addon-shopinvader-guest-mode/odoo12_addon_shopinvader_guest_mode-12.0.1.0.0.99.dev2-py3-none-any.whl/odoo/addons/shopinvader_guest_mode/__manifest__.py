11370a9e49729d220bf117abe8322f33b0cf94f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package odoo12-addon-shopinvader-guest-mode/12.0.1.0.0.99.dev2/odoo12_addon_shopinvader_guest_mode-12.0.1.0.0.99.dev2-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader-guest-mode/odoo12_addon_shopinvader_guest_mode-12.0.1.0.0.99.dev2-py3-none-any.whl/odoo/addons/shopinvader_guest_mode/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Guest Mode",
+    "summary": """
+        Guest mode for Shopinvader""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Stable/Production",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader"],
+    "data": ["views/shopinvader_backend.xml", "views/shopinvader_partner.xml"],
+    "installable": True,
+}
