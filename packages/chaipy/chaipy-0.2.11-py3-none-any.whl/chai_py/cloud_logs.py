08d71c8a486f2337724d54c5bdfe77c665c203c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:35 +0000

    Add package chaipy/0.2.11/chaipy-0.2.11-py3-none-any.whl

---
+++ packages/chaipy/chaipy-0.2.11-py3-none-any.whl/chai_py/cloud_logs.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from typing import List
+
+import requests
+from typing_extensions import TypedDict
+
+from .auth import auth
+from .defaults import DEFAULT_BOT_LOGS_ENDPOINT
+
+
+class Log(TypedDict):
+    timestamp: int
+    severity: str
+    log: str
+
+
+def get_logs(bot_uid: str, errors: bool = False) -> List[Log]:
+    """Retrieves logs for specified bot.
+
+    Logs can only be pulled by the bot's developer.
+
+    :param bot_uid: Bot UID
+    :param errors: If True, only retrieves logs with severity: Error
+    :return:
+    """
+    r = requests.get(
+        DEFAULT_BOT_LOGS_ENDPOINT,
+        params={
+            'uid': auth.uid,
+            'key': auth.key,
+            'bot_uid': bot_uid,
+            'errors': True if errors else None
+        }
+    )
+    r.raise_for_status()
+    return r.json()['data']
+
+
+def display_logs(logs: List[Log]):
+    """Prints logs with clear separation.
+
+    :param logs: Logs retrieved with get_logs()
+    :return:
+    """
+    for log in logs:
+        timestamp = datetime.fromtimestamp(1617002881385 / 1000).isoformat(timespec='seconds')
+        print(f"===== LOG AT {timestamp} (SEVERITY: {log['severity']}) =====")
+        print(log['log'])
+        print("===== END LOG =====")
