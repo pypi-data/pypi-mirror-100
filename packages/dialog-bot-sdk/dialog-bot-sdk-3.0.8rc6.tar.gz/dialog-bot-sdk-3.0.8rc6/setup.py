0ca0f91f41448ffff8aa8036d49304414485b826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:47 +0000

    Add package dialog-bot-sdk/3.0.8rc6/dialog-bot-sdk-3.0.8rc6.tar.gz

---
+++ packages/dialog-bot-sdk/dialog-bot-sdk-3.0.8rc6.tar.gz/dialog-bot-sdk-3.0.8rc6/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dialog-bot-sdk",
+    version="3.0.8rc6",
+    author="Dmitry Borodkin, Dialog LLC",
+    author_email="d.borodkin@dlg.im",
+    description="Python Bot SDK for Dialog Messenger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dialogs/python-bot-sdk",
+    packages=setuptools.find_packages(),
+    license='Apache License 2.0',
+    keywords='dialog messenger bot sdk',
+    install_requires=[
+        'dialog_api==1.89.1',
+        'protobuf==3.15.6',
+        'grpcio==1.36.1',
+        'grpcio-tools==1.36.1',
+        'google-api-python-client==2.0.2',
+        'googleapis-common-protos==1.53.0',
+        'requests==2.25.1',
+        'pyopenssl==20.0.1',
+        'Pillow==8.1.2',
+    ],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
