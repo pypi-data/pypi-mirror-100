10fbaa9b066882c54ff0f7ae017be8736b75a9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:39 +0000

    Add package uofsc-calculus-labs/0.0/uofsc_calculus_labs-0.0.tar.gz

---
+++ packages/uofsc-calculus-labs/uofsc_calculus_labs-0.0.tar.gz/uofsc_calculus_labs-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='uofsc_calculus_labs',
+      version='0.0',
+      description='Functions for UofSC Calculus Labs',
+      packages=['uofsc_calculus_labs'],
+      author_email='cwarnock@math.sc.edu',
+      zip_safe=False)
