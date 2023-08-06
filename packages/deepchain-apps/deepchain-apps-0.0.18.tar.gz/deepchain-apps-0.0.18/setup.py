720b7b9994a040157e5dd8d2a4506002d25b037c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:33 +0000

    Add package deepchain-apps/0.0.18/deepchain-apps-0.0.18.tar.gz

---
+++ packages/deepchain-apps/deepchain-apps-0.0.18.tar.gz/deepchain-apps-0.0.18/setup.py
@@ -0,0 +1,45 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+from deepchainapps.version import VERSION
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+req = [
+    "numpy>=1.16",
+    "requests>=2.23.0",
+    "torch==1.5.0",
+    "tensorflow==2.3.0",
+    "fair-esm==0.3.1",
+]
+
+
+def make_install():
+    """main install function"""
+
+    setup_fn = setup(
+        name="deepchain-apps",
+        version=VERSION,
+        description="Define a personnal scorer for the user of DeepChain.bio",
+        author="Instadeep",
+        long_description=README,
+        long_description_content_type="text/markdown",
+        author_email="a.delfosse@instadeep.com",
+        packages=find_packages(),
+        entry_points={
+            "console_scripts": ["deepchain=deepchainapps.cli.deepchain:main"],
+        },
+        install_requires=req,
+        include_package_data=True,
+        zip_safe=False,
+        python_requires=">=3.7",
+    )
+
+    return setup_fn
+
+
+if __name__ == "__main__":
+    make_install()
