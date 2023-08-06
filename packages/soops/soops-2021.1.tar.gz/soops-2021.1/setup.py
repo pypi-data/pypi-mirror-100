89f7def8a00e0bef3277d0af5923aabbe77071be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:28 +0000

    Add package soops/2021.1/soops-2021.1.tar.gz

---
+++ packages/soops/soops-2021.1.tar.gz/soops-2021.1/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+This file is only required for editable installs, the project configuration
+is in setup.cfg.
+"""
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
