167286615f963aaafd3d2ca91449657fb3e772bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:35 +0000

    Add package fiveg-naas-api/1.2.3/fiveg_naas_api-1.2.3-py3-none-any.whl

---
+++ packages/fiveg-naas-api/fiveg_naas_api-1.2.3-py3-none-any.whl/src/fiveg_naas_api.py
@@ -0,0 +1,13 @@
+import logging
+
+network = "fiveg-edge-01-us"
+user = "geneleblanc"
+userFullname = "Gene LeBlanc"
+
+def deploy_slice(profile):
+    message = f'Test slice deployed on network {network} for user {userFullname}'
+
+
+    
+
+    return message
