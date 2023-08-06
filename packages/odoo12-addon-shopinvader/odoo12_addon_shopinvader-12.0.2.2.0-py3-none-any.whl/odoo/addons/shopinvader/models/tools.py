a37d7deee883591bdb9365932fcb4bf765772604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:46 +0000

    Add package odoo12-addon-shopinvader/12.0.2.2.0/odoo12_addon_shopinvader-12.0.2.2.0-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader/odoo12_addon_shopinvader-12.0.2.2.0-py3-none-any.whl/odoo/addons/shopinvader/models/tools.py
@@ -0,0 +1,26 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from unidecode import unidecode
+except ImportError:
+    _logger.debug("Cannot `import unidecode`.")
+
+try:
+    from slugify import slugify
+except ImportError:
+    _logger.debug("Cannot `import slugify`.")
+
+
+def sanitize_attr_name(attribute):
+    key = attribute.name
+    return unidecode(key.replace(" ", "_").lower())
+
+
+def _build_slugified_field_by_id(records, field_name):
+    return {r.id: slugify(r[field_name]) for r in records}
