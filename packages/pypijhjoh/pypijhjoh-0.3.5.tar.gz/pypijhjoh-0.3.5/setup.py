21d9c34306c016272def95bbc5d8917148751f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:05 +0000

    Add package pypijhjoh/0.3.5/pypijhjoh-0.3.5.tar.gz

---
+++ packages/pypijhjoh/pypijhjoh-0.3.5.tar.gz/pypijhjoh-0.3.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'pypijhjoh',          
+  version = '0.3.5',      
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository 
+  author = 'Joakim Johansen',
+  description="Test package by jhjoh",
+  long_description=long_description,
+  long_description_content_type="text/markdown",                   
+  author_email = 'jhjoh@equinor.com',         
+  keywords = ['TEST', 'PYPI'],   
+  install_requires=[            
+          'numpy',
+  ],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+  ],
+  packages=find_packages(),
+  python_requires=">=3.6",
+)
