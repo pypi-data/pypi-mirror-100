50d4e76c2bd5dea2e5dcfe3825633819de6265f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:53 +0000

    Add package chaipy/0.1.25/chaipy-0.1.25-py3-none-any.whl

---
+++ packages/chaipy/chaipy-0.1.25-py3-none-any.whl/chai_py/notebook_utils.py
@@ -0,0 +1,77 @@
+import builtins
+import functools
+
+from segno import QRCode
+
+
+def check_is_notebook() -> bool:
+    """Checks if the current environment is a Python notebook."""
+    return hasattr(builtins, "__IPYTHON__")
+
+
+def only_notebook(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if IS_NOTEBOOK:
+            return func(*args, **kwargs)
+        else:
+            raise RuntimeError(
+                "Attempting to run notebook utility in a non-notebook environment. "
+                "If you are not in a notebook environment, this is a bug - let us know how you ran into it!"
+            )
+
+    return wrapper
+
+
+@only_notebook
+def show_qr(qr: QRCode):
+    """Generates and displays a QR code."""
+    from IPython.display import display, SVG
+    return display(SVG(
+        qr.svg_inline(scale=10, light="#fff")
+    ))
+
+
+@only_notebook
+def register_write_and_run():
+    from IPython.core.magic import register_cell_magic
+    from importlib import reload
+    from pathlib import Path
+
+    @register_cell_magic
+    def write_and_run(line, cell):
+        """Saves the contents of the cell to a file and reloads the class.
+
+        Call with
+            %%write_and_run directory file_name class_name
+        E.g.
+          %%write_and_run bot bot.py Bot
+              will save the cell to bot/bot.py, and import the class Bot
+        """
+        directory, file_name, class_name = line.split()
+        file_path = Path(directory) / file_name
+        file_path.parent.mkdir(exist_ok=True)
+
+        print(f"Saving cell to {file_path} and reloading class {class_name}.")
+
+        # Save cell to file
+        with file_path.open('w') as f:
+            f.write(cell)
+
+        # Import and reload the bot class.
+        exec(f"import {directory}.{file_path.stem}")
+        eval(f"reload({directory}.{file_path.stem})")
+        exec(f"global {class_name}; {class_name} = {directory}.{file_path.stem}.{class_name}")
+
+
+IS_NOTEBOOK = check_is_notebook()
+if IS_NOTEBOOK:
+    try:
+        import nest_asyncio
+
+        nest_asyncio.apply()
+    except Exception as e:
+        raise RuntimeError(
+            "Error setting up nest_asyncio. If you are in a notebook, use `pip install chaipy[notebook]`. "
+            "If not, this is a bug - let us know how you ran into it!"
+        )
