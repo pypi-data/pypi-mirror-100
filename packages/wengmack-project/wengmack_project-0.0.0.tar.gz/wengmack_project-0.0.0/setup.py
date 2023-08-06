472ae2ab024c4441f01b0587a4fd9bd051777f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:18 +0000

    Add package wengmack-project/0.0.0/wengmack_project-0.0.0.tar.gz

---
+++ packages/wengmack-project/wengmack_project-0.0.0.tar.gz/wengmack_project-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "wengmack_project",
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['wengmack_project']
+)
