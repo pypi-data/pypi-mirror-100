25b8418ceb8ccf3855875712c970e37f3119e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:18 +0000

    Add package roadelou-compass/0.0.8/roadelou_compass-0.0.8-py3-none-any.whl

---
+++ packages/roadelou-compass/roadelou_compass-0.0.8-py3-none-any.whl/compass/ast/binary/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+################################### METADATA ###################################
+
+# Contributors: roadelou
+# Contacts:
+# Creation Date: 2021-03-14
+# Language: Python3
+
+################################### IMPORTS ####################################
+
+# Standard library
+# Your imports from the standard library go here
+
+
+# External imports
+# Your imports from other packages go here
+
+
+# Internal imports
+from compass.ast.binary.binary import Binary
+from compass.ast.binary.and_op import AndOp
+from compass.ast.binary.div_op import DivOp
+from compass.ast.binary.minus_op import MinusOp
+from compass.ast.binary.mod_op import ModOp
+from compass.ast.binary.or_op import OrOp
+from compass.ast.binary.plus_op import PlusOp
+from compass.ast.binary.testeq_op import TestEqOp
+from compass.ast.binary.times_op import TimesOp
+from compass.ast.binary.xor_op import XorOp
+from compass.ast.binary.lshift_op import LShiftOp
+from compass.ast.binary.rshift_op import RShiftOp
+from compass.ast.binary.noteq_op import NotEqOp
+from compass.ast.binary.lowereq_op import LowerEqOp
+from compass.ast.binary.highereq_op import HigherEqOp
+from compass.ast.binary.lowerneq_op import LowerNotEqOp
+from compass.ast.binary.higherneq_op import HigherNotEqOp
+from compass.ast.binary.bitand_op import BitAndOp
+from compass.ast.binary.bitor_op import BitOrOp
+
+################################### CLASSES ####################################
+
+# Your classes go here
+
+################################## FUNCTIONS ###################################
+
+# Your functions go here
+
+##################################### MAIN #####################################
+
+if __name__ == "__main__":
+    # The code to run when this file is used as a script goes here
+    pass
+
+##################################### EOF ######################################
