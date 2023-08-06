094b205d925395f8f26e70082a84ec4a550bb2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:50 +0000

    Add package olxbrasil/0.2.3/olxbrasil-0.2.3-py3-none-any.whl

---
+++ packages/olxbrasil/olxbrasil-0.2.3-py3-none-any.whl/olxbrasil/parsers/__init__.py
@@ -0,0 +1,5 @@
+from olxbrasil.parsers.car_parser import CarParser
+from olxbrasil.parsers.item_parser import ItemParser
+from olxbrasil.parsers.list_parser import ListParser
+
+__all__ = ("CarParser", "ItemParser", "ListParser")
