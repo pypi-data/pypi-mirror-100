40b009ff0b8c39e29b8071242c9194cf5ce25fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:49 +0000

    Add package ldict/0.210330.2/ldict-0.210330.2.tar.gz

---
+++ packages/ldict/ldict-0.210330.2.tar.gz/ldict-0.210330.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ldict']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['garoupa>=1.210328.3,<2.0.0', 'orjson>=3.5.0,<4.0.0', 'pylint>=2.7.4,<3.0.0']
+
+setup_kwargs = {
+    'name': 'ldict',
+    'version': '0.210330.2',
+    'description': 'Uniquely identified lazy dict',
+    'long_description': None,
+    'author': 'davips',
+    'author_email': 'dpsabc@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
