c54ee634188a879f9d0fa5c8a0870512057b6828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:33 +0000

    Add package vans-eightball/0.0.1/vans-eightball-0.0.1.tar.gz

---
+++ packages/vans-eightball/vans-eightball-0.0.1.tar.gz/vans-eightball-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="vans-eightball",
+    version="0.0.1",
+    description="Provides wise answers to tough problems",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bluedenim/vans-eightball",
+    author="Van Ly",
+    author_email="vancly@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["v8ball"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "v8ball=v8ball.van.eightball:get_answer",
+        ]
+    },
+)
