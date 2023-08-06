b7e0edd316336e43917c676a6cf3601209d358b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:32 +0000

    Add package zstatspython/1.0/zstatspython-1.0.tar.gz

---
+++ packages/zstatspython/zstatspython-1.0.tar.gz/zstatspython-1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zstatspython",
+    version="1.0",
+    author="Astrogamer54",
+    author_email="mail@astrogamer54.com",
+    description="Get Player Data From zstats plugin. Not An Official Plugin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Astrogamer54/zstatspython",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    	'mysql-connector-python',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
