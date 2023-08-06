8ff01b61fba2a9d1d10eaefcd44f1e389af0dcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package Wordbuilds/0.0.2/Wordbuilds-0.0.2-py3-none-any.whl

---
+++ packages/Wordbuilds/Wordbuilds-0.0.2-py3-none-any.whl/Base/builder.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+def build(array, arr):
+    return "".join(array[i] for i in arr)
+
+def Roll():
+    webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+
+
