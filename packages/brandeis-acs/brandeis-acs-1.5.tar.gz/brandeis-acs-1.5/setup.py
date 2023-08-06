b994e5d8fcf7e8d3e42186c92dc31d13e92065cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:11 +0000

    Add package brandeis-acs/1.5/brandeis-acs-1.5.tar.gz

---
+++ packages/brandeis-acs/brandeis-acs-1.5.tar.gz/brandeis-acs-1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('requirements.txt') as requirements:
+    requires = requirements.readlines()
+
+setup(
+    name='brandeis-acs',
+    version='1.5',
+    packages=['bacs'],
+    url='https://github.com/brandeis-llc/acoustic-classification-segmentation',
+    license='MIT',
+    author='Keigh Rim',
+    author_email='krim@brandeis.edu',
+    description='Brandeis Acoustic Classification & Segmentation tool',
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'bacs = bacs.run:main'
+        ]
+    },
+    package_data={
+        'bacs': ['defmodel/*', 'defmodel/**/*']
+    },
+
+)
