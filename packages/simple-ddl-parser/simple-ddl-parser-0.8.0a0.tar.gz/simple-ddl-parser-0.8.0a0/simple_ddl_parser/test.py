52774767f3d9a47a736e411fa81b03df8c278085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:34 +0000

    Add package simple-ddl-parser/0.8.0a0/simple-ddl-parser-0.8.0a0.tar.gz

---
+++ packages/simple-ddl-parser/simple-ddl-parser-0.8.0a0.tar.gz/simple-ddl-parser-0.8.0a0/simple_ddl_parser/test.py
@@ -0,0 +1,23 @@
+from simple_ddl_parser import DDLParser
+
+
+ddl = """
+CREATE EXTERNAL TABLE IF NOT EXISTS database.table_name
+(
+    day_long_nm     string,
+    calendar_dt     date,
+    source_batch_id string,
+    field_qty       decimal(10, 0),
+    field_bool      boolean,
+    field_float     float,
+    create_tmst     timestamp,
+    field_double    double,
+    field_long      bigint
+) PARTITIONED BY (batch_id int) STORED AS PARQUET LOCATION 's3://datalake/table_name/v1'
+
+"""
+
+
+result = DDLParser(ddl).run()
+
+print(result)
