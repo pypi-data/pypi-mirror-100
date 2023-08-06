cb17529eca01acdf63f079d5cf65f815ac343a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:10 +0000

    Add package rci-py-client/1.0.1/rci_py_client-1.0.1-py3-none-any.whl

---
+++ packages/rci-py-client/rci_py_client-1.0.1-py3-none-any.whl/rci/Client.py
@@ -0,0 +1,16 @@
+import http.client
+from rci.Exceptions import RciError
+
+class Client:
+  def __init__(self, address):
+    self.conn = http.client.HTTPConnection(address)
+
+  def getTasks(self):
+    self.conn.request('POST', '/PollTasksImage')
+    resp = self.conn.getresponse()
+    if resp.status != 200:
+      raise RciNetworkError()
+    return = resp.read()
+
+  def close(self):
+    self.conn.close()
