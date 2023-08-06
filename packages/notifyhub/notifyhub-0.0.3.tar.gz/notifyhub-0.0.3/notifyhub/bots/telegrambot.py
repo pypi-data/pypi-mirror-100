d9484bff0a378ff87670856545dad02957a35b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:35 +0000

    Add package notifyhub/0.0.3/notifyhub-0.0.3.tar.gz

---
+++ packages/notifyhub/notifyhub-0.0.3.tar.gz/notifyhub-0.0.3/notifyhub/bots/telegrambot.py
@@ -0,0 +1,20 @@
+import platform
+import requests
+
+CHAT_ID = '238741623'
+BOT_TOKEN = '1386719865:AAG1pim7Di8pUOJYOgh_tUMLGTLI2BPHk9Q'
+
+
+def send(message: str, chat_id: int, bot_token: str):
+    payload = {
+        'chat_id': chat_id,
+        'text': message + '\nPC: {}'.format(platform.node()),
+        'parse_mode': 'HTML'
+    }
+    rtn = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(bot_token),
+                        data=payload)
+    return rtn
+
+
+if __name__ == '__main__':
+    send('hello', CHAT_ID, BOT_TOKEN)
