a67b5af8e4c07e0f5169fb30293cea92a7383c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:52 +0000

    Add package zjk-try/0.0.1/zjk_try-0.0.1-py3-none-any.whl

---
+++ packages/zjk-try/zjk_try-0.0.1-py3-none-any.whl/zjk_try/zjk_try.py
@@ -0,0 +1,12 @@
+def zjk_try_output():
+	print("This is a girl-friend seeking notice! ")
+	print("I'm zjk!")
+	print("I'm 21!")
+	print("I'm in Beijing now!")
+	print("Try to contact me quickly!")
+	print("Qucikly!Qucikly!Qucikly!")
+	print("Or give me your address and I will seek you!")
+	print("Over!")
+
+if __name__ == '__main__':
+	zjk_try_output()
