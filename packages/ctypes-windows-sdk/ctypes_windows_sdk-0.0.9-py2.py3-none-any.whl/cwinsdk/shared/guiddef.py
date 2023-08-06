2f70886f32f4b1b298fcb2a3559e0305bb3af628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:02 +0000

    Add package ctypes-windows-sdk/0.0.9/ctypes_windows_sdk-0.0.9-py2.py3-none-any.whl

---
+++ packages/ctypes-windows-sdk/ctypes_windows_sdk-0.0.9-py2.py3-none-any.whl/cwinsdk/shared/guiddef.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from ctypes import POINTER, Structure, Union, c_ubyte, c_ulong, c_ushort
+
+
+class GUID(Structure):
+	_fields_ = [
+		("Data1", c_ulong),
+		("Data2", c_ushort),
+		("Data3", c_ushort),
+		("Data4", c_ubyte*8),
+	]
+
+IID = GUID
+LPIID = POINTER(IID)
+CLSID = GUID
+LPCLSID = POINTER(CLSID)
+FMTID = GUID
+LPFMTID = POINTER(FMTID)
+REFCLSID = POINTER(IID) # const POINTER
+REFIID = POINTER(IID) # const POINTER
---
+++ packages/ctypes-windows-sdk/ctypes_windows_sdk-0.0.9-py2.py3-none-any.whl/cwinsdk/um/Unknwn.py
@@ -0,0 +1,7 @@
+from ctypes import POINTER, Structure
+
+
+class IUnknown(Structure):
+	pass
+
+LPUNKNOWN = POINTER(IUnknown)
