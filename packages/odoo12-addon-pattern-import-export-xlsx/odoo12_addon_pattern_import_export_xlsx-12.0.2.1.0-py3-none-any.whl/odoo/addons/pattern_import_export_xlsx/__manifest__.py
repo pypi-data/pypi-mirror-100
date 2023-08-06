30ee653e4e0f889bcdd1a783f972a052d5be21e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:39 +0000

    Add package odoo12-addon-pattern-import-export-xlsx/12.0.2.1.0/odoo12_addon_pattern_import_export_xlsx-12.0.2.1.0-py3-none-any.whl

---
+++ packages/odoo12-addon-pattern-import-export-xlsx/odoo12_addon_pattern_import_export_xlsx-12.0.2.1.0-py3-none-any.whl/odoo/addons/pattern_import_export_xlsx/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Pattern Import Export XLSX",
+    "summary": "Pattern for import or export from to XLSX files",
+    "version": "12.0.2.1.0",
+    "category": "Extra Tools",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "license": "AGPL-3",
+    "depends": ["pattern_import_export"],
+    "external_dependencies": {"python": ["openpyxl"]},
+    "demo": ["demo/demo.xml"],
+    "data": ["views/ir_exports.xml"],
+    "installable": True,
+}
