c79dea102b70d483c0c0132f09410bfb21406452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:44 +0000

    Add package genCode/0.0.16/genCode-0.0.16.tar.gz

---
+++ packages/genCode/genCode-0.0.16.tar.gz/genCode-0.0.16/gencode/models.py
@@ -0,0 +1,37 @@
+class Table:
+    comment: str
+    xx_table_name: str
+    XX_TABLE_NAME: str
+    XxTableName: str
+    xxTableName: str
+    xxtablename: str
+    XXTABLENAME: str
+    table_name: str
+    TABLE_NAME: str
+    tablename: str
+    TABLENAME: str
+    tableName: str
+    TableName: str
+
+    columns: []
+
+
+class Column:
+    comment: str
+    column_name: str
+    Column_Name: str
+    COLUMN_NAME: str
+    ColumnName: str
+    columnname: str
+    COLUMNNAME: str
+    dataType: str
+    data_length: int
+
+
+class Gcode:
+    is_dir = False
+    temp_path: str
+    temp_content: str
+    target_path: str
+    target_content: str
+    encode: str
