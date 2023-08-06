0629d425d4c3a7213b79eb306cb4435319c0338a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:45 +0000

    Add package firedantic/0.2.1/firedantic-0.2.1-py3-none-any.whl

---
+++ packages/firedantic/firedantic-0.2.1-py3-none-any.whl/firedantic/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from firedantic._async.model import AsyncModel
+from firedantic._sync.model import Model
+from firedantic.configurations import CONFIGURATIONS, configure
+from firedantic.exceptions import *
