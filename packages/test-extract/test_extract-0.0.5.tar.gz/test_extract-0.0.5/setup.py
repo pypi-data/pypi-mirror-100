d9a8f50e33f2c59ba2bb4dbe06f20f6c7977dcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package test-extract/0.0.5/test_extract-0.0.5.tar.gz

---
+++ packages/test-extract/test_extract-0.0.5.tar.gz/test_extract-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import io
+
+with io.open(r"README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_extract", # Replace with your own username
+    version="0.0.5",
+    author="yan2164",
+    author_email="yanuariusbasilius@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=2.7",
+)
