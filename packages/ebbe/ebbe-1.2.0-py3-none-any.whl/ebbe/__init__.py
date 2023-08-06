a2ad856fb5111bab47328c2a8be53c26e67ecc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:59 +0000

    Add package ebbe/1.2.0/ebbe-1.2.0-py3-none-any.whl

---
+++ packages/ebbe/ebbe-1.2.0-py3-none-any.whl/ebbe/__init__.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# Ebbe Library Enpoint
+# =============================================================================
+#
+from ebbe.functions import (
+    as_chunks,
+    fail_fast,
+    uniq,
+    with_prev,
+    with_prev_and_next,
+    with_next,
+    with_is_first,
+    with_is_last,
+    without_first
+)
