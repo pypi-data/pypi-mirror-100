fcc6b337d582a7ca7164ac5126d0d456ce3897bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:50 +0000

    Add package fsak/0.1.0/fsak-0.1.0.tar.gz

---
+++ packages/fsak/fsak-0.1.0.tar.gz/fsak-0.1.0/fsak/fsak.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+class fsak:
+    def __init__(self):
+        self.dt = datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=6)))
+
+    def help(self):
+        return f"{str(self.dt)}"
