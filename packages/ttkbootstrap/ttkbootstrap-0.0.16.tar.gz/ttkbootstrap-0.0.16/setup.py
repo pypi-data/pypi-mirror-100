07957c629fc26a107ef5e275d5c0fbd0076e1f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:01 +0000

    Add package ttkbootstrap/0.0.16/ttkbootstrap-0.0.16.tar.gz

---
+++ packages/ttkbootstrap/ttkbootstrap-0.0.16.tar.gz/ttkbootstrap-0.0.16/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ttkbootstrap",
+    version="0.0.16",
+    author="Israel Dryer",
+    author_email="israel.dryer@gmail.com",
+    description="A collection of modern ttk themes inspired by Bootstrap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/israel-dryer/ttkbootstrap",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={"": ["*.json"]},
+    include_package_data=True,
+    install_requires=["pillow"],
+    python_requires=">=3.6",
+)
