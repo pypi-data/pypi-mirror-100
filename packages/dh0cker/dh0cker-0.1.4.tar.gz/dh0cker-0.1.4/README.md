b1be4f11ec8a3f0bb1cb9292be4da8cb0a2c52f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:02 +0000

    Add package dh0cker/0.1.4/dh0cker-0.1.4.tar.gz

---
+++ packages/dh0cker/dh0cker-0.1.4.tar.gz/dh0cker-0.1.4/README.md
@@ -0,0 +1,3 @@
+# dh0cker
+
+example showing how you shouldn't trust 3rd-party packages
---
+++ packages/dh0cker/dh0cker-0.1.4.tar.gz/dh0cker-0.1.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dh0cker",
+    version="0.1.4",
+    author="dh0ck",
+    author_email="dh0ck666@gmail.com",
+    description="test package",
+    long_description="Mystiko{D0nT_7rU$T_$tR@n63Rs}",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires  = [], # List all your dependencies inside the list
+    license = 'MIT'
+)
