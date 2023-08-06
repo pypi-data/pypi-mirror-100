04c21d5e78668eeef834c7372333516f2eda2d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:44 +0000

    Add package deltalanguage/0.6.0/deltalanguage-0.6.0.tar.gz

---
+++ packages/deltalanguage/deltalanguage-0.6.0.tar.gz/deltalanguage-0.6.0/deltalanguage/test/execution/test_performance.py
@@ -0,0 +1,26 @@
+"""Collection of graphs, which we use to track performance of
+runtimes and runtime simulators.
+
+Note that performance benchmarks depend on hardware, thus if any of processing
+units is updated the performance might benchmarks should too.
+
+Proposed topics:
+
+- time as metrics (MOVE from test_blocking.py)
+  - speed tests with a lot of messages
+  - how queue blocking work
+
+- number of messages as metrics
+  - we don't have them yet
+"""
+
+import unittest
+
+import deltalanguage as dl
+
+from deltalanguage.test.execution.base import TestExecutionBaseDL, PYSIMULATOR
+
+
+class TestExecutionPerformance(TestExecutionBaseDL):
+
+    pass
