4049878583d822f23ebc6da7a6633e4a3bff3219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:39 +0000

    Add package ar-corrector/1.0.8a0/ar_corrector-1.0.8a0.tar.gz

---
+++ packages/ar-corrector/ar_corrector-1.0.8a0.tar.gz/ar_corrector-1.0.8a0/ar_corrector/preprocess.py
@@ -0,0 +1,39 @@
+#%%
+
+import re
+from collections import Counter
+from ar_corrector.io_handler import *
+from ar_corrector.config import *
+
+class Preprocessor:
+    def __init__(self):
+        pass
+    def clean(self, txt):
+        res = re.sub(r'[^\w\s]+', '', txt)
+        res = re.sub(r'[\d_\-]+', ' ', res)
+        res = re.sub(r'\s+', ' ', res)
+        res = re.sub(r'[^ابتةثجحخدذرزسشصضطظعغفقكلمنهويءآأؤإئ\s]+', '', res)
+        res = re.sub(r'\s+', ' ', res)
+        return res.strip()
+
+    def tokenize(self, txt):
+        return txt.split()
+    
+    def __call__(self, txt):
+        ctxt = self.clean(txt)
+        txt_arr = self.tokenize(ctxt)
+        word_counts = Counter(txt_arr)
+        
+        save_dict_file(config['vocabs_dict'], dict(word_counts))
+
+    def get_letters(self, txt):
+        res = self.clean(txt)
+        chars = set(res)
+        return chars
+    
+if __name__ == '__main__':
+    text = read_txt_file('../data/TXT_No_header.txt')
+    preprocessor = Preprocessor()
+    preprocessor(text)
+
+# %%
