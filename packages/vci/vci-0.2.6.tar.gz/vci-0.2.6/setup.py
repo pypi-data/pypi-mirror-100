0a0e2376c9d3909be5f008b67f308392316f85b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:07 +0000

    Add package vci/0.2.6/vci-0.2.6.tar.gz

---
+++ packages/vci/vci-0.2.6.tar.gz/vci-0.2.6/setup.py
@@ -0,0 +1,40 @@
+
+import sys
+from setuptools import find_packages
+from setuptools import setup
+
+extras_require = {
+    'packaging': ['stdeb', 'twine']
+}
+
+setup(
+    name='vci',
+    version="0.2.6",
+    packages=find_packages(exclude=['tests*', 'docs*']),
+    # check into catkin_tools/ckx_tools for a smarter, but complicated method
+    author='Daniel Stonier',
+    author_email='d.stonier@gmail.com',
+    maintainer='Daniel Stonier',
+    maintainer_email='d.stonier@gmail.com',
+    url='http://github.com/stonier/vci',
+    keywords=['vcs'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Utilities'
+    ],
+    description="version control management from an index of listed workspaces",
+    long_description="Version control index handling from a yaml file on the internet (e.g. github).",
+    license='BSD',
+    # test_suite='tests',
+    install_requires=["distro", "pyyaml"],
+    extras_require=extras_require,
+    entry_points={
+        'console_scripts': [
+            'vci = vci:main',
+        ],
+    },
+)
