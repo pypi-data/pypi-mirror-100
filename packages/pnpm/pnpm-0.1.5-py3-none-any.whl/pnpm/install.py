8766a708e9306a6c65b623bda7fc5d12119b59da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:57 +0000

    Add package pnpm/0.1.5/pnpm-0.1.5-py3-none-any.whl

---
+++ packages/pnpm/pnpm-0.1.5-py3-none-any.whl/pnpm/install.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import site
+from rich import print
+import os
+import subprocess as sp
+
+# helpers
+def _params(length=3):
+    return " ".join([f"echo ${i}" for i in range(1, length)])
+
+def alias(name, executable, _params_length):
+    return "alias " + name + "='() { " + executable + " " + _params(3) + "}'"
+
+
+def install():
+    print("Installing pnpm...")
+    PATH = site.getsitepackages()[0] + '/pnpm'
+
+    if 'zsh' in os.environ.get("SHELL", ""):
+        print('Installing with zsh')
+        with open(Path.home() / ".zshrc", 'r') as f:
+            if "alias pnpm" in f.read():
+                return print('pnpm is already installed!')
+            
+        with open(Path.home() / ".zshrc", 'a') as f:
+            path = 'test'
+            executable = f"python3 {PATH+'/generator/generate'}"
+            f.write(alias('pnpm', executable, 5))
+            
