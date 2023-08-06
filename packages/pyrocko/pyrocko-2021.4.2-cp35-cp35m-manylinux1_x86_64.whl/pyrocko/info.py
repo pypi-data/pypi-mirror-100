c493e0cb693d137c897d5409642b3551cc85c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:25 +0000

    Add package pyrocko/2021.4.2/pyrocko-2021.4.2-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2021.4.2-cp35-cp35m-manylinux1_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '4971d5e5ac3c4ac4b75992452a934e01a784ea8d'
+local_modifications = True
+version = '2021.04.02'
+long_version = 'pyrocko-2021.04.02-4971d5e5ac3c4ac4b75992452a934e01a784ea8d-modified-2021-04-02_09:15:37'  # noqa
+installed_date = '2021-04-02_09:15:37'
