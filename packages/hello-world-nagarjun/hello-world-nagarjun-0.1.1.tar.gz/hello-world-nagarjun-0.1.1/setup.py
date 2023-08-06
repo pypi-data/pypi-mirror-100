b565dc5b23406f1fe427ab402d99dbd3c7c57af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:49 +0000

    Add package hello-world-nagarjun/0.1.1/hello-world-nagarjun-0.1.1.tar.gz

---
+++ packages/hello-world-nagarjun/hello-world-nagarjun-0.1.1.tar.gz/hello-world-nagarjun-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setup(
+
+	name = "hello-world-nagarjun",
+	version = "0.1.1",
+	py_modules =["hello_world"],
+	package_dir = {'': 'src'},
+	python_requires = ">=3.6.0",
+	install_requires = ["numpy"],
+	author = "Nagarjun",
+	description='A sample Python project',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	license='MIT',
+	)
