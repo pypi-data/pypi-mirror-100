7dade8d1b07b6b41c4588da6f76546876a4563e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package vistorch/0.0.1.dev1/vistorch-0.0.1.dev1.tar.gz

---
+++ packages/vistorch/vistorch-0.0.1.dev1.tar.gz/vistorch-0.0.1.dev1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup,find_packages
+setup(name = 'vistorch',
+      version = '0.0.1.dev1',
+      description = 'use visdom on pytorch',
+      author = 'xi wei',
+      author_email = 'xiwei.cv@gmail.com',
+      license = 'BSD',
+      keywords = 'visdom torch',
+      packages = ['vistorch']
+      )
