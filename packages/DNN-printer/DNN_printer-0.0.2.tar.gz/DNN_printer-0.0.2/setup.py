40ed9b662b9601b268bae5a0d9c8126f961a8305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:34 +0000

    Add package DNN-printer/0.0.2/DNN_printer-0.0.2.tar.gz

---
+++ packages/DNN-printer/DNN_printer-0.0.2.tar.gz/DNN_printer-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='DNN_printer',
+    version='0.0.2',
+    author='CPing',
+    author_email='635897373@qq.com',
+    url='',
+    description=u'Print feature map and weight size of PyTorch models.',
+    long_description_content_type="text/markdown",
+    long_description = long_description,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
