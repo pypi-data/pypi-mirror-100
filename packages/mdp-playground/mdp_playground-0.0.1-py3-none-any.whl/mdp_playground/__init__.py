fecb335c0829a58e3ab42acf01e5046c401e0b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:39 +0000

    Add package mdp-playground/0.0.1/mdp_playground-0.0.1-py3-none-any.whl

---
+++ packages/mdp-playground/mdp_playground-0.0.1-py3-none-any.whl/mdp_playground/__init__.py
@@ -0,0 +1,12 @@
+from gym.envs.registration import register
+
+register(
+    id='RLToy-v0',
+    entry_point='mdp_playground.envs:RLToyEnv',
+)
+
+register(
+    id='RLToyFiniteHorizon-v0',
+    entry_point='mdp_playground.envs:RLToyEnv',
+    max_episode_steps=100,
+)
