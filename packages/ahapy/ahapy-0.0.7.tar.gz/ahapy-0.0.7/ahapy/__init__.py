145bf3f057e18fafdca194bffa7e1d18c27f30dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:48 +0000

    Add package ahapy/0.0.7/ahapy-0.0.7.tar.gz

---
+++ packages/ahapy/ahapy-0.0.7.tar.gz/ahapy-0.0.7/ahapy/__init__.py
@@ -0,0 +1,90 @@
+import requests
+import re
+
+
+class BaseAPI(object):
+    def __init__(self, sub_domain, api_key):
+        self.sub_domain = sub_domain 
+        self.api_key = api_key
+
+        self.url = 'https://{}.aha.io/api'.format(self.sub_domain)
+
+        self.session = requests.Session()
+        self.session.headers.update({'Authorization': 'Bearer ' + api_key})
+
+
+
+class AhaV1(BaseAPI):
+    def __init__(self, sub_domain, api_key):
+        self.sub_domain = sub_domain 
+        self.api_key = api_key
+        self.version = '/v1/'
+        self.page = 1
+        self.total_pages = 0
+        self.count = 0
+        self.endpoint = ''
+
+        super(AhaV1, self).__init__(sub_domain, api_key)
+
+
+    def _count(self, endpoint, **options):
+        url = self.url + self.version + endpoint
+        response = self.session.get(url, params=options)
+        r = response.json()
+        self.count = r['pagination']['total_records']
+        return self.count
+    
+
+    def _parse_endpoint_arg(self, endpoint_arg):
+        s = re.split('/', endpoint_arg)
+        if re.match('[a-z]', s[-1]):
+            return s[-1]
+        else:
+            return s[-2]
+
+
+    def _is_paginated(self, json_response):
+        if 'pagination' in json_response.keys():
+            return True
+        else:
+            return False
+
+
+    def query(self, endpoint, **options):
+        """
+        Query for Aha.io V1 endpoint
+        """
+        self.endpoint = self._parse_endpoint_arg(endpoint)
+
+        while True:
+            url = self.url + self.version + endpoint
+            if self.page <= self.total_pages:
+                url = url + '?page=' + str(self.page)
+                response = self.session.get(url, params=options)
+            else:
+                response = self.session.get(url, params=options)
+                r = response.json()
+                if self._is_paginated(r):
+                    self.count = r['pagination']['total_records']
+
+            if not response.ok:
+                print("Response status code: " + str(response.status_code))
+                print("Response text: " + response.text)
+            
+            r = response.json()
+            if self._is_paginated(r):
+                self.total_pages = r['pagination']['total_pages']
+                self.page = r['pagination']['current_page'] + 1
+            else:
+                # trim pluralization from endpoint as this is 
+                # singluar in response body if no pagination
+                # this is used as a check later to make sure 
+                # response body matches expectations
+                self.endpoint = self.endpoint[:-1]
+
+            if self.endpoint in r.keys():
+                for i in r[self.endpoint]:
+                    yield i
+            
+            if self.page > self.total_pages:
+                break
---
+++ packages/ahapy/ahapy-0.0.7.tar.gz/ahapy-0.0.7/test/test_client.py
@@ -0,0 +1,68 @@
+import os
+import re
+import math
+import json
+import unittest
+from ahapy import AhaV1
+
+# To run tests, cd into package directory and the run the following:
+# python -m unittest
+
+AHA_KEY = os.environ.get('AHA_API_KEY')
+
+class TestClient(unittest.TestCase):
+
+    def test_v1_query(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        data = aha.query('initiatives')
+        self.assertIsNotNone(data)
+
+
+    def test_v1_count(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        count = aha._count('epics')
+        self.assertGreater(count, -1)
+
+
+    def test_v1_fields(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        data = aha.query('initiatives', fields='name,id')
+        for i in data:
+            self.assertIsNotNone(i['name'])
+
+
+    def test_v1_page(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        aha.query('initiatives', per_page=200)
+        expected_pages = math.ceil(aha.count / 200)
+        self.assertEqual(expected_pages, aha.total_pages)
+
+
+    def test_v1_endpoint_parse_1(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        endpoint = aha._parse_endpoint_arg('initiatives/12345/epics')
+        self.assertEqual('epics', endpoint)
+
+
+    def test_v1_endpoint_parse_2(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        endpoint = aha._parse_endpoint_arg('initiatives/12345')
+        self.assertEqual('initiatives', endpoint)
+
+
+    def test_v1_endpoint_parse_3(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        endpoint = aha._parse_endpoint_arg('initiatives')
+        self.assertEqual('initiatives', endpoint)
+
+
+    def test_v1_is_paginated_true(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        mock = {"pagination":"yes"}
+        self.assertTrue(aha._is_paginated(mock))
+    
+
+    def test_v1_is_paginated_false(self):
+        aha = AhaV1('enverus', AHA_KEY)
+        mock = {"no_pagination":"no"}
+        self.assertFalse(aha._is_paginated(mock))
