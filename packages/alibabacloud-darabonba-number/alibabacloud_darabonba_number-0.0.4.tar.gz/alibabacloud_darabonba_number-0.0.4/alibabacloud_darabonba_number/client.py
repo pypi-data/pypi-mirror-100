9194575fb407ae6fe54261e3e30eb1a4c153b20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:56 +0000

    Add package alibabacloud-darabonba-number/0.0.4/alibabacloud_darabonba_number-0.0.4.tar.gz

---
+++ packages/alibabacloud-darabonba-number/alibabacloud_darabonba_number-0.0.4.tar.gz/alibabacloud_darabonba_number-0.0.4/alibabacloud_darabonba_number/client.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+
+
+class Client:
+    """
+    This is a number module
+    """
+
+    @staticmethod
+    def parse_int(
+            raw: str,
+    ) -> int:
+        return int(raw)
+
+    @staticmethod
+    def parse_long(
+            raw: str,
+    ) -> int:
+        return int(raw)
+
+    @staticmethod
+    def parse_float(
+            raw: str,
+    ) -> float:
+        return float(raw)
+
+    @staticmethod
+    def parse_double(
+            raw: str,
+    ) -> float:
+        return float(raw)
+
+    @staticmethod
+    def itol(
+        val: int,
+    ) -> int:
+        return val
+
+    @staticmethod
+    def ltoi(
+        val: int,
+    ) -> int:
+        return val
+
+    @staticmethod
+    def add(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 + val2
+
+    @staticmethod
+    def sub(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 - val2
+
+    @staticmethod
+    def mul(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 * val2
+
+    @staticmethod
+    def div(
+        val1: int,
+        val2: int,
+    ) -> float:
+        return val1 / val2
+
+    @staticmethod
+    def gt(
+        val1: int,
+        val2: int,
+    ) -> bool:
+        return val1 > val2
+
+    @staticmethod
+    def gte(
+        val1: int,
+        val2: int,
+    ) -> bool:
+        return val1 >= val2
+
+    @staticmethod
+    def lt(
+            val1: int,
+            val2: int,
+    ) -> bool:
+        return val1 < val2
+
+    @staticmethod
+    def lte(
+            val1: int,
+            val2: int,
+    ) -> bool:
+        return val1 <= val2
