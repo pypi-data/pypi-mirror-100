477a71dafbe302aeb2ffdd7981fc0b826b015955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:04 +0000

    Add package vpstool/0.0.4/vpstool-0.0.4.tar.gz

---
+++ packages/vpstool/vpstool-0.0.4.tar.gz/vpstool-0.0.4/vps/argparse_helper.py
@@ -0,0 +1,42 @@
+from dofast.simple_parser import SimpleParser
+from dofast.oss import Bucket, Message
+import dofast.utils as du
+
+from .toolkits.telegram import read_hema_bot, download_file_by_id, bot_messalert
+from .network import Network, PapaPhone
+from .linuxtools import LinuxTools
+
+msg = """VPS toolkits 😏
+-m(--monitor) ip:port ::: Monitor ip:port proxy
+-ma(--messalert) message ::: Bot MessAlert. 
+-k(--kill) process ::: Kill linux process.
+-p(--phone) ::: Papa phone monitor.
+-socks [port] ::: Set up a socks5 proxy.
+"""
+
+
+def parse_arguments():
+    sp = SimpleParser()
+    sp.add_argument('-m', '--monitor')
+    sp.add_argument('-k', '--kill')
+    sp.add_argument('-p', '--phone')
+    sp.add_argument('-socks', '--socks')
+    sp.parse_args()
+
+    if sp.monitor:
+        Network().monitor_proxy(sp.monitor.value)
+
+    elif sp.kill:
+        LinuxTools().pskill(sp.kill.value)
+
+    elif sp.phone:
+        PapaPhone().entry()
+
+    elif sp.socks:
+        port = sp.socks.value
+        if port == 'PLACEHOLDER': port = 8888
+        Network.setup_socks5_proxy(port)
+    else:
+        for l in msg.split("\n"):
+            c, e = (l + " ::: ").split(':::')[:2]
+            print("{:<70} {:<20}".format(c, e))
