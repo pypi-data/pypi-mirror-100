6879375b3f6841778ced33aa474524afacf19c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:24 +0000

    Add package tap-shopify-scentbird/1.2.12/tap-shopify-scentbird-1.2.12.tar.gz

---
+++ packages/tap-shopify-scentbird/tap-shopify-scentbird-1.2.12.tar.gz/tap-shopify-scentbird-1.2.12/tap_shopify/streams/disputes.py
@@ -0,0 +1,15 @@
+import shopify
+
+from tap_shopify.context import Context
+
+class Disputes(Stream):
+    name = 'disputes'
+    replication_object = shopify.Disputes
+
+    def get_query_params(self, since_id, updated_at_min, updated_at_max, results_per_page):
+        return {
+            "since_id": since_id,
+            "initiated_at": updated_at_min
+        }
+
+Context.stream_objects['disputes'] = Disputes
