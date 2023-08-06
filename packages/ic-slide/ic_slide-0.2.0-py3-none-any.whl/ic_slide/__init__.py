83ce6def1b6792dcd92f89c91b4db34ae4ef8d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:14 +0000

    Add package ic-slide/0.2.0/ic_slide-0.2.0-py3-none-any.whl

---
+++ packages/ic-slide/ic_slide-0.2.0-py3-none-any.whl/ic_slide/__init__.py
@@ -0,0 +1,10 @@
+from .slide_source import open_slide, enumerate_tiles,\
+    enumerate_content_tiles
+
+from .slide_index import get_slide_entries,\
+    get_distinct_slide_ids,\
+    get_slide_entry_annotations
+
+from .config import get_config, \
+    update_config, use_client_grant, \
+    use_password_grant
