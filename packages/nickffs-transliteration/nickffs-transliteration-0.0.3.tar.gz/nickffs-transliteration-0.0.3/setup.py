6819c6a68d7bd9b20fd9f391a1910e391c5f9d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:17 +0000

    Add package nickffs-transliteration/0.0.3/nickffs-transliteration-0.0.3.tar.gz

---
+++ packages/nickffs-transliteration/nickffs-transliteration-0.0.3.tar.gz/nickffs-transliteration-0.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nickffs-transliteration", # Replace with your own username
+    version="0.0.3",
+    author="bhavnicksm",
+    author_email="bhavnicksm@gmail.com",
+    description="Hinglish --> Hindi transliteration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bhavnicksm/nickffs-transliteration",
+    project_urls={
+        "Bug Tracker": "https://github.com/Bhavnicksm/nickffs-transliteration/issues",
+    },
+    keywords = [
+        'Artificial Intelligence',
+        'Transliteration',
+        'PyTorch',
+        'Hindi',
+        'Hinglish'
+    ],
+    install_requires=[
+        "torch >= 1.6", 
+        "torchtext>=0.8", 
+        "pickle>=3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "model"},
+    packages= find_packages(where="model"),
+    python_requires=">=3.6",
+)
