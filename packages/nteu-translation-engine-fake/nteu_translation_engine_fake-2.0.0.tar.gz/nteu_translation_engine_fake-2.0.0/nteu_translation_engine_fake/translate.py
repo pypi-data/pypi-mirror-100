ebfcee711f3cb79809520d97b980ba693ff02099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:03 +0000

    Add package nteu-translation-engine-fake/2.0.0/nteu_translation_engine_fake-2.0.0.tar.gz

---
+++ packages/nteu-translation-engine-fake/nteu_translation_engine_fake-2.0.0.tar.gz/nteu_translation_engine_fake-2.0.0/nteu_translation_engine_fake/translate.py
@@ -0,0 +1,39 @@
+from aiohttp import web
+import signal
+import asyncio
+import logging
+import traceback
+import os
+
+
+async def translate(request) -> web.Response:
+    try:
+        data = await request.json()
+        texts = data["texts"]
+        translations = []
+
+        for text in texts:
+            command = text.split()
+
+            if len(command) == 2 and "wait" == command[0]:
+                await asyncio.sleep(int(command[1]))
+            elif len(command) == 1 and "error" == command[0]:
+                raise Exception("Error keyword given.")
+            elif len(command) == 1 and "shutdown" == command[0]:
+                pid = os.getpid()
+                os.kill(pid, signal.SIGINT)
+
+            translations.append({
+                "text": text,
+                "translation": f'[Fake translation] {text}'
+            })
+
+        return web.json_response({
+            "translations": translations
+        })
+
+    except Exception:
+        tb = traceback.format_exc()
+        tb_str = str(tb)
+        logging.error('Error: %s', tb_str)
+        return web.Response(text=tb_str, status=500)
