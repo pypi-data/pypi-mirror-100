985f45f9402022ca6790acff5b18d9c376c1961e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:42 +0000

    Add package file-zilla/1.1/file-zilla-1.1.tar.gz

---
+++ packages/file-zilla/file-zilla-1.1.tar.gz/file-zilla-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'file-zilla',
+    version = '1.1',
+    scripts = ['./script/fm'],
+    author = 'Gaurav Arora',
+    description = ('File manager'),
+    packages = ['file-zilla'],
+    install_requires=[
+        'setuptools',
+        'PyPDF2 >= 1.26.0',
+        'Pillow >= 7.0.0'
+    ],
+    python_requires='>=3.7'
+)
