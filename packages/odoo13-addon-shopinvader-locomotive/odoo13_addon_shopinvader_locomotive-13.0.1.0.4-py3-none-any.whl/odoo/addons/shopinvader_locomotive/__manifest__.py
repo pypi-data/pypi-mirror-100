70ce197b72076bb72f6d3f8d5576ae6f52e140b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package odoo13-addon-shopinvader-locomotive/13.0.1.0.4/odoo13_addon_shopinvader_locomotive-13.0.1.0.4-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-locomotive/odoo13_addon_shopinvader_locomotive-13.0.1.0.4-py3-none-any.whl/odoo/addons/shopinvader_locomotive/__manifest__.py
@@ -0,0 +1,26 @@
+# © 2016 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Locomotive CMS Connector",
+    "version": "13.0.1.0.4",
+    "category": "Connector",
+    "summary": "Manage communications between Shopinvader and Locomotive CMS",
+    "author": "Akretion",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "license": "AGPL-3",
+    "development_status": "Stable/Production",
+    "depends": [
+        "connector",
+        "shopinvader_search_engine",
+        "queue_job",
+        "shopinvader",
+    ],
+    "data": ["views/shopinvader_backend_view.xml", "data/ir_cron.xml"],
+    "demo": ["demo/backend_demo.xml"],
+    "external_dependencies": {"python": ["locomotivecms"]},
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
