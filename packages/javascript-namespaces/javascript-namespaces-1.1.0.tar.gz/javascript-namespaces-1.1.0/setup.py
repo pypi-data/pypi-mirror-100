a8292fe7b87503e0d8e30fdff7365bf583e0e608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:07 +0000

    Add package javascript-namespaces/1.1.0/javascript-namespaces-1.1.0.tar.gz

---
+++ packages/javascript-namespaces/javascript-namespaces-1.1.0.tar.gz/javascript-namespaces-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'javascript-namespaces',
+  description = 'Namespace',
+  version = '1.1.0',
+  license = 'MIT',
+  author = 'Jakub21',
+  url = 'https://github.com/Jakub21',
+  packages = find_packages(),
+)
