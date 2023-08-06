f99482d9fe9ef4f03945b53263ada61ca5f88784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:58 +0000

    Add package transcription-burato/0.0.2/transcription-burato-0.0.2.tar.gz

---
+++ packages/transcription-burato/transcription-burato-0.0.2.tar.gz/transcription-burato-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="transcription-burato",
+    version="0.0.2",
+    author="Otavio Burato de Oliveira",
+    author_email="otavioburato42@gmail.com",
+    description="A simples program that transcribes dna",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Otavio-Burato/DNA",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
