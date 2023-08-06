310cf5ed4a4c90cc59eb0209c790547b0cd5f8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:10 +0000

    Add package xesL2D5P/0.0.7/xesL2D5P-0.0.7.tar.gz

---
+++ packages/xesL2D5P/xesL2D5P-0.0.7.tar.gz/xesL2D5P-0.0.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='xesL2D5P',#库名
+    version='0.0.7',#版本号，建议一开始取0.0.1
+    author='Zhuoran Shi',#你的名字，名在前，姓在后
+    author_email='2720791562@qq.com',#邮箱
+    description='学而思编程Level2下第五讲随堂测的库',#库介绍
+    long_descripition_content_type="text/markdown",
+    url='https://github.com/brity-wu',
+    packages=setuptools.find_packages(),
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent" ,
+    ],
+)
