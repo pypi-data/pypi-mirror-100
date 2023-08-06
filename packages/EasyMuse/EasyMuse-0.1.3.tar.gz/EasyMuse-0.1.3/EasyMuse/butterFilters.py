d6669844e09cc3ae185490ee7233bc261a57f2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:41 +0000

    Add package EasyMuse/0.1.3/EasyMuse-0.1.3.tar.gz

---
+++ packages/EasyMuse/EasyMuse-0.1.3.tar.gz/EasyMuse-0.1.3/EasyMuse/butterFilters.py
@@ -0,0 +1,33 @@
+from scipy.signal import butter, lfilter, iirnotch
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+
+def iir_notch(notch_freq, fs, Q=30):
+    b, a = iirnotch(w0=notch_freq, Q=Q, fs=fs)
+    return b, a
+
+
+def applyButter(sample, highPass, lowPass, notchFilter):
+    filtered = sample
+    for channel in range(sample.sape[1]):
+        if lowPass is not None:  # Low pass
+            filtered[:, channel] = lfilter(lowPass[0], lowPass[1], filtered[:, channel])
+        if highPass is not None:
+            filtered[:, channel] = lfilter(notchFilter[0], notchFilter[1], filtered[:, channel])
+        if notchFilter is not None:
+            filtered[:, channel] = lfilter(highPass[0], highPass[1], filtered[:, channel])
+
+    return filtered
