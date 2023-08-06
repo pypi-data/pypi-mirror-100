8fb50a1f8079f922234f5e5a8b296603e992ea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:48 +0000

    Add package pytubekit/0.0.13/pytubekit-0.0.13.tar.gz

---
+++ packages/pytubekit/pytubekit-0.0.13.tar.gz/pytubekit-0.0.13/pytubekit/main.py
@@ -0,0 +1,105 @@
+"""
+main entry point to the program
+"""
+import os
+
+import pylogconf.core
+# import pyvardump
+from pygooglehelper import register_functions
+from pytconf import register_main, config_arg_parse_and_launch, register_endpoint
+
+from pytubekit.configs import ConfigPlaylist, ConfigPagination
+from pytubekit.scopes import SCOPES
+from pytubekit.static import DESCRIPTION, APP_NAME, VERSION_STR
+
+from pytubekit.util import create_playlists, get_youtube, get_playlist_id_from_name, create_playlist
+
+
+@register_endpoint(
+    description="Show all playlists in your youtube account",
+    configs=[ConfigPagination],
+)
+def playlists() -> None:
+    youtube = get_youtube()
+    r = create_playlists(youtube)
+    items = r.get_all_items()
+    for item in items:
+        f_title = item["snippet"]["title"]
+        print(f"{f_title}")
+
+
+@register_endpoint(
+    description="List all entries in a playlist",
+    configs=[ConfigPagination, ConfigPlaylist],
+)
+def playlist() -> None:
+    youtube = get_youtube()
+    playlist_id = get_playlist_id_from_name(youtube, ConfigPlaylist.name)
+    r = create_playlist(youtube, playlist_id=playlist_id)
+    items = r.get_all_items()
+    for item in items:
+        f_video_id = item["snippet"]["resourceId"]["videoId"]
+        print(f"{f_video_id}")
+
+
+@register_endpoint(
+    description="Dump all playlists",
+    configs=[ConfigPagination],
+)
+def dump() -> None:
+    youtube = get_youtube()
+    r = create_playlists(youtube)
+    items = r.get_all_items()
+    id_to_title = {}
+    for item in items:
+        f_id = item["id"]
+        f_title = item["snippet"]["title"]
+        id_to_title[f_id] = f_title
+    print("got lists data")
+    for f_id, f_title in id_to_title.items():
+        print(f"dumping [{f_title}]")
+        with open(f_title, "w") as f:
+            r = create_playlist(youtube, playlist_id=f_id)
+            items = r.get_all_items()
+            for item in items:
+                f_video_id = item["snippet"]["resourceId"]["videoId"]
+                print(f"{f_video_id}", file=f)
+
+
+@register_endpoint(
+    description="Mark all entries in playlist as seen",
+    configs=[ConfigPlaylist],
+)
+def mark_seen() -> None:
+    print("TBD")
+
+
+@register_endpoint(
+    description="Cleanup a specific playlist from deleted or privatized entries",
+)
+def cleanup() -> None:
+    print("TBD")
+
+
+@register_endpoint(
+    description="Collect you tube video ids from files",
+)
+def collect_ids() -> None:
+    for filename in os.listdir():
+        _, extension = os.path.splitext(filename)
+        print(extension)
+
+
+@register_main(
+    main_description=DESCRIPTION,
+    app_name=APP_NAME,
+    version=VERSION_STR,
+)
+def main():
+    pylogconf.core.setup()
+    register_functions(scopes=SCOPES, app_name=APP_NAME)
+    config_arg_parse_and_launch()
+
+
+if __name__ == "__main__":
+    main()
