626746e308162630ee700ba7cf469d66c7d90260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:43 +0000

    Add package pyHPump/1.1.3/pyHPump-1.1.3.tar.gz

---
+++ packages/pyHPump/pyHPump-1.1.3.tar.gz/pyHPump-1.1.3/pyHPump/__init__.py
@@ -0,0 +1,27 @@
+from .psd import *
+from .communication import init_serial
+from .util import *
+from .commands import *
+
+
+#List of pumps. Initially the list is empty
+pumps = []
+pumpLenght = 16
+
+def communicationInitialization(port, baudrate):
+    communication.init_serial(port, baudrate)
+
+
+def executeCommand(pump, command, waitForPump=False):
+    commands.forwardType(pump.type)
+    communication.send_command(pump.asciiAddress, command, waitForPump)
+
+
+def definePump(address: str, type=4, baudRate=9600, resolutionMode=0):
+    if len(pumps) < pumpLenght:
+        pumps.append( PSD(address, type, baudRate, resolutionMode) )
+
+
+
+
+
