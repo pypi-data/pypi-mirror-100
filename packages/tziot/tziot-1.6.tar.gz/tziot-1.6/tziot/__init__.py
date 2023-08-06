9361b5202224bcdeaa60c4f520bcb7582d51a983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:49 +0000

    Add package tziot/1.6/tziot-1.6.tar.gz

---
+++ packages/tziot/tziot-1.6.tar.gz/tziot-1.6/tziot/__init__.py
@@ -0,0 +1,5 @@
+from tziot.config import config_core_param, config_dcom_param
+from tziot.conn import is_conn
+from tziot.tziot import call, register, bind_pipe_net, bind_pipe
+
+__version__ = '1.6'
