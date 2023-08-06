c12423e0b0c1dfad25b5c036d383abbfbfa346f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:43 +0000

    Add package pinout/0.0.8/pinout-0.0.8.tar.gz

---
+++ packages/pinout/pinout-0.0.8.tar.gz/pinout-0.0.8/pinout/file_manager.py
@@ -0,0 +1,42 @@
+# File Manager: common file manipulation functions
+from pathlib import Path
+
+def unique_filepath(filepath):
+    """Generate a unique filename to ensure existing files are not overwritten.
+
+    :param filepath: File-name, including path, to the file location
+    :type filepath: str
+    :return: Unique filepath (filename and path to file location)
+    :rtype: str
+    """
+    filepath = Path(filepath)
+    suffix = filepath.suffix
+    name = filepath.name[:-len(filepath.suffix)]
+    path = filepath.parent
+    count = 0
+
+    while filepath.is_file():
+        count += 1
+        filepath = Path('{}/{}_{}{}'.format(path, name, count, suffix))
+    return filepath
+
+
+def export_file(content, path, overwrite=False):
+    """Create a file and export content to it. If overwrite is False a new filename is generated.
+
+    :param content: Text data to be written to file
+    :type content: str
+    :param path: Path, including filename, for the file to be created
+    :type path: str
+    :param overwrite: Dictate if an existing file should be overwritten. If False 'path' is amended to be a unique filename, defaults to False
+    :type overwrite: bool, optional
+    :return: Actual path the file was written to. Note: this may differ from supplied 'path' if 'overwrite' is False)
+    :rtype: str
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    if not overwrite:
+        path = unique_filepath(path)
+    path.touch(exist_ok=True)
+    path.write_text(content)
+    return path
