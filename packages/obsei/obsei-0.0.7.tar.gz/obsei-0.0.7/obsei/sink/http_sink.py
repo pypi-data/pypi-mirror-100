c1e52d053ccfc2c1cbb213d84b43ee0861395773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:33 +0000

    Add package obsei/0.0.7/obsei-0.0.7.tar.gz

---
+++ packages/obsei/obsei-0.0.7.tar.gz/obsei-0.0.7/obsei/sink/http_sink.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+from typing import Any, Dict, List
+
+import requests
+
+from obsei.sink.base_sink import BaseSink, BaseSinkConfig, Convertor
+from obsei.analyzer.base_analyzer import AnalyzerResponse
+
+
+DEFAULT_HEADERS = {
+    "Content-type": "application/json"
+}
+
+
+class HttpSinkConfig(BaseSinkConfig):
+    TYPE: str = "Http"
+    url: str
+    headers: Dict[str, Any] = None
+    base_payload: Dict[str, Any] = None
+    # analyzer_output to payload mapping
+    payload_mapping: Dict[str, List[str]] = None
+    field_conversion: Dict[str, str] = None
+
+
+class HttpSink(BaseSink):
+    def __init__(self, convertor: Convertor = Convertor(), **data: Any):
+        super().__init__(convertor=convertor, **data)
+
+    def send_data(
+        self,
+        analyzer_responses: List[AnalyzerResponse],
+        config: HttpSinkConfig,
+        **kwargs
+    ):
+
+        headers = config.headers or DEFAULT_HEADERS
+
+        payloads = []
+        responses = []
+        for analyzer_response in analyzer_responses:
+            payloads.append(self.convertor.convert(
+                analyzer_response=analyzer_response,
+                base_payload=dict() if config.base_payload is None else deepcopy(config.base_payload)
+            ))
+
+        for payload in payloads:
+            response = requests.post(
+                url=config.url,
+                json=payload,
+                headers=headers,
+            )
+            responses.append(response)
+
+        return responses
