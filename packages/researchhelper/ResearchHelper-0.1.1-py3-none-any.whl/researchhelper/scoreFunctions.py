af61bb51ae3120080589c540b59b6057ce39231d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:28 +0000

    Add package researchhelper/0.1.1/ResearchHelper-0.1.1-py3-none-any.whl

---
+++ packages/researchhelper/ResearchHelper-0.1.1-py3-none-any.whl/researchhelper/scoreFunctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def MAD(tdata, data):
+    """Scorefunction mean absolute difference"""
+    abs_diff = np.abs(tdata - data)
+    return abs_diff[~np.isnan(abs_diff)].mean()
+
+
+def NormalizedMAD(tdata, data):
+    """Scorefunction normalized mean absolute difference"""
+    abs_diff = np.abs(tdata - data) / tdata
+    return abs_diff[~np.isnan(abs_diff)].mean()
