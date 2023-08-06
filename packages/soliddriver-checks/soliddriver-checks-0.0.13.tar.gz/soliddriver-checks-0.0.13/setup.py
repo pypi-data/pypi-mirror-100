795ed680f6cf3d4a71e3838a1877cddc85370c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:53 +0000

    Add package soliddriver-checks/0.0.13/soliddriver-checks-0.0.13.tar.gz

---
+++ packages/soliddriver-checks/soliddriver-checks-0.0.13.tar.gz/soliddriver-checks-0.0.13/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="soliddriver-checks",
+    version="0.0.13",
+    author="Hui-Zhi Zhao",
+    author_email="hui.zhi.zhao@suse.com",
+    description=("Check RPM(s) and Drivers information"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suse/soliddriver-checks",
+    project_urls={
+        "Bug Tracker": "https://github.com/suse/soliddriver-checks/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    packages=['soliddriver_checks'],
+    package_dir={"soliddriver_checks": "src/soliddriver-checks"},
+    data_files=[
+        ('config', ['src/soliddriver-checks/config/soliddriver-checks.conf'])
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'soliddriver-checks=soliddriver_checks:run'
+        ]
+    },
+)
