22423a707bb4578c7117b0bcf0e69c2dcfa71903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package procuret/0.0.19/procuret-0.0.19.tar.gz

---
+++ packages/procuret/procuret-0.0.19.tar.gz/procuret-0.0.19/procuret/human/headline.py
@@ -0,0 +1,25 @@
+"""
+Procuret Python
+Human Headine Module
+author: hugh@blinkybeach.com
+"""
+from procuret.data.codable import Codable, CodingDefinition as CD
+
+
+class HumanHeadline(Codable):
+
+    coding_map = {
+        'agent_id': CD(int),
+        'full_name': CD(str)
+    }
+
+    def __init__(
+        self,
+        agent_id: int,
+        full_name: str
+    ) -> None:
+
+        self._agent_id = agent_id
+        self._full_name = full_name
+
+        return
