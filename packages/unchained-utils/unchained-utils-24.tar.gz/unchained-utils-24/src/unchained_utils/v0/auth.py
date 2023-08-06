d90568d859543cc8189cb841ef4727f9e9fbda15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:15 +0000

    Add package unchained-utils/24/unchained-utils-24.tar.gz

---
+++ packages/unchained-utils/unchained-utils-24.tar.gz/unchained-utils-24/src/unchained_utils/v0/auth.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AnonymousUser
+
+
+class TokenAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+
+    def __call__(self, scope):
+        from rest_framework.authtoken.models import Token
+        headers = dict(scope['headers'])
+        print('headers', headers)
+        if b'authorization' in headers:
+            try:
+                token_name, token_key = headers[b'authorization'].decode().split()
+                if token_name == 'Token':
+                    token = Token.objects.get(key=token_key)
+                    scope['user'] = token.user
+            except Token.DoesNotExist:
+                scope['user'] = AnonymousUser()
+        return self.inner(scope)
+
+def TokenAuthMiddlewareStack(inner):
+    from channels.auth import AuthMiddlewareStack
+    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
