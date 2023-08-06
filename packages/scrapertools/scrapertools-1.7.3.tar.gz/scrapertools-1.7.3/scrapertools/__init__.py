7a6af1baa4db328aeeed463efc4c009c301ab2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:44 +0000

    Add package scrapertools/1.7.3/scrapertools-1.7.3.tar.gz

---
+++ packages/scrapertools/scrapertools-1.7.3.tar.gz/scrapertools-1.7.3/scrapertools/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "1.7.3"
+__author__ = "UltrafunkAmsterdam"
+__url__ = "https://github.com/ultrafunkamsterdam/scrapertools"
+__license__ = "GPL3.0 License"
+__author_email__ = "info@ultrafunk.nl"
+__description__ = "scraping for humans"
+
+
+from ._ua import (
+    GENERIC_CURL,
+    GENERIC_WINDOWS,
+    GENERIC_GOOGLEBOT,
+    GENERIC_MACOS,
+    GENERIC_MOBILE,
+    GENERIC_WEBKIT,
+)
+
+from ._util import load_state, save_state, fetch
+from . import _util as util
