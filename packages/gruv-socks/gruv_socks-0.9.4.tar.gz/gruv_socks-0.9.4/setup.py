6df7ac0ada0791a628eebea1365625a307f971f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:31 +0000

    Add package gruv-socks/0.9.4/gruv_socks-0.9.4.tar.gz

---
+++ packages/gruv-socks/gruv_socks-0.9.4.tar.gz/gruv_socks-0.9.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name='gruv_socks',
+    version='0.9.4',
+    author="Forest Jacobsen",
+    description="Easy to use standard for abstracting the work needed to transport and reconstruct messages over TCP.",
+    packages=setuptools.find_packages(include=['gruv_socks', 'gruv_socks.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
