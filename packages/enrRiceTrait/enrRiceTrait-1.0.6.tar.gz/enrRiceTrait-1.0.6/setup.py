c5f567454ce339670427d4c019668f826b51b11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:20 +0000

    Add package enrRiceTrait/1.0.6/enrRiceTrait-1.0.6.tar.gz

---
+++ packages/enrRiceTrait/enrRiceTrait-1.0.6.tar.gz/enrRiceTrait-1.0.6/setup.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+# ! usr/bin/env python3
+"""
+Created on 28/03/2021 22:58
+@Author: XINZHI YAO
+"""
+
+# from distutils.core import setup
+import setuptools
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+# setup(
+#   name="enrRiceTrait",
+#   version="1.0.2",
+#   author="Xinzhi_Yao",
+#   author_email="xinzhi_bioinfo@163.com",
+#   description="A python package for Rice Trait Enrichment.",
+#   long_description=long_description,
+#   long_description_content_type="text/markdown",
+#   url="https://github.com/YaoXinZhi/enrRiceTrait",
+#   packages=find_packages('src'),
+#   package_dir={'': 'src'},
+#   include_package_data=True,
+#   classifiers=[
+#   "Programming Language :: Python :: 3",
+#   "License :: OSI Approved :: MIT License",
+#   "Operating System :: OS Independent",
+#   ],
+# )
+
+setup(
+  name='enrRiceTrait',
+  version='1.0.6',
+  author='xinzhi_yao',
+  author_email='xinzhi_bioinfo@163.com',
+  description="A python package for Rice Trait Enrichment.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/YaoXinZhi/enrRiceTrait",
+  include_package_data=True,
+  package_data={
+    'enrRiceTrait': ['data/to.wto.ro.funRiceGene.obo',
+                     'data/Total_Association.txt']
+  },
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>3.7'
+)
+
+
