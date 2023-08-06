0bb337135ca2d19dc632e6ec9280c980555f62f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:34 +0000

    Add package tykit/0.0.4/tykit-0.0.4.tar.gz

---
+++ packages/tykit/tykit-0.0.4.tar.gz/tykit-0.0.4/tykit/__init__.py
@@ -0,0 +1,14 @@
+'''
+Description: 
+version: 
+Author: TianyuYuan
+Date: 2021-04-02 15:40:39
+LastEditors: TianyuYuan
+LastEditTime: 2021-04-02 18:38:00
+'''
+name = "tykit"
+from tykit import *
+
+if __name__ == "__main__":
+    rlog = RLog()
+    rlog.start('sdlf')
