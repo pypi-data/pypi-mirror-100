1bd7d04efb142202a09c49c5cf0c78e7c6ad1e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:29 +0000

    Add package seqRegression/0.0.1/seqRegression-0.0.1.tar.gz

---
+++ packages/seqRegression/seqRegression-0.0.1.tar.gz/seqRegression-0.0.1/setup.py
@@ -0,0 +1,28 @@
+"""
+Created on Wen Apr 8 10:18 2020
+
+@author: Hanwen Xu
+
+E-mail: hw-xu16@mails.tsinghua.edu.cn
+
+New update: process data with unknown content
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="seqRegression",
+    version="0.0.1",
+    keywords=("pip", "seqRegression"),
+    description="seqRegression models used by WangLab",
+    long_description="Including three models",
+    license="Hanwen",
+
+    url="https://github.com/WangLabTHU/RareDecipher/",
+    author="Hanwen Xu",
+    author_email="xuhw20@mails.tsinghua.edu.cn",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['numpy', 'torch', 'scipy', 'sklearn', 'statsmodels', 'pandas']
+)
