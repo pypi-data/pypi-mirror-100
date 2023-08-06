1c29f73f091cd55704bf79b7339bf209c6e902ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:50 +0000

    Add package dashactyl.py/0.0.7/dashactyl.py-0.0.7.tar.gz

---
+++ packages/dashactyl.py/dashactyl.py-0.0.7.tar.gz/dashactyl.py-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='dashactyl.py',
+    version='0.0.7',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/Mystik-Lukas/dashactyl.py',  
+    author='Lukas Canter',
+    author_email='lukascanter07@outlook.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords='dahsactyl', 
+    packages=find_packages(),
+    install_requires=['requests'] 
+)
