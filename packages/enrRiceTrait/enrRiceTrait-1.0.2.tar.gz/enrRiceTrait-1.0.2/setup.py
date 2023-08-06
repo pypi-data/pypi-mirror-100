e11e91b8a763032a8652561d30a79d0263479bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:53 +0000

    Add package enrRiceTrait/1.0.2/enrRiceTrait-1.0.2.tar.gz

---
+++ packages/enrRiceTrait/enrRiceTrait-1.0.2.tar.gz/enrRiceTrait-1.0.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# ! usr/bin/env python3
+"""
+Created on 28/03/2021 22:58
+@Author: XINZHI YAO
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name="enrRiceTrait",
+  version="1.0.2",
+  author="Xinzhi_Yao",
+  author_email="xinzhi_bioinfo@163.com",
+  description="A python package for Rice Trait Enrichment.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/YaoXinZhi/enrRiceTrait",
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  include_package_data=True,
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
+
---
+++ packages/enrRiceTrait/enrRiceTrait-1.0.2.tar.gz/enrRiceTrait-1.0.2/src/enrRiceTrait/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# ! usr/bin/env python3
+"""
+Created on 28/03/2021 23:15
+@Author: XINZHI YAO
+"""
+from __future__ import absolute_import
+from .Enrichment import *
+
+__version__ = '1.0.1'
+__license__ = ''
+
