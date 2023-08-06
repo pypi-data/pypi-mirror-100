c6a88b56e8a77d63227234c3860c84ced5953cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package odoo13-addon-shopinvader/13.0.1.0.1/odoo13_addon_shopinvader-13.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader/odoo13_addon_shopinvader-13.0.1.0.1-py3-none-any.whl/odoo/addons/shopinvader/tests/common.py
@@ -0,0 +1,118 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=method-required-super
+
+from contextlib import contextmanager
+
+from odoo.addons.base_rest.controllers.main import _PseudoCollection
+from odoo.addons.base_rest.tests.common import BaseRestCase
+from odoo.addons.component.core import WorkContext
+from odoo.addons.component.tests.common import ComponentMixin
+from odoo.addons.queue_job.job import Job
+from odoo.addons.shopinvader import shopinvader_response
+from odoo.tests import SavepointCase
+
+
+class CommonMixin(ComponentMixin):
+    @staticmethod
+    def _setup_backend(cls):
+        cls.env = cls.env(context={"lang": "en_US"})
+        cls.backend = cls.env.ref("shopinvader.backend_1")
+        cls.backend.bind_all_product()
+        cls.shopinvader_session = {}
+        cls.existing_jobs = cls.env["queue.job"].browse()
+
+    @contextmanager
+    def work_on_services(self, **params):
+        params = params or {}
+        if "shopinvader_backend" not in params:
+            params["shopinvader_backend"] = self.backend
+        if "shopinvader_session" not in params:
+            params["shopinvader_session"] = {}
+        collection = _PseudoCollection("shopinvader.backend", self.env)
+        yield WorkContext(
+            model_name="rest.service.registration",
+            collection=collection,
+            **params
+        )
+
+    def _init_job_counter(self):
+        self.existing_jobs = self.env["queue.job"].search([])
+
+    @property
+    def created_jobs(self):
+        return self.env["queue.job"].search([]) - self.existing_jobs
+
+    def _check_nbr_job_created(self, nbr):
+        self.assertEqual(len(self.created_jobs), nbr)
+
+    def _perform_job(self, job):
+        Job.load(self.env, job.uuid).perform()
+
+    def _perform_created_job(self):
+        for job in self.created_jobs:
+            self._perform_job(job)
+
+
+class CommonCase(SavepointCase, CommonMixin):
+
+    # by default disable tracking suite-wise, it's a time saver :)
+    tracking_disable = True
+
+    @classmethod
+    def setUpClass(cls):
+        super(CommonCase, cls).setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context, tracking_disable=cls.tracking_disable
+            )
+        )
+        CommonMixin._setup_backend(cls)
+        cls.setUpComponent()
+
+    def setUp(self):
+        SavepointCase.setUp(self)
+        CommonMixin.setUp(self)
+
+        shopinvader_response.set_testmode(True)
+
+        @self.addCleanup
+        def cleanupShopinvaderResponseTestMode():
+            shopinvader_response.set_testmode(False)
+
+
+class ProductCommonCase(CommonCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.template = cls.env.ref(
+            "product.product_product_4_product_template"
+        )
+        cls.variant = cls.env.ref("product.product_product_4b")
+        cls.template.taxes_id = cls.env.ref("shopinvader.tax_1")
+        cls.shopinvader_variants = cls.env["shopinvader.variant"].search(
+            [
+                ("record_id", "in", cls.template.product_variant_ids.ids),
+                ("backend_id", "=", cls.backend.id),
+            ]
+        )
+        cls.shopinvader_variant = cls.env["shopinvader.variant"].search(
+            [
+                ("record_id", "=", cls.variant.id),
+                ("backend_id", "=", cls.backend.id),
+            ]
+        )
+        cls.env.user.company_id.currency_id = cls.env.ref("base.USD")
+        base_price_list = cls.env.ref("product.list0")
+        base_price_list.currency_id = cls.env.ref("base.USD")
+        cls.shopinvader_variant.record_id.currency_id = cls.env.ref("base.USD")
+
+
+class ShopinvaderRestCase(BaseRestCase):
+    def setUp(self, *args, **kwargs):
+        super(ShopinvaderRestCase, self).setUp(*args, **kwargs)
+        self.backend = self.env.ref("shopinvader.backend_1")
+        # To ensure multi-backend works correctly, we just have to create
+        # a new one on the same company.
+        self.backend_copy = self.env.ref("shopinvader.backend_2")
