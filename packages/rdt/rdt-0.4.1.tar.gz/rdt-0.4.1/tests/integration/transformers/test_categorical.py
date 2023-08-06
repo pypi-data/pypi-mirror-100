6f336e0a424c8e82dfda6141f15efd1102d12d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:41 +0000

    Add package rdt/0.4.1/rdt-0.4.1.tar.gz

---
+++ packages/rdt/rdt-0.4.1.tar.gz/rdt-0.4.1/tests/integration/transformers/test_categorical.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+
+from rdt.transformers import (
+    CategoricalTransformer, LabelEncodingTransformer, OneHotEncodingTransformer)
+
+
+def test_categorical_numerical_nans():
+    """Ensure CategoricalTransformer works on numerical + nan only columns."""
+
+    data = pd.Series([1, 2, float('nan'), np.nan])
+
+    transformer = CategoricalTransformer()
+    transformer.fit(data)
+    transformed = transformer.transform(data)
+    reverse = transformer.reverse_transform(transformed)
+
+    pd.testing.assert_series_equal(reverse, data)
+
+
+def test_one_hot_numerical_nans():
+    """Ensure OneHotEncodingTransformer works on numerical + nan only columns."""
+
+    data = pd.Series([1, 2, float('nan'), np.nan])
+
+    transformer = OneHotEncodingTransformer()
+    transformer.fit(data)
+    transformed = transformer.transform(data)
+    reverse = transformer.reverse_transform(transformed)
+
+    pd.testing.assert_series_equal(reverse, data)
+
+
+def test_label_numerical_2d_array():
+    """Ensure LabelEncodingTransformer works on numerical + nan only columns."""
+
+    data = pd.Series([1, 2, 3, 4])
+
+    transformer = LabelEncodingTransformer()
+    transformer.fit(data)
+    transformed = np.array([[0], [1], [2], [3]])
+    reverse = transformer.reverse_transform(transformed)
+
+    pd.testing.assert_series_equal(reverse, data)
+
+
+def test_label_numerical_nans():
+    """Ensure LabelEncodingTransformer works on numerical + nan only columns."""
+
+    data = pd.Series([1, 2, float('nan'), np.nan])
+
+    transformer = LabelEncodingTransformer()
+    transformer.fit(data)
+    transformed = transformer.transform(data)
+    reverse = transformer.reverse_transform(transformed)
+
+    pd.testing.assert_series_equal(reverse, data)
