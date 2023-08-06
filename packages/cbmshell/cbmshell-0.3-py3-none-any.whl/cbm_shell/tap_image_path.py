0d84bd33f878c415fa3994531d12121854fe710e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:17 +0000

    Add package cbmshell/0.3/cbmshell-0.3-py3-none-any.whl

---
+++ packages/cbmshell/cbmshell-0.3-py3-none-any.whl/cbm_shell/tap_image_path.py
@@ -0,0 +1,38 @@
+from cbm_shell.image_path import ImagePath
+
+
+class TapImagePath(ImagePath):
+    def __init__(self, drive, header, data):
+        self.drive = drive
+        self.header = header
+        self.data = data
+
+    @property
+    def encoded_name(self):
+        return self.header.name
+
+    @property
+    def file_type(self):
+        return str(self.header.htype)
+
+    @property
+    def size_bytes(self):
+        return self.header.end-self.header.start
+
+    def open(self, mode, ftype=None):
+        if mode != 'rb':
+            raise NotImplementedError('tap file read only')
+        return self.data.open()
+
+    def exists(self):
+        return True
+
+    def name(self, encoding):
+        return self.header.name.decode(encoding)
+
+    def file_info(self, token_set, encoding):
+        info = "{}: {}, size={} bytes".format(self.name(encoding), self.file_type, self.size_bytes)
+        if self.file_type.startswith('PRG'):
+            info += ", start=${:04x}".format(self.header.start)
+
+        return info
