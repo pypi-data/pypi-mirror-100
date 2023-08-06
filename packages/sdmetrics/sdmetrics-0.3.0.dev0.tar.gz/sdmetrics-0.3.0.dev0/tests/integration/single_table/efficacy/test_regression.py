8be2e1dc43105d9f3bd128ca723aedc8164ceba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:38 +0000

    Add package sdmetrics/0.3.0.dev0/sdmetrics-0.3.0.dev0.tar.gz

---
+++ packages/sdmetrics/sdmetrics-0.3.0.dev0.tar.gz/sdmetrics-0.3.0.dev0/tests/integration/single_table/efficacy/test_regression.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from sdmetrics.single_table.efficacy.regression import LinearRegression, MLPRegressor
+
+METRICS = [
+    LinearRegression,
+    MLPRegressor,
+]
+
+
+@pytest.fixture
+def real_data():
+    return pd.DataFrame({
+        'a': np.random.normal(size=600),
+        'b': np.random.randint(0, 10, size=600),
+        'c': ['a', 'b', 'b', 'c', 'c', 'c'] * 100,
+        'd': [True, True, True, True, True, False] * 100,
+    })
+
+
+@pytest.fixture
+def good_data():
+    return pd.DataFrame({
+        'a': np.random.normal(loc=0.01, size=600),
+        'b': np.random.randint(0, 10, size=600),
+        'c': ['a', 'b', 'b', 'b', 'c', 'c'] * 100,
+        'd': [True, True, True, True, False, False] * 100,
+    })
+
+
+@pytest.fixture
+def bad_data():
+    return pd.DataFrame({
+        'a': np.random.normal(loc=5, scale=3, size=600),
+        'b': np.random.randint(5, 15, size=600),
+        'c': ['a', 'a', 'a', 'a', 'b', 'b'] * 100,
+        'd': [True, False, False, False, False, False] * 100,
+    })
+
+
+@pytest.mark.parametrize('metric', METRICS)
+def test_rank(metric, real_data, good_data, bad_data):
+    bad = metric.compute(real_data, bad_data, target='a')
+    good = metric.compute(real_data, good_data, target='a')
+    real = metric.compute(real_data, real_data, target='a')
+
+    normalized_bad = metric.normalize(bad)
+    normalized_good = metric.normalize(good)
+    normalized_real = metric.normalize(real)
+
+    assert metric.min_value <= bad < good < real <= metric.max_value
+    assert 0.0 <= normalized_bad <= normalized_good <= normalized_real <= 1.0
