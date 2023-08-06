c2873903bfbe43dffd1ddf486b135c82fba4b9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:09 +0000

    Add package libauc/1.0.5/libauc-1.0.5-py3-none-any.whl

---
+++ packages/libauc/libauc-1.0.5-py3-none-any.whl/libauc/models/__init__.py
@@ -0,0 +1,12 @@
+from .densenet_v2 import DenseNet121
+from .densenet_v2 import DenseNet169
+from .densenet_v2 import DenseNet201
+from .densenet_v2 import DenseNet161
+from .resnet import resnet18 as ResNet18
+from .resnet import resnet34 as ResNet34
+from .resnext import resnext101_32x8d_wsl
+from .resnext import resnext101_32x16d_wsl
+from .resnext import resnext101_32x32d_wsl
+from .resnext import resnext101_32x48d_wsl
+from .resnet_small import resnet20_cifar as ResNet20
+from .resnet_small import resnet56_cifar as ResNet56
