56ec0f6790e9c72e870c47c0cdef316866635068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:00 +0000

    Add package StratBox/0.0.12/StratBox-0.0.12.tar.gz

---
+++ packages/StratBox/StratBox-0.0.12.tar.gz/StratBox-0.0.12/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 14:56:44 2021
+
+@author: HanaFI
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.12'
+DESCRIPTION = 'My first Strategy package'
+LONG_DESCRIPTION = "".join("My first package which should help systematize"+
+                           "the back-end of strategy trading, from underlying"+
+                           " selection portfolio optimization.")
+
+# Setting up
+setup(
+        name="StratBox",
+        version=VERSION,
+        author="Kelian Ferhat",
+        author_email="<kelianferhat@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that
+        # needs to be installed along with your package.
+
+        keywords=['python', 'finance'],
+        classifiers= [
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Financial and Insurance Industry",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
