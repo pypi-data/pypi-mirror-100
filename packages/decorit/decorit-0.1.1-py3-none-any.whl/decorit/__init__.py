009509fbad15d9692e9da47f63d7f77836f62009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:14 +0000

    Add package decorit/0.1.1/decorit-0.1.1-py3-none-any.whl

---
+++ packages/decorit/decorit-0.1.1-py3-none-any.whl/decorit/__init__.py
@@ -0,0 +1,15 @@
+"""
+.. include:: ../../README.md
+
+"""
+
+from .decorit import (
+    alias,
+    copy_doc,
+    copy_doc_params,
+    deprecated,
+    logging,
+    time,
+)
+
+__version__ = '0.1.1'
