7c92457892d5754e70071460fc7724d329b43482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:53 +0000

    Add package probabis/0.1/probabis-0.1.tar.gz

---
+++ packages/probabis/probabis-0.1.tar.gz/probabis-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabis',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probabs'],
+      author = 'Alvaro Montoya',
+      author_email = 'ajaltamiranomontoya@gmail.com',
+      zip_safe=False)
