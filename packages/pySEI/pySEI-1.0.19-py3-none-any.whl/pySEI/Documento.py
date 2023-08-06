6702fadc04899113c1e94c7ea353b8d65873f660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:17 +0000

    Add package pySEI/1.0.19/pySEI-1.0.19-py3-none-any.whl

---
+++ packages/pySEI/pySEI-1.0.19-py3-none-any.whl/pySEI/Documento.py
@@ -0,0 +1,15 @@
+class Documento:
+    def __init__(self,documento,full_crawl=False):
+        pass
+
+    def get_conteudo(self):
+        pass
+
+    def listar_botoes(self):
+        pass
+
+    def is_assinado(self):
+        pass
+
+    def assinar(self):
+        pass
---
+++ packages/pySEI/pySEI-1.0.19-py3-none-any.whl/pySEI/Processo.py
@@ -0,0 +1,54 @@
+class Processo:
+    def __init__(self,numero,full_crawl=False):
+        pass
+
+    def full_crawl(self):
+        pass
+
+    def listar_documentos(self):
+        pass
+
+    def get_documento(self,numero):
+        pass
+
+    def listar_botoes(self):
+        pass
+
+    def clicar_botao(self,botao):
+        pass
+
+    def is_aberto(self):
+        pass
+
+    def abrir(self):
+        pass
+
+    def fechar(self):
+        pass
+
+    def atribuir(self,usuario):
+        pass
+
+    def get_mensagem(self):
+        pass
+
+    def get_tipo_processo(self):
+        pass
+
+    def set_tipo_processo(self):
+        pass
+
+    def get_especificacao(self):
+        pass
+
+    def set_especificacao(self):
+        pass
+
+    def get_interessados(self):
+        pass
+
+    def get_atribuicao(self):
+        pass
+
+    def consultar_andamento(self):
+        pass
