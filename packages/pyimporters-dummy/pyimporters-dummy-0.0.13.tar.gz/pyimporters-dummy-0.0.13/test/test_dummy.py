6e0da5dae00c509cb23794e5b92e88de4ec13e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:17 +0000

    Add package pyimporters-dummy/0.0.13/pyimporters-dummy-0.0.13.tar.gz

---
+++ packages/pyimporters-dummy/pyimporters-dummy-0.0.13.tar.gz/pyimporters-dummy-0.0.13/test/test_dummy.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from progress.bar import Bar
+from pyimporters_dummy.dummy import DummyOptions, DummyKnowledgeParser, DummyOptionsModel
+from pyimporters_plugins.base import Term
+
+
+def test_dummy():
+    source = Path()
+    parser = DummyKnowledgeParser()
+    options = DummyOptionsModel(foo="test")
+    concepts = list(parser.parse(source, options.dict(), Bar('Processing')))
+    assert len(concepts) == 1
+    c0 : Term = concepts[0]
+    assert c0.identifier == 'test'
+    assert c0.preferredForm == 'test'
+    assert c0.properties is None
