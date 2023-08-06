b0fbd0d6332c1a21f5b8fb25eb4fb2a573049072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:03 +0000

    Add package ga-chgraph/1.3/ga-chgraph-1.3.tar.gz

---
+++ packages/ga-chgraph/ga-chgraph-1.3.tar.gz/ga-chgraph-1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ga-chgraph',
+    version=1.3,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ['GA_CHGRAPH = clickhouse_api_server.main:main']
+    },
+    data_files=[('clickhouse_api_server', ['clickhouse_api_server/config/graph_config.json'])],
+    install_requires=[	
+	"tornado-swagger==1.2.8",
+	"ujson==4.0.1",
+	"clickhouse-driver==0.2.0",
+	"tornado==5.0.2"
+    ],
+    license='GNU General Public License v3.0',
+    author='Liu Feng',
+    author_email='18389597276@163.com',
+    description='Graph Function'
+)
