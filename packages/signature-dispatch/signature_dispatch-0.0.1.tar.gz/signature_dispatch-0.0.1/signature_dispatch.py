1546286cb4a99f10c054c8860d0f12b07e5d3b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:05 +0000

    Add package signature-dispatch/0.0.1/signature_dispatch-0.0.1.tar.gz

---
+++ packages/signature-dispatch/signature_dispatch-0.0.1.tar.gz/signature_dispatch-0.0.1/signature_dispatch.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+"""
+Execute the first function that matches the given arguments.
+
+Use this object to decorate multiple functions.  When called, all of the 
+decorated functions will be tested in order to see if they accept the given 
+arguments.  The first one that does will be invoked.  A TypeError will be 
+raised if none of the functions can accept the arguments.
+
+Each decorated function will be replaced by the same callable.  To avoid 
+confusion, then, it's best to use the same name for each function.  The 
+docstring of the ultimate callable will be taken from the final decorated 
+function.
+
+Examples:
+
+>>> import signature_dispatch
+>>> dispatch = signature_dispatch()
+>>> @dispatch
+... def f(x):
+...    return x
+...
+>>> 
+>>> @dispatch
+... def f(x, y):
+...    return x, y
+...
+>>> f(1)
+1
+>>> f(1, 2)
+(1, 2)
+>>> f(1, 2, 3)
+Traceback (most recent call last):
+    ...
+TypeError: f() takes 2 positional arguments but 3 were given
+"""
+
+# This is pretty similar to:
+# https://github.com/Lucretiel/Dispatch
+
+import sys, inspect
+from functools import update_wrapper
+
+__version__ = '0.0.1'
+
+class Decorator:
+    __doc__ = __doc__
+
+    def __init__(self):
+        self.dispatcher = Dispatcher()
+
+    def __call__(self, f):
+        self.dispatcher += f
+        return update_wrapper(self.dispatcher, f)
+
+class Dispatcher:
+
+    def __init__(self):
+        self.candidates = []
+
+    def __iadd__(self, f):
+        self.candidates.append(f)
+        return self
+
+    def __call__(self, *args, **kwargs):
+        assert self.candidates
+
+        for f in self.candidates:
+            sig = inspect.signature(f)
+            try:
+                sig.bind(*args, **kwargs)
+                break
+            except TypeError:
+                continue
+
+        return f(*args, **kwargs)
+
+
+
+
+# Hack to make the module directly usable as a decorator.  Only works for 
+# python 3.5 or higher.  See this Stack Overflow post:
+# https://stackoverflow.com/questions/1060796/callable-modules
+
+class CallableModule(sys.modules[__name__].__class__):
+
+    def __call__(self):
+        return Decorator()
+
+sys.modules[__name__].__class__ = CallableModule
+
---
+++ packages/signature-dispatch/signature_dispatch-0.0.1.tar.gz/signature_dispatch-0.0.1/tests/test_dispatch.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import signature_dispatch
+import pytest
+
+def test_positional_or_keyword():
+    d = signature_dispatch()
+
+    @d
+    def f(a):
+        return a
+    @d
+    def f(a, b):
+        return a, b
+
+    assert f(1) == 1
+    assert f(a=1) == 1
+
+    assert f(1, 2) == (1, 2)
+    assert f(1, b=2) == (1, 2)
+    assert f(a=1, b=2) == (1, 2)
+
+    with pytest.raises(TypeError):
+        f()
+    with pytest.raises(TypeError):
+        f(1, 2, 3)
+
+def test_var_positional():
+    d = signature_dispatch()
+
+    @d
+    def f(*a):
+        return a
+    @d
+    def f(*a, b):
+        return a, b
+
+    assert f() == ()
+    assert f(1) == (1,)
+    assert f(1, 2) == (1, 2)
+
+    assert f(b=1) == ((), 1)
+    assert f(1, b=2) == ((1,), 2)
+    assert f(1, 2, b=3) == ((1, 2), 3)
+
+    with pytest.raises(TypeError):
+        f(c=1)
+
+def test_keyword_only():
+    d = signature_dispatch()
+
+    @d
+    def f(*, a):
+        return a
+    @d
+    def f(*, a, b):
+        return a, b
+
+    assert f(a=1) == 1
+    assert f(a=1, b=2) == (1, 2)
+
+    with pytest.raises(TypeError):
+        f()
+    with pytest.raises(TypeError):
+        f(1)
+    with pytest.raises(TypeError):
+        f(b=1)
+
+def test_var_keyword():
+    d = signature_dispatch()
+
+    @d
+    def f(**kwargs):
+        return kwargs
+    @d
+    def f(a, **kwargs):
+        return a, kwargs
+
+    assert f() == {}
+    assert f(a=1) == {'a': 1}
+    assert f(b=1) == {'b': 1}
+    assert f(a=1, b=2) == {'a': 1, 'b': 2}
+
+    assert f(1) == (1, {})
+    assert f(1, b=2) == (1, {'b': 2})
+    assert f(1, c=2) == (1, {'c': 2})
+    assert f(1, b=2, c=3) == (1, {'b': 2, 'c': 3})
+
+    with pytest.raises(TypeError):
+        f(1, 2)
+    with pytest.raises(TypeError):
+        f(1, a=2)  # `a` specified twice
+
+def test_docstring():
+    d = signature_dispatch()
+
+    @d
+    def f(a):
+        "a"
+        return a
+    @d
+    def f(a, b):
+        "a, b"
+        return a, b
+
+    assert f.__doc__ == "a, b"
