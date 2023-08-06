73c24517e9f689f192f4864e29eb8eada7d833bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:42 +0000

    Add package isOddNum/1.0.0/isOddNum-1.0.0.tar.gz

---
+++ packages/isOddNum/isOddNum-1.0.0.tar.gz/isOddNum-1.0.0/isOdd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+# @Author : qa.yw
+# @Time : 2021/3/30 11:19
+# @File : isOdd.py
+# @desc : 判断奇数函数
+'''
+此函数判断num是否属于奇数
+'''
+
+
+def isOddNum(num):
+    if num % 2 == 0:
+        return False
+    else:
+        return True
---
+++ packages/isOddNum/isOddNum-1.0.0.tar.gz/isOddNum-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name='isOddNum',
+    version='1.0.0',
+    py_modules=['isOdd'],
+    author='qa.yw',
+    author_email='byteflow@163.com',
+    url='https://www.baidu.com',
+    description='A function judging odd'
+)
