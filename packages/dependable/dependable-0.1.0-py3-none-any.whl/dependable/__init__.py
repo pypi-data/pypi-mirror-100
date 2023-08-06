658b02f39da766abe2fe9c9125b39ab4fc8d917b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:41 +0000

    Add package dependable/0.1.0/dependable-0.1.0-py3-none-any.whl

---
+++ packages/dependable/dependable-0.1.0-py3-none-any.whl/dependable/__init__.py
@@ -0,0 +1,14 @@
+__author__ = """Dimitrios Flaco Mengidis """
+__email__ = "tydeas.dr@gmail.com"
+__version__ = "0.1.0"
+
+from typing import Any, Callable, Optional
+
+from .core import Depends as InternalDepends
+from .decorator import dependant as dependant
+
+
+def Depends(  # noqa: N802
+    dependency: Optional[Callable[..., Any]] = None, *, use_cache: bool = True
+) -> Any:
+    return InternalDepends(dependency=dependency, use_cache=use_cache)
---
+++ packages/dependable/dependable-0.1.0-py3-none-any.whl/dependable/core.py
@@ -0,0 +1,44 @@
+from typing import Any, Callable, List, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+class Depends:
+    def __init__(
+        self, dependency: Optional[Callable[..., Any]] = None, *, use_cache: bool = True
+    ):
+        self.dependency = dependency
+        self.use_cache = use_cache
+
+    def __repr__(self) -> str:
+        attr = getattr(self.dependency, "__name__", type(self.dependency).__name__)
+        cache = "" if self.use_cache else ", use_cache=False"
+        return f"{self.__class__.__name__}({attr}{cache})"
+
+
+class Dependant:
+    def __init__(
+        self,
+        *,
+        dependencies: Optional[List["Dependant"]] = None,
+        name: Optional[str] = None,
+        call: Callable[..., Any],
+        # request_param_name: Optional[str] = None,
+        # websocket_param_name: Optional[str] = None,
+        # http_connection_param_name: Optional[str] = None,
+        # response_param_name: Optional[str] = None,
+        # background_tasks_param_name: Optional[str] = None,
+        use_cache: bool = True,
+        # path: Optional[str] = None,
+    ) -> None:
+        self.dependencies = dependencies or []
+        # self.websocket_param_name = websocket_param_name
+        # self.http_connection_param_name = http_connection_param_name
+        # self.response_param_name = response_param_name
+        # self.background_tasks_param_name = background_tasks_param_name
+        self.name = name
+        self.call = call
+        self.use_cache = use_cache
+        # Store the path to be able to re-generate a dependable from it in overrides
+        # self.path = path
+        self.cache_key = (self.call,)
