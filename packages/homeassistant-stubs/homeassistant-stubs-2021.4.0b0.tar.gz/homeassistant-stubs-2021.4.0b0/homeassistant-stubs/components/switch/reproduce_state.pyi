33e30f229562213d8ae87b6cdc3e23cf72dd2d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:01 +0000

    Add package homeassistant-stubs/2021.4.0b0/homeassistant-stubs-2021.4.0b0.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant-stubs-2021.4.0b0.tar.gz/homeassistant-stubs-2021.4.0b0/homeassistant-stubs/components/switch/reproduce_state.pyi
@@ -0,0 +1,8 @@
+from . import DOMAIN as DOMAIN
+from homeassistant.const import ATTR_ENTITY_ID as ATTR_ENTITY_ID, SERVICE_TURN_OFF as SERVICE_TURN_OFF, SERVICE_TURN_ON as SERVICE_TURN_ON, STATE_OFF as STATE_OFF, STATE_ON as STATE_ON
+from homeassistant.core import Context as Context, HomeAssistant as HomeAssistant, State as State
+from typing import Any, Iterable
+
+VALID_STATES: Any
+
+async def async_reproduce_states(hass: HomeAssistant, states: Iterable[State], *, context: Union[Context, None]=..., reproduce_options: Union[dict[str, Any], None]=...) -> None: ...
