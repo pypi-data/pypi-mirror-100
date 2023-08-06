7d0999d5a2674a978ef84f5105f7eb15d94a9b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:31 +0000

    Add package yk-face-api-model/2.0.0/yk-face-api-model-2.0.0.tar.gz

---
+++ packages/yk-face-api-model/yk-face-api-model-2.0.0.tar.gz/yk-face-api-model-2.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yk-face-api-model",
+    version="2.0.0",
+    description="YooniK face API data model package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="YooniK",
+    author_email="tech@yoonik.me",
+    url="https://yoonik.me",
+    license='MIT',
+    packages=["yk_face_api_models"],
+    install_requires=[
+        'six',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
