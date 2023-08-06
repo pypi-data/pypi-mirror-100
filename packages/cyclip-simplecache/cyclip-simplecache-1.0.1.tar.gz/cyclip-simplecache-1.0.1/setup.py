4b6dd5bd743b8f2846d541ca0d483cd3b56b3539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package cyclip-simplecache/1.0.1/cyclip-simplecache-1.0.1.tar.gz

---
+++ packages/cyclip-simplecache/cyclip-simplecache-1.0.1.tar.gz/cyclip-simplecache-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cyclip-simplecache",
+    version="1.0.1",
+    description="Pure-python implementation of Least Recently Used (LRU) caching. It allows a developer to easily use memoization on expensive function calls.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cyclip/SimpleCache",
+    author="Cyclip",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["SimpleCache"],
+    include_package_data=True,
+)
