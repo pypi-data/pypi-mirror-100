5c93028bab397d54eaa563048ee3ad930f1f1f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:37 +0000

    Add package hello-world-lobo/0.0.1/hello-world-lobo-0.0.1.tar.gz

---
+++ packages/hello-world-lobo/hello-world-lobo-0.0.1.tar.gz/hello-world-lobo-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name="hello-world-lobo",
+      version="0.0.1",
+      author="Miguel López Cruz",
+      author_email="miguellcl@hotmail.com",
+      url="",
+      description="A hello-world example package",
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          ],
+      )
+      
