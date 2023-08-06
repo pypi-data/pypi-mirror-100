4f1c9bfeaa0a4081924e69dadd2497370464432e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package iflearn/1.0.0/iflearn-1.0.0-py3-none-any.whl

---
+++ packages/iflearn/iflearn-1.0.0-py3-none-any.whl/iflearn.py
@@ -0,0 +1,11 @@
+def ifab():
+	textina = input("Number for var a ")
+	textinb = input("Number for var b ")
+
+	a = int(textina)
+	b = int(textinb)
+
+	if a > b:
+		print("a > b")
+	elif a < b:
+		print("a < b")
