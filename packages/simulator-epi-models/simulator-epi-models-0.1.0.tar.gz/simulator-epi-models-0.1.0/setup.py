5dbd7fece675979a544708f3f154e1f484cd88db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:21 +0000

    Add package simulator-epi-models/0.1.0/simulator-epi-models-0.1.0.tar.gz

---
+++ packages/simulator-epi-models/simulator-epi-models-0.1.0.tar.gz/simulator-epi-models-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="simulator-epi-models",
+    version="0.1.0",
+    description="Epi models used by the Simulator web app",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIforGoodSimulator/epi-models",
+    author="Crisis Modelling Devs",
+    author_email="billy@aiforgoodsimulator.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "scipy",
+        "numpy",
+        "pandas",
+    ],
+    zip_safe=False,
+)
