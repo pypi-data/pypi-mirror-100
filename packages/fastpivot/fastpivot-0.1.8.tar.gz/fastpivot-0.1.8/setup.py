56aba7f558962751c73e7bdc4483075eb44a6c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:13 +0000

    Add package fastpivot/0.1.8/fastpivot-0.1.8.tar.gz

---
+++ packages/fastpivot/fastpivot-0.1.8.tar.gz/fastpivot-0.1.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    ext_modules = cythonize("src/fastpivot/pivot/pivot.pyx", language="c++"),
+    packages=find_packages(),
+    include_dirs=[numpy.get_include(), 'src/fastpivot/pivot'],
+    install_requires=[
+        'numpy>=1.19.0',
+        'pandas>=1.0.0'
+        'scipy>=1.6.0'
+    ]
+)
