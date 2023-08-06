6cdfdf5db73f41a2a38816032e2af1255306f438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:21 +0000

    Add package scinet-josugoar/0.5.0/scinet-josugoar-0.5.0.tar.gz

---
+++ packages/scinet-josugoar/scinet-josugoar-0.5.0.tar.gz/scinet-josugoar-0.5.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="scinet-josugoar",
+    version="0.5.0",
+    description="Network science abstract data types",
+    author="josugoar",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/josugoar/scinet",
+    download_url="https://github.com/josugoar/scinet/archive/v0.5.0.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="algorithms data-structures docker graph library pipenv python unittest",
+    project_urls={"Source": "https://github.com/josugoar/scinet"},
+    python_requires=">=3.8",
+)
