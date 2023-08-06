212ad07425c87854c56cdfa3d205d5b2bc1a90e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:48 +0000

    Add package hi-hello/1.0.4/hi-hello-1.0.4.tar.gz

---
+++ packages/hi-hello/hi-hello-1.0.4.tar.gz/hi-hello-1.0.4/README.md
@@ -0,0 +1,23 @@
+### Hi Hello!
+
+Just a very nice library that says hello to you.
+
+#### Installation
+* Install the **Hi Hello** package for Python.
+
+        pip install hi-hello
+
+* Print a new word while starting up your program (or any other time)
+
+        const learnAWord = require('learn-a-word');
+        
+        // Print a new word 
+        learnAWord.printWord();
+
+* Output
+
+        ================== WORD OF THE DAY ==================
+        insatiable
+        not easily satisfied; unquenchable
+        =====================================================
+
---
+++ packages/hi-hello/hi-hello-1.0.4.tar.gz/hi-hello-1.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hi-hello",
+    version="1.0.4",
+    author="Pru",
+    author_email="pru@spike.sh",
+    description="Just saying hello.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://spike.sh",
+    install_requires=[],
+    package_dir={"": "src"},
+    #packages=['src/hi_hello'],
+    # package_data={
+    #     # 'needle_sdk': ['core/classes.py', 'core/needle_app.py', 'core/utilities.py', 'core/wrappers.py',
+    #     #                'core/libinjection2/linux/_libinjection.so', 'core/libinjection2/linux/libinjection.py',
+    #     #                'core/libinjection2/mac_x86_64/_libinjection.so',
+    #     #                'core/libinjection2/mac_x86_64/libinjection.py',
+    #     #                'core/data/js_event', 'core/data/unix_cmd', 'core/data/scan']
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
