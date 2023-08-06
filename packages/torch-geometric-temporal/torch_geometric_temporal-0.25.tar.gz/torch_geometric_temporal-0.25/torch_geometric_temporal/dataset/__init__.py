f61400a5a9b6a96199fda6e68b2ce43b4e3bd72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:20 +0000

    Add package torch-geometric-temporal/0.25/torch_geometric_temporal-0.25.tar.gz

---
+++ packages/torch-geometric-temporal/torch_geometric_temporal-0.25.tar.gz/torch_geometric_temporal-0.25/torch_geometric_temporal/dataset/__init__.py
@@ -0,0 +1,7 @@
+from .chickenpox import ChickenpoxDatasetLoader
+from .pedalme import PedalMeDatasetLoader
+from .metr_la import METRLADatasetLoader
+from .pems_bay import PemsBayDatasetLoader
+from .wikimath import WikiMathsDatasetLoader
+from .windmill import WindmillOutputDatasetLoader
+from .encovid import EnglandCovidDatasetLoader
