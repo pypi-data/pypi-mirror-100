0dd4eb42d34ae9ac331d75e56a0da755f2f10bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:10 +0000

    Add package calendarutil/2021.4.1/calendarutil-2021.4.1-py3-none-any.whl

---
+++ packages/calendarutil/calendarutil-2021.4.1-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,67 @@
+from datetime import date
+
+## First
+fmom = date(2021, 1, 4)
+ftum = date(2021, 1, 5)
+fwem = date(2021, 1, 6)
+fthm = date(2021, 1, 7)
+ffrm = date(2021, 1, 1)
+fsam = date(2021, 1, 2)
+fsum = date(2021, 1, 3)
+
+## Second
+smom = date(2021, 1, 11)
+stum = date(2021, 1, 12)
+swem = date(2021, 1, 13)
+sthm = date(2021, 1, 14)
+sfrm = date(2021, 1, 8)
+ssam = date(2021, 1, 9)
+ssum = date(2021, 1, 10)
+
+## Third
+tmom = date(2021, 1, 18)
+ttum = date(2021, 1, 19)
+twem = date(2021, 1, 20)
+tthm = date(2021, 1, 21)
+tfrm = date(2021, 1, 15)
+tsam = date(2021, 1, 16)
+tsum = date(2021, 1, 17)
+
+## Fourth
+frmom = date(2021, 1, 25)
+frtum = date(2021, 1, 26)
+frwem = date(2021, 1, 27)
+frthm = date(2021, 1, 28)
+frfrm = date(2021, 1, 22)
+frsam = date(2021, 1, 23)
+frsum = date(2021, 1, 24)
+
+## Last
+lmom = date(2021, 1, 25)
+ltum = date(2021, 1, 26)
+lwem = date(2021, 1, 27)
+lthm = date(2021, 1, 28)
+lfrm = date(2021, 1, 29)
+lsam = date(2021, 1, 30)
+lsum = date(2021, 1, 31)
+
+## Last day of month
+ldom = [date(2021, 1, 31), date(2021, 2, 28), date(2021, 3, 31), date(2021, 4, 30), date(2021, 5, 31), date(2021, 6, 30), date(2021, 7, 31), date(2021, 8, 31), date(2021, 9, 30), date(2021, 10, 31), date(2021, 11, 30), date(2021, 12, 31)]
+
+## First weekday of month
+fwdom = [date(2021, 1, 1), date(2021, 2, 1), date(2021, 3, 1), date(2021, 4, 1), date(2021, 5, 3), date(2021, 6, 1), date(2021, 7, 1), date(2021, 8, 2), date(2021, 9, 1), date(2021, 10, 1), date(2021, 11, 1), date(2021, 12, 1)]
+
+## Last weekday of month
+lwdom = [date(2021, 1, 29), date(2021, 2, 26), date(2021, 3, 31), date(2021, 4, 30), date(2021, 5, 31), date(2021, 6, 30), date(2021, 7, 30), date(2021, 8, 31), date(2021, 9, 30), date(2021, 10, 29), date(2021, 11, 30), date(2021, 12, 31)]
+
+## First day of quarter
+fdoq = [date(2021, 1, 1), date(2021, 4, 1), date(2021, 7, 1), date(2021, 10, 1)]
+
+## Last day of quarter
+ldoq = [date(2021, 3, 31), date(2021, 6, 30), date(2021, 9, 30), date(2021, 12, 31)]
+
+## First weekday of quarter
+fwdoq = [date(2021, 1, 1), date(2021, 4, 1), date(2021, 7, 1), date(2021, 10, 1)]
+
+## Last weekday of quarter
+lwdoq = [date(2021, 3, 31), date(2021, 6, 30), date(2021, 9, 30), date(2021, 12, 31)]
