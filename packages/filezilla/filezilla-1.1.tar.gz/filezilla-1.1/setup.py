351596b968cf7153965753ffb70fe37bcc8afcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package filezilla/1.1/filezilla-1.1.tar.gz

---
+++ packages/filezilla/filezilla-1.1.tar.gz/filezilla-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name = 'filezilla',
+    version = '1.1',
+    scripts = ['./script/filezilla'],
+    author = 'Gaurav Arora',
+    description = ('File manager'),
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    packages = ['filezilla'],
+    install_requires=[
+        'setuptools',
+        'PyPDF2 >= 1.26.0',
+        'Pillow >= 7.0.0'
+    ],
+    python_requires='>=3.7'
+)
