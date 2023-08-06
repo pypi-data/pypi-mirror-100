70965ebd72e76a473b227500bf1b4793b7f5fea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:57 +0000

    Add package dbaas-base-provider/0.0.1/dbaas-base-provider-0.0.1.tar.gz

---
+++ packages/dbaas-base-provider/dbaas-base-provider-0.0.1.tar.gz/dbaas-base-provider-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from dbaas_base_provider.version import Version
+
+
+setuptools.setup(name='dbaas-base-provider',
+                 version=Version('0.0.1').number,
+                 description='Base for DBaaS providers',
+                 long_description=open('README.md').read().strip(),
+                 author='DBaaS Team',
+                 author_email='db@g.globo',
+                 url='',
+                 py_modules=['dbaas_base_provider'],
+                 install_requires=[],
+                 license='BSD 3-Clause License',
+                 zip_safe=False,
+                 keywords='dbaas',
+                 classifiers=['Development Status :: 5 - Production/Stable'])
