e6a797afdf72761e8bd425fdb1bcd410f1986ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package esparto/0.1.0.dev1/esparto-0.1.0.dev1.tar.gz

---
+++ packages/esparto/esparto-0.1.0.dev1.tar.gz/esparto-0.1.0.dev1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['esparto']
+
+package_data = \
+{'': ['*'], 'esparto': ['templates/*']}
+
+install_requires = \
+['Pillow>=7.0.0', 'jinja2>=2.10.1', 'markdown>=3.1']
+
+extras_require = \
+{'extras': ['beautifulsoup4>=4.9.3', 'prettierfier>=1.0.3']}
+
+setup_kwargs = {
+    'name': 'esparto',
+    'version': '0.1.0.dev1',
+    'description': 'Simple toolkit for building minimal Bootstrap pages.',
+    'long_description': None,
+    'author': 'Dominic Thorn',
+    'author_email': 'dominic.thorn@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
