853dd39ef5246f1531e7efab90e8762f0a30bec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:17 +0000

    Add package pypijhjoh/0.3.4/pypijhjoh-0.3.4-py3-none-any.whl

---
+++ packages/pypijhjoh/pypijhjoh-0.3.4-py3-none-any.whl/pypi_test_pkg/math.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+class Math:
+    def addition(a, b):
+        return a + b
+
+    def subtraction(a, b):
+        return a - b
+
+    # def inverse(matrix):
+    #     return np.linalg.inv(matrix)
