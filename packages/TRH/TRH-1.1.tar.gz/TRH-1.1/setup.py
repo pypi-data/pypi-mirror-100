3d26a221308b85b717c214dc2bf6eda24014b88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package TRH/1.1/TRH-1.1.tar.gz

---
+++ packages/TRH/TRH-1.1.tar.gz/TRH-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+
+
+VERSION = '1.1'
+DESCRIPTION = 'TypeRacer Hack Browser'
+
+# Setting up
+setup(
+    name="TRH",
+    version=VERSION,
+    author="TanmayArya1-p",
+    author_email="<tanmayarya2018@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['selenium', 'beautifulsoup4'],
+    keywords=['python', 'selenium', 'web-scraping', 'discord'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
