8de492f36fd619ffdf37e66a039e0d052269a1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:24 +0000

    Add package car-speed-detection/0.7.1/car_speed_detection-0.7.1.tar.gz

---
+++ packages/car-speed-detection/car_speed_detection-0.7.1.tar.gz/car_speed_detection-0.7.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='car_speed_detection',
+    version='0.7.1',
+    author='Shao-chieh Lien',
+    author_email='shaochiehlien@gmail.com',
+    packages=find_packages(),
+)
+
