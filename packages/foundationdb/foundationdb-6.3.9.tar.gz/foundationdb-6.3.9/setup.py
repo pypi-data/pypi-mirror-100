4dcc9a17913a976b3bd412fa848ed8ac720102be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:08 +0000

    Add package foundationdb/6.3.9/foundationdb-6.3.9.tar.gz

---
+++ packages/foundationdb/foundationdb-6.3.9.tar.gz/foundationdb-6.3.9/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+try:
+    with open("README.rst") as f:
+        long_desc = f.read()
+except:
+    long_desc = ""
+
+setup(name="foundationdb",
+      version="6.3.9",
+      author="FoundationDB",
+      author_email="foundationdb_ci@apple.com",
+      description="Python bindings for the FoundationDB database",
+      url="https://www.foundationdb.org",
+      packages=['fdb'],
+      package_data={'fdb': ["fdb/*.py"]},
+      long_description=long_desc,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Database',
+          'Topic :: Database :: Front-Ends'
+      ]
+      )
