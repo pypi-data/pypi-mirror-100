5924bb0d83651e487571a326369ada116187682d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:52 +0000

    Add package byteplug/0.1/byteplug-0.1-py3-none-any.whl

---
+++ packages/byteplug/byteplug-0.1-py3-none-any.whl/byteplug/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2020-2021 - Jonathan De Wachter
+#
+# This source file is part of Byteplug which is released under the OSL-3.0
+# license. Please refer to the LICENSE file that can be found at the root of
+# the project directory.
+#
+# Written by Jonathan De Wachter <dewachter.jonathan@gmail.com>, April 2021
