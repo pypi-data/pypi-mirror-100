7f58b6c0efe14f735299a218b1c5b020259df8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:27 +0000

    Add package medtools/0.0.1/medtools-0.0.1.tar.gz

---
+++ packages/medtools/medtools-0.0.1.tar.gz/medtools-0.0.1/README.txt
@@ -0,0 +1,2 @@
+
+Thank you for installing MedTools library, for any suggestions contact me at elhasnaouymed@gmail.com
---
+++ packages/medtools/medtools-0.0.1.tar.gz/medtools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="medtools",
+    version="0.0.1",
+    author='Mohamed El-Hasnaouy',
+    author_email='elhasnaouymed@gmail.com',
+    license='GNU GPLv3',
+    long_description=open('README.txt').read(),
+    packages=find_packages(),
+    install_requires=['']
+)
