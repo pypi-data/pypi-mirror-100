107089fe788b85c4b4dbdb5092b4a4dd47e9683b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:05 +0000

    Add package template-python-project/0.5.0/template_python_project-0.5.0-py3.8.egg

---
+++ packages/template-python-project/template_python_project-0.5.0-py3.8.egg/template_python_project/core/compute.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
+def multiply(value1, value2):
+    return value1 * value2
+
+def add(value1, value2):
+    return value1 + value2
