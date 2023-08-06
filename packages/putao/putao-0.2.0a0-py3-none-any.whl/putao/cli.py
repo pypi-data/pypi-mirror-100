9cdad34a7803b1360c5a75867c6c09b766c92764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:54 +0000

    Add package putao/0.2.0a0/putao-0.2.0a0-py3-none-any.whl

---
+++ packages/putao/putao-0.2.0a0-py3-none-any.whl/putao/cli.py
@@ -0,0 +1,134 @@
+# coding: utf8
+
+import functools
+import json
+import logging
+import pathlib
+import shutil
+import tempfile
+
+import click
+
+from . import core, backend, model, utau
+from .__version__ import __version__
+
+click.option = functools.partial(click.option, show_default=True)  # type: ignore
+
+
+@click.group()
+@click.version_option(__version__)
+def cli():
+    logging.basicConfig(level=logging.DEBUG)
+
+
+@cli.command("extract")
+@click.argument("zfile")
+@click.option("-t", "--target", default=".", help="where to extract the voicebank to")
+def v_extract(zfile, target):
+    """Extract the voicebank(s) in zfile."""
+
+    with tempfile.TemporaryDirectory() as _tempdir:
+        tempdir = pathlib.Path(_tempdir)
+
+        # extract to tempdir first
+        utau.extract(zfile, tempdir)
+
+        # find all folders with an oto.ini file
+        voicebanks = [p.parent for p in tempdir.rglob(utau.CONFIG_FILE)]
+        for count, voicebank in enumerate(voicebanks):
+            print(f"{count}: {voicebank.name}")
+
+        choice = click.prompt("Voicebank to use", 0)
+        voicebank_to_move = voicebanks[choice]
+
+        for path in voicebank_to_move.iterdir():
+            shutil.move(path, target)
+
+    print("done")
+
+
+def _p_new(name):
+    click.echo(f"Creating new project '{name}'.")
+
+    author = click.prompt("Author of project (your name)")
+
+    voicebank = click.prompt("Path to UTAU voicebank")
+
+    resampler = click.prompt(
+        "Choose a resampler to use",
+        type=click.Choice(list(model.RESAMPLERS)),
+        default="WorldResampler",
+    )
+
+    click.echo(f"Sucessfully created project '{name}'.")
+
+    return {
+        "name": name,
+        "author": author,
+        "voicebank": voicebank,
+        "resampler": resampler,
+        "putao_version": __version__,
+    }
+
+
+@cli.command("new")
+@click.argument("name")
+@click.option(
+    "-o", "--output", help="where to save new project (defaults to curdir/(name).json)"
+)
+def p_new(name, output):
+    """Create a new project through an interactive wizard."""
+
+    config = _p_new(name)
+    config_path = output or pathlib.Path(".") / f"{name}.json"
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+@cli.command("import")
+@click.argument("file")
+@click.argument("proj_file")
+@click.option(
+    "-f",
+    "--fmt",
+    type=click.Choice(backend.BACKENDS),
+    help="external file format",
+)
+def p_import(file, proj_file, fmt):
+    """Import an external file into an existing project in proj_file."""
+
+    file = pathlib.Path(file)
+
+    if not fmt:
+        # guess format using file extension
+        fmt = file.suffix[1:]
+
+    config = json.load(open(proj_file))
+
+    with file.open("rb") as f:
+        config["tracks"] = backend.load(f, fmt)
+
+    with open(proj_file, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+@cli.command("render")
+@click.argument("proj_file")
+@click.option("-o", "--output", default="render.wav", help="file to render to")
+@click.option(
+    "-f", "--frq", is_flag=True, help="regenerate frq files for the voicebank"
+)
+def p_render(proj_file, output, frq):
+    """Render a project file."""
+
+    proj = core.Project.load(proj_file)
+
+    if "frq" not in proj.config or frq:
+        click.echo(
+            f"generating frq files using {proj.resampler.__class__.__name__}, this might take a while..."
+        )
+        proj.resampler.gen_frq_all(force=frq)
+        proj.config["frq"] = True
+
+    proj.render(output)
+    proj.dump(proj_file)
---
+++ packages/putao/putao-0.2.0a0-py3-none-any.whl/putao/core.py
@@ -0,0 +1,263 @@
+# coding: utf8
+
+from __future__ import annotations
+
+import collections.abc as c_abc
+import json
+import logging
+import pathlib
+from typing import Any, Dict, IO, List, Optional, Union
+
+from pydub import AudioSegment
+
+from . import model, utau
+from .exceptions import TrackError, ProjectError
+
+_log = logging.getLogger("putao")
+
+
+class Track:
+    """A track (sequence of notes).
+
+    Args:
+        resampler: The resampler to use to render notes.
+    """
+
+    def __init__(self, resampler: model.Resampler):
+        self._notes: List[Union[model.Note, model.Rest]] = []
+        self.resampler = resampler
+
+    def note(self, phoneme: str, pitch: int, duration: int):
+        """Add a note.
+
+        Args:
+            phoneme: The syllable to sing.
+            pitch: The absolute semitone value of the note from A0, i.e 49 (A4).
+            duration: How long to hold the note for (in miliseconds).
+        """
+        if phoneme not in self.resampler.voicebank:
+            raise TrackError(f"'{phoneme}' does not exist in the voicebank")
+
+        self._notes.append(
+            model.Note(duration, pitch, self.resampler.voicebank[phoneme])
+        )
+
+    def rest(self, duration: int):
+        """Add a rest (break in-between notes).
+
+        Args:
+            duration: How long to rest for.
+        """
+        self._notes.append(model.Rest(duration))
+
+    def render(self) -> AudioSegment:
+        """Render all notes sequentially to an audio segment.
+
+        Returns:
+            The audio segment.
+        """
+        track_render = AudioSegment.empty()
+        total = len(self._notes)
+
+        for count, note in enumerate(self._notes, start=1):
+
+            timestamp = len(track_render)
+            overlap = 0
+
+            next_note = None
+
+            try:
+                next_note = self._notes[count + 1]
+                if isinstance(next_note, model.Note):
+                    overlap = next_note.entry.overlap
+            except IndexError:
+                # no more notes
+                pass
+
+            _log.debug(
+                "[track] rendering note %s of %s (track duration: %ss)",
+                count,
+                total,
+                timestamp / 1000,
+            )
+
+            try:
+                render = self.resampler.render(note, next_note)
+            except Exception as e:
+                _log.critical(f"[track] failed to render note {count} ({note})!!!")
+                raise e
+
+            # extend final render (so overlay won't be truncated)
+            track_render += AudioSegment.silent(len(render) - overlap)
+
+            # overlap this note into previous one
+            track_render = track_render.overlay(render, position=timestamp - overlap)
+
+        return track_render
+
+    def dump(self) -> List[dict]:
+        notes = []
+
+        for note in self._notes:
+            notes.append(note.dump())
+
+        return notes
+
+    def load(self, note_dumps: List[dict]):
+        self._notes.clear()
+
+        for dump in note_dumps:
+            if dump["type"] == "note":
+                self.note(*[dump[arg] for arg in ("phoneme", "pitch", "duration")])
+            elif dump["type"] == "rest":
+                self.rest(dump["duration"])
+
+
+class Project(c_abc.MutableMapping):
+    """A project (a.k.a song).
+
+    First create a new track:
+
+    proj = Project(".", 49)  # use the voicebank in curdir, pitched to C4
+    track = proj.new_track("lead")
+
+    If you want to access any existing tracks, use dict notation:
+
+    existing_track = proj["lead"]
+
+    You can also delete tracks:
+
+    del proj["lead"]
+
+    Args:
+        config: The project config (as a dict) to load.
+            If not given, a new project will be created using defaults.
+
+            Any config given should at least have these keys:
+
+            'voicebank': The path to the voicebank.
+                The voicebank must be in UTAU format, and must have a oto.ini file.
+            'resampler': The name of the resampler to use.
+                Available resamplers are in the dictionary model.RESAMPLERS.
+                If not given, defaults to 'WorldResampler'.
+
+    Raises:
+        ProjectError, if the resampler given by name does not exist.
+    """
+
+    DEFAULTS: Dict[str, Any] = {"voicebank": ".", "resampler": "WorldResampler"}
+
+    def __init__(self, config: Optional[dict] = None):
+
+        if config is None:
+            config = self.DEFAULTS.copy()
+
+        self.config = config
+        self.voicebank = utau.Voicebank(self.config["voicebank"])
+
+        try:
+            resampler_cls = model.RESAMPLERS[self.config["resampler"]]
+        except KeyError:
+            raise ProjectError(f"resampler {self.config['resampler']} does not exist")
+        else:
+            self.resampler = resampler_cls(self.voicebank)  # type:ignore
+
+        # load tracks
+        self.tracks: Dict[str, Track] = {}
+
+        if "tracks" in self.config:
+            for name, notes in self.config["tracks"].items():
+                track = self.new_track(name)
+                track.load(notes)
+
+    def __getitem__(self, name):
+        return self.tracks[name]
+
+    def __setitem__(self, name, value):
+        raise ProjectError("can't directly set tracks: use .new_track() instead")
+
+    def __delitem__(self, name):
+        del self.tracks[name]
+
+    def __iter__(self):
+        return iter(self.tracks)
+
+    def __len__(self):
+        return len(self.tracks)
+
+    def new_track(self, name: str) -> Track:
+        """Create a new track.
+
+        Args:
+            name: The track name.
+
+        Returns:
+            The newly created track.
+
+        Raises:
+            ProjectError, if it already exists.
+        """
+
+        if name in self.tracks:
+            raise ProjectError(f"track already exists: '{name}'")
+
+        track = Track(self.resampler)
+        self.tracks[name] = track
+
+        _log.debug("[project] created new track %s", name)
+
+        return track
+
+    def render(self, path: Union[str, pathlib.Path]):
+        """Render all tracks to a wavfile.
+
+        Args:
+            path: The file to render to.
+        """
+
+        project_render = AudioSegment.empty()
+        for name, track in self.tracks.items():
+            _log.info("[project] rendering track %s", name)
+            render = track.render()
+
+            project_len = len(project_render)
+            render_len = len(render)
+
+            if project_len < render_len:
+                project_render += AudioSegment.silent(render_len - project_len)
+
+            project_render = project_render.overlay(render)
+
+        project_render.export(path, format="wav")
+
+    @classmethod
+    def loads(cls, config: str) -> Project:
+        """Load a project from a JSON string."""
+
+        return cls(json.loads(config))
+
+    @classmethod
+    def load(cls, fp: Union[str, pathlib.Path, IO]) -> Project:
+        """Load a project from a file."""
+
+        if isinstance(fp, (str, pathlib.Path)):
+            fp = open(fp, "r")
+
+        return cls.loads(fp.read())
+
+    def dumps(self) -> str:
+        """Dump this project to a JSON string."""
+
+        config = {
+            **self.config,
+            "tracks": {name: track.dump() for name, track in self.tracks.items()},
+        }
+        return json.dumps(config, indent=4)
+
+    def dump(self, fp: Union[str, pathlib.Path, IO]):
+        """Dump this project to a file."""
+
+        if isinstance(fp, (str, pathlib.Path)):
+            fp = open(fp, "w")
+
+        fp.write(self.dumps())
