4b4c11c2207dcea129fb7a051d6f3bff2202946e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:58 +0000

    Add package covfee/0.0.1/covfee-0.0.1.tar.gz

---
+++ packages/covfee/covfee-0.0.1.tar.gz/covfee-0.0.1/covfee/client/utils.tsx
@@ -0,0 +1,73 @@
+import {message} from 'antd'
+
+import Constants from 'Constants'
+
+// read a cookie in the browser
+function getCookieValue(a: string) {
+    var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)')
+    return b ? b.pop() : null
+}
+
+function getUrlQueryParam(name: string) {
+    // querystring is after the hash
+    let search
+    if (window.location.hash.indexOf('?') !== -1) {
+        search = '?' + window.location.hash.split('?')[1]
+    } else {
+        search = window.location.search
+    }
+    const params = new URLSearchParams(search);
+    return params.get(name)
+}
+
+// error method that
+//   prints an message to screen in production environment
+//   prints and logs a detailed error in dev mode
+function myerror(msg: string, error?: any) {
+    if(Constants.env == 'production') {
+        message.error(msg + ' Please try again later or contact the organizer(s).')
+    } else {
+        if(error) {
+            console.error(error)
+            message.error(error.stack, 0)
+        }
+    }
+}
+
+function myinfo(msg: string) {
+    message.info(msg)
+}
+
+// fetch wrapper that appends the csrf_access_token cookie for authentication
+function fetcher(input: RequestInfo, options?: RequestInit) {
+    const cookie = getCookieValue('csrf_access_token')
+    const newOptions = { ...options}
+    if(cookie != null) {
+        newOptions.headers = {
+            ...newOptions.headers,
+            'X-CSRF-TOKEN': cookie
+        }
+    }
+    return fetch(input, newOptions)
+}
+
+// fetch then function that throws an error for error status codes
+const throwBadResponse = async (response: any) => {
+    if (!response.ok) {
+        const data = await response.json()
+        if(data.hasOwnProperty('msg')) {
+            throw Error(data.msg)
+        }
+        throw Error(response.statusText)
+    }
+    return await response.json()
+}
+
+export { 
+    fetcher, 
+    myerror, 
+    myinfo, 
+    getUrlQueryParam,
+    getCookieValue,
+    throwBadResponse
+}
