3567bec49d3af9d7acb635dec69880fffe5ec8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:19 +0000

    Add package mytestlib-mi417ya/1.0.0/mytestlib-mi417ya-1.0.0.tar.gz

---
+++ packages/mytestlib-mi417ya/mytestlib-mi417ya-1.0.0.tar.gz/mytestlib-mi417ya-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-mi417ya',
+    version='1.0.0',
+    url='https://github.com/FumiyaO/mytestlib',
+    author='Fumiya.O',
+    author_email='ayimuf0975@icloud.com',
+    description='My Test Library',
+    package=find_packages()
+)
