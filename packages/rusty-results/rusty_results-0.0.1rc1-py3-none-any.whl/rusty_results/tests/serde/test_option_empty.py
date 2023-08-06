95ade2dc80fd883aebf2940e40c556793563411d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:35 +0000

    Add package rusty-results/0.0.1rc1/rusty_results-0.0.1rc1-py3-none-any.whl

---
+++ packages/rusty-results/rusty_results-0.0.1rc1-py3-none-any.whl/rusty_results/tests/serde/test_option_empty.py
@@ -0,0 +1,31 @@
+import pydantic
+import pytest
+from rusty_results import Option, Empty
+import json
+
+
+class Model(pydantic.BaseModel):
+    optional_value: Option[str]
+
+
+TEST_MODEL_SERIALIZED = '{"optional_value": {}}'
+
+
+def test_serialize():
+    model = Model(optional_value=Empty())
+    assert model.json() == TEST_MODEL_SERIALIZED
+
+
+def test_deserialize():
+    model = Model(**json.loads(TEST_MODEL_SERIALIZED))
+    assert model == Model(optional_value=Empty())
+
+
+def test_deserialize_fails():
+    wrong_values = [
+        10,
+        {"foo": 10},
+    ]
+    with pytest.raises(pydantic.ValidationError):
+        for value in wrong_values:
+            Model(foo=value)
---
+++ packages/rusty-results/rusty_results-0.0.1rc1-py3-none-any.whl/rusty_results/tests/serde/test_result_ok.py
@@ -0,0 +1,31 @@
+import pydantic
+import pytest
+from rusty_results import Result, Ok
+import json
+
+
+class Model(pydantic.BaseModel):
+    result_value: Result[int, str]
+
+
+TEST_MODEL_SERIALIZED = '{"result_value": {"Ok": 10}}'
+
+
+def test_serialize():
+    model = Model(result_value=Ok(10))
+    assert model.json() == TEST_MODEL_SERIALIZED
+
+
+def test_deserialize():
+    model = Model(**json.loads(TEST_MODEL_SERIALIZED))
+    assert model == Model(result_value=Ok(10))
+
+
+def test_deserialize_fails():
+    wrong_values = [
+        10,
+        {"foo": 10},
+    ]
+    with pytest.raises(pydantic.ValidationError):
+        for value in wrong_values:
+            Model(foo=value)
