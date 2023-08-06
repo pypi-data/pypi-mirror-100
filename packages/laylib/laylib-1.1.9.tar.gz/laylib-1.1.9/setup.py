90dead31e4be7f30d41356a4840a9ba79afc7915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:29 +0000

    Add package laylib/1.1.9/laylib-1.1.9.tar.gz

---
+++ packages/laylib/laylib-1.1.9.tar.gz/laylib-1.1.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+_version_ = "1.1.9"
+_url_ = "https://github.com/Layto888/laylib"
+
+with open("README.rst", "r") as fh:
+    _long_description = fh.read()
+
+setuptools.setup(
+    name="laylib",
+    version=_version_,
+    license="MIT License",
+    description="A 2-D engine for Python and pygame",
+    long_description=_long_description,
+	long_description_content_type='text/markdown',
+    author="Amardjia Amine",
+    author_email="amardjia.amine@gmail.com",
+    url=_url_,
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
