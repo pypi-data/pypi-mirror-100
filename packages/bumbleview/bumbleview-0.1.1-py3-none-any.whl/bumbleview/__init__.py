baa71fabbde39e60da3c7e8bbfb3179d989c305f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:43 +0000

    Add package bumbleview/0.1.1/bumbleview-0.1.1-py3-none-any.whl

---
+++ packages/bumbleview/bumbleview-0.1.1-py3-none-any.whl/bumbleview/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# version
+__version__ = "0.1.1"
+
+# run
+import convwale
+print("Welcome to the bumbleview package. Here you can covnert emission spectra to its reaction in an insect eye.")
