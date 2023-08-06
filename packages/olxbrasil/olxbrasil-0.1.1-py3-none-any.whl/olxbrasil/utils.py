26df6d55b182c74e84d8a6fac281cda508353ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:36 +0000

    Add package olxbrasil/0.1.1/olxbrasil-0.1.1-py3-none-any.whl

---
+++ packages/olxbrasil/olxbrasil-0.1.1-py3-none-any.whl/olxbrasil/utils.py
@@ -0,0 +1,53 @@
+from typing import Optional, Any
+
+from olxbrasil.constants import (
+    ALLOWED_BOOLEAN_FILTERS,
+    ALLOWED_DYNAMIC_FILTERS,
+)
+
+
+def format_price(price: Optional[str] = None) -> float:
+    try:
+        price_value = (
+            price.replace("R$ ", "").replace(".", "").replace(",", ".")
+        )
+        return float(price_value)
+    except (ValueError, AttributeError):
+        return 0
+
+
+def build_boolean_parameters(*olx_filters: str) -> dict:
+    params = {}
+    for olx_filter in olx_filters:
+        if olx_filter not in ALLOWED_BOOLEAN_FILTERS:
+            continue
+
+        for key, value in ALLOWED_BOOLEAN_FILTERS[olx_filter].items():
+            append_parameter(params, key, value)
+
+    return params
+
+
+def build_search_parameters(**parameters) -> dict:
+    params = {}
+    for search_filter, value in parameters.items():
+        if search_filter not in ALLOWED_DYNAMIC_FILTERS:
+            continue
+
+        chosen_value = value
+        chosen_filter = ALLOWED_DYNAMIC_FILTERS[search_filter]
+        if chosen_filter.get("has_parse_dict"):
+            chosen_value = chosen_filter["parse_dict"][value]
+
+        append_parameter(params, chosen_filter["filter_name"], chosen_value)
+
+    return params
+
+
+def append_parameter(parameters: dict, parameter: str, value: Any):
+    if parameter in parameters and not isinstance(parameters[parameter], list):
+        parameters[parameter] = sorted([parameters[parameter], value])
+    elif parameter not in parameters:
+        parameters[parameter] = value
+    else:
+        parameters[parameter].append(value)
