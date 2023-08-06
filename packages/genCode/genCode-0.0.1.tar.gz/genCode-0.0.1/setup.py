dea596eda251d2b4b3925e2d67dcf8c6038c2149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:29 +0000

    Add package genCode/0.0.1/genCode-0.0.1.tar.gz

---
+++ packages/genCode/genCode-0.0.1.tar.gz/genCode-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# with open("./README.md", "rb") as fh:
+#     long_description = fh.read()
+long_description = "生成代码"
+
+setup(
+    name='genCode',
+    version='0.0.1',
+    description='生成代码',
+    author='hammer',
+    author_email='liuzhuogood@foxmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    # scripts=['scripts/gcode.py'],
+    # package_data={'db_hammer': ['README.md', 'LICENSE']},
+    install_requires=["pydantic", 'db-hammer', 'PyMySQL', 'cx_Oracle'],
+    entry_points={
+        'console_scripts': ['driver=gcode.main:run'], }
+)
