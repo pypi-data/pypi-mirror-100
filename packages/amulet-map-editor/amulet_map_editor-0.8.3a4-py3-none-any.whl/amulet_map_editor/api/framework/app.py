3cdb059736cdf9bbb415aea423df3867bec47a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:58 +0000

    Add package amulet-map-editor/0.8.3a4/amulet_map_editor-0.8.3a4-py3-none-any.whl

---
+++ packages/amulet-map-editor/amulet_map_editor-0.8.3a4-py3-none-any.whl/amulet_map_editor/api/framework/app.py
@@ -0,0 +1,26 @@
+import wx
+from .amulet_ui import AmuletUI
+# from amulet_map_editor import log
+# import locale
+
+
+class AmuletApp(wx.App):
+    def OnInit(self):
+        # log.info(str(locale.getdefaultlocale()))
+        # log.info(str(locale.getlocale()))
+        # self.locale = wx.Locale(wx.LANGUAGE_DEFAULT)
+        self._frame = AmuletUI(None)
+        self.SetTopWindow(self._frame)
+        self._frame.Show()
+        return True
+
+    # def InitLocale(self):
+    #     # https://discuss.wxpython.org/t/what-is-wxpython-doing-to-the-locale-to-makes-pandas-crash/34606/18
+    #     self.ResetLocale()
+    #     import locale
+    #     lang, enc = locale.getdefaultlocale()
+    #     self._initial_locale = wx.Locale(lang, lang[:2], lang)
+    #     try:
+    #         locale.setlocale(locale.LC_ALL, lang)
+    #     except locale.Error:
+    #         locale.setlocale(locale.LC_ALL, (lang, enc))
