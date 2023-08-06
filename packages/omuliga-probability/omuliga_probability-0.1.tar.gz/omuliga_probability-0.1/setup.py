71783ab8fe79998091d86980b825cadf4833b595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:19 +0000

    Add package omuliga-probability/0.1/omuliga_probability-0.1.tar.gz

---
+++ packages/omuliga-probability/omuliga_probability-0.1.tar.gz/omuliga_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='omuliga_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['omuliga_probability'],
+      author ='Olivia Nabbosa',
+      author_email = 'olivia.nabbosa@outlook.com',
+      zip_safe=False)
