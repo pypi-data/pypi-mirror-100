03e594f7247184133d5a243e0ac19904ed0ea43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:27 +0000

    Add package paper2remarkable/0.9.2/paper2remarkable-0.9.2-py3-none-any.whl

---
+++ packages/paper2remarkable/paper2remarkable-0.9.2-py3-none-any.whl/paper2remarkable/providers/aaai.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Provider for AAAI
+
+Author: G.J.J. van den Burg
+License: See LICENSE file
+Copyright: 2021, G.J.J. van den Burg
+
+"""
+
+import re
+
+from ._base import Provider
+from ._info import Informer
+from ..exceptions import URLResolutionError
+
+class AAAIInformer(Informer):
+    
+    raise NotImplementedError
+
+
+class AAAI(Provider):
+    pass
+
+
