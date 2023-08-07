fcb9b0e924c1206d224d6fbde6f60f0363eddcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:51 +0000

    Add package bertforyou/1.0.0/bertforyou-1.0.0.tar.gz

---
+++ packages/bertforyou/bertforyou-1.0.0.tar.gz/bertforyou-1.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+
+setup(
+    name='bertforyou',
+    version='1.0.0',
+    description='a package for your bert using',
+    long_description='使用keras实现你的bert项目',
+    author='xiaoguzai',
+    author_email='474551240@qq.com',
+    license='apache2.0',
+    url='http://github.com/liangdongchang/django-xadmin',
+    download_url='http://github.com/liangdongchang/django-xadmin/master.zip',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'tensorflow>=2.4.0rc0'
+    ]
+)
