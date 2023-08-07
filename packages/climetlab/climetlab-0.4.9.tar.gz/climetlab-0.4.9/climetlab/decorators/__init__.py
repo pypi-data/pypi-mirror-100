92d3243bfa4583a93fe75d3258dc98f93ca34a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:11 +0000

    Add package climetlab/0.4.9/climetlab-0.4.9.tar.gz

---
+++ packages/climetlab/climetlab-0.4.9.tar.gz/climetlab-0.4.9/climetlab/decorators/__init__.py
@@ -0,0 +1,40 @@
+# (C) Copyright 2021 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import threading
+
+
+def dict_args(func):
+    def wrapped(*args, **kwargs):
+        m = []
+        p = {}
+        for q in args:
+            if isinstance(q, dict):
+                p.update(q)
+            else:
+                m.append(q)
+        p.update(kwargs)
+        return func(*m, **p)
+
+    wrapped.__name__ = func.__name__
+
+    return wrapped
+
+
+LOCK = threading.RLock()
+
+
+def locked(func):
+    def wrapped(*args, **kwargs):
+        with LOCK:
+            return func(*args, **kwargs)
+
+    wrapped.__name__ = func.__name__
+
+    return wrapped
