3bf3196c53dbae67ea5f270a2af8a9b19ae322c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:07 +0000

    Add package template-python-project/0.5.1/template_python_project-0.5.1.tar.gz

---
+++ packages/template-python-project/template_python_project-0.5.1.tar.gz/template_python_project-0.5.1/template_python_project/__main__.py
@@ -0,0 +1,8 @@
+# pylint: disable=no-value-for-parameter
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
+from . import main
+
+if __name__ == '__main__':
+    main.main()
