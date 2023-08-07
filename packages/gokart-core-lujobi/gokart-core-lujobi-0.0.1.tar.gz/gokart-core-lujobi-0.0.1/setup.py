8688450cbbb573b3dd8660d5472e7e24cf63dd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:43 +0000

    Add package gokart-core-lujobi/0.0.1/gokart-core-lujobi-0.0.1.tar.gz

---
+++ packages/gokart-core-lujobi/gokart-core-lujobi-0.0.1.tar.gz/gokart-core-lujobi-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def get_version(filename):
+    import ast
+
+    version = None
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError('No version found in %r.' % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+
+install_requires = [
+]
+
+module = 'gokart-core'
+package = 'gokart-core-lujobi'
+src = 'src'
+
+version = get_version(filename=f'{src}/{module}/__init__.py')
+
+setup(
+    name=package,
+    package_dir={'': src},
+    packages=[module],
+    version=version,
+    author='IDSC Frazzoli',
+    author_email='azanardi@ethz.ch',
+    url='https://github.com/idsc-frazzoli/gokart-core',
+    zip_safe=False,
+    install_requires=install_requires,
+)
---
+++ packages/gokart-core-lujobi/gokart-core-lujobi-0.0.1.tar.gz/gokart-core-lujobi-0.0.1/src/gokart-core/__init__.py
@@ -0,0 +1,4 @@
+import gokart_model as gokart_model
+from .test import uelifunc as peterfunc
+__version__ = "0.0.1"
+
