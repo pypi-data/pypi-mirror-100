86f8c21b8b84125359a713a6cbebde43028b4386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:29 +0000

    Add package rundeckpy/0.1.9/rundeckpy-0.1.9.tar.gz

---
+++ packages/rundeckpy/rundeckpy-0.1.9.tar.gz/rundeckpy-0.1.9/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['rundeckpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['paramiko>=2.7.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'rundeckpy',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Thiago Takayama',
+    'author_email': 'thiago@takayama.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
