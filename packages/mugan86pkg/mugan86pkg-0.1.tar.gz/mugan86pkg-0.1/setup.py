30741b16707dedcba0e4b30b709d1da3daa72ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:23 +0000

    Add package mugan86pkg/0.1/mugan86pkg-0.1.tar.gz

---
+++ packages/mugan86pkg/mugan86pkg-0.1.tar.gz/mugan86pkg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mugan86pkg',
+version='0.1',
+description='Testing installation of Package',
+url='https://anartz-mugika.com',
+author='Anartz Mugika Ledo',
+author_email='mugan86@gmail.com',
+license='MIT',
+packages=['mugan86pkg'],
+zip_safe=False)
