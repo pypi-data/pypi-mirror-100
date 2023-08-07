ce39b604cfe0bc875367b557bee0d534062a8dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:16 +0000

    Add package gaussian-binomial-distributions-FT/0.1/gaussian_binomial_distributions_FT-0.1.tar.gz

---
+++ packages/gaussian-binomial-distributions-FT/gaussian_binomial_distributions_FT-0.1.tar.gz/gaussian_binomial_distributions_FT-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_distributions_FT',
+      version='0.1',
+      description='gaussian_binomial_distributions',
+      packages=['gaussian_binomial_distributions_FT'],
+      author = 'Funing Tian',
+      zip_safe=False)
