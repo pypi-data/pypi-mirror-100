df9df70ce1c5f6c4f9566cdb15fd0ed7c7aace0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:29 +0000

    Add package supersql/2021.0.8/supersql-2021.0.8-py3-none-any.whl

---
+++ packages/supersql/supersql-2021.0.8-py3-none-any.whl/supersql/datatypes/__init__.py
@@ -0,0 +1,14 @@
+
+from supersql.datatypes.numeric import (
+    Bigint,
+    Decimal,
+    Double,
+    Integer,
+    Money,
+    Real,
+    Serial,
+    Smallint,
+)
+
+from supersql.datatypes.string import String, Varchar
+from supersql.datatypes.boolean import Boolean
