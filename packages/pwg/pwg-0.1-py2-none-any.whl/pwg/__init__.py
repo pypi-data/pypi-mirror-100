24e35d71125bcbfce22d3cd44b728e5bfbbb06e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:23 +0000

    Add package pwg/0.1/pwg-0.1-py2-none-any.whl

---
+++ packages/pwg/pwg-0.1-py2-none-any.whl/pwg/__init__.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('ah')
+
+def hmeasure(x1, x2, y, **kwargs):
+    pass
+
+def vmeasure(x, y1, y2, **kwargs):
+    pass
+
+def datapointer(x, y, xtext=None, ytext=None, **kwargs):
+    pass
+
+plt.hmeasure = hmeasure
+plt.vmeasure = vmeasure
+plt.datapointer = datapointer
+
