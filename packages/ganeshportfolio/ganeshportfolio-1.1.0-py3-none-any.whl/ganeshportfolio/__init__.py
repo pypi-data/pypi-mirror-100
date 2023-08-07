069b2dd90ca717bfd1512c5151b41244fc6c9b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:58 +0000

    Add package ganeshportfolio/1.1.0/ganeshportfolio-1.1.0-py3-none-any.whl

---
+++ packages/ganeshportfolio/ganeshportfolio-1.1.0-py3-none-any.whl/ganeshportfolio/__init__.py
@@ -0,0 +1,9 @@
+def author_name():
+    return "Ganesh"
+def author_education():
+    return "Purusing Engineering Pre-final Year"
+def author_socialmedia():
+    return "https://www.linkedin.com/in/ganeshuthiravasagam/"
+def author_github():
+    return "https://github.com/Ganeshuthiravasagam/"
+
