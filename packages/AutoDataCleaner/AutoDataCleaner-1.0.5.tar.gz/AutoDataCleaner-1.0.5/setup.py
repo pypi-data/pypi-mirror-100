acb09a046d3fce1b6b8cda1d4f20eef64dda5542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:32 +0000

    Add package AutoDataCleaner/1.0.5/AutoDataCleaner-1.0.5.tar.gz

---
+++ packages/AutoDataCleaner/AutoDataCleaner-1.0.5.tar.gz/AutoDataCleaner-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+from AutoDataCleaner import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='AutoDataCleaner',
+    version=__version__,
+
+    url='https://github.com/sinkingtitanic/AutoDataCleaner',
+    author='Elhoussine Talab',
+    author_email='ofcourse7878@gmail.com',
+    description="Simple and automatic data cleaning in one line of code! It performs One Hot Encoding, Cleans Dirty/Empty Values, Normalizes values and Removes unwanted columns all in one line of code. Get your data ready for model training and fitting quickly.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+    install_requires=[
+    'pandas',
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Database"
+    ]
+)
