964181f31ce7ccec8db596153a8ae3035c75f023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:35 +0000

    Add package aimsprop/0.2.0/aimsprop-0.2.0-py3-none-any.whl

---
+++ packages/aimsprop/aimsprop-0.2.0-py3-none-any.whl/aimsprop/vis_aims/render.py
@@ -0,0 +1,22 @@
+import os
+import re
+import sys
+
+import numpy as np
+
+
+def render():
+
+    """Runs vmd and renders images
+
+    Params:
+
+    Returns:
+
+    """
+
+    # Requires:
+    # Names of vmd and tcl scripts
+    # Options:
+    # render without opening vmd
+    # open vmd before sourcing file
