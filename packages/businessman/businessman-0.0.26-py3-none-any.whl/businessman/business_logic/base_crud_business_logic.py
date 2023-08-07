a9a177f513036adbe6b2048acecc2f1c70ae5609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:00 +0000

    Add package businessman/0.0.26/businessman-0.0.26-py3-none-any.whl

---
+++ packages/businessman/businessman-0.0.26-py3-none-any.whl/businessman/business_logic/base_crud_business_logic.py
@@ -0,0 +1,80 @@
+from typing import List, Type, Union
+
+from mongoengine import Document
+
+from businessman.errors import NotFound
+from .base_business_logic import BaseBusinessLogic
+
+
+class BaseCRUDBusinessLogic(BaseBusinessLogic):
+	model: Type[Document] = Type[Document]
+
+	#
+	# @classmethod
+	# def get_form(cls):
+	# 	# TODO get form from model
+	# 	form = cls.form
+	# 	if not form:
+	# 		pass
+	# 	return form
+
+	@classmethod
+	def get_all(cls) -> List[model]:
+		"""
+		:return: all object of model
+		"""
+		return cls.model.objects.all()
+
+	# @classmethod
+	# def get_all_pagination(cls, after: Union[str, int] = None, first: int = 0, ) -> Dict[str, Any]:
+	# 	"""
+	# 	:param first: count of object after $after
+	# 	:param after: first object as courser
+	# 	:return: List of [model] object paginated
+	#
+	# 	"""
+	# 	# TODO from $after object count of $first
+	# 	# TODO It must Contain filter for object
+	# 	# TODO add [hasNextPage], [hasPreviousPage], [Count]
+	# 	# TODO add filtering for object
+	# 	res = {
+	# 		"hasNextPage": True,
+	# 		"hasPreviousPage": True,
+	# 		"data": []
+	# 	}
+	# 	return res
+
+	@classmethod
+	def get_by_id(cls, pk: Union[str, int]) -> model:
+		selected_users = cls.model.objects(pk=pk)
+		if len(selected_users) > 0:
+			return selected_users[0]
+		raise NotFound(pk, cls.model)
+
+	@classmethod
+	def create(cls, **kwargs) -> model:
+		item = cls.model(**kwargs)
+		return item.save()
+
+	# @classmethod
+	# def create(cls, payload: Dict) -> model:
+	# 	# TODO check with form
+	# 	# TODO create new object
+	#
+	# 	# model_form = cls.get_form()
+	# 	# form = model_form(payload)
+	# 	# if form.validate():
+	# 	# 	pass
+	# 	pass
+
+	@classmethod
+	def update(cls, pk: str, **item_info) -> model:
+		item = cls.get(pk)
+		for key, val in item_info.items():
+			setattr(item, key, val)
+		return item.save()
+
+	@classmethod
+	def delete(cls, pk: str) -> None:
+		item = cls.get(pk)
+		item.delete()
