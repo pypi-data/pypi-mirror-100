a8c2cd999a23a68095efc342bd965e6568247653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:09 +0000

    Add package purevpn-event-bus/0.0.3/purevpn_event_bus-0.0.3.tar.gz

---
+++ packages/purevpn-event-bus/purevpn_event_bus-0.0.3.tar.gz/purevpn_event_bus-0.0.3/purevpn_event_bus/__init__.py
@@ -0,0 +1,4 @@
+from src.event import Event
+from src.event_handler import EventHandler
+from src.event_logger import EventLogger
+from src.event_registry import EventRegistry
---
+++ packages/purevpn-event-bus/purevpn_event_bus-0.0.3.tar.gz/purevpn_event_bus-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import setuptools
+
+
+setup(
+    name='purevpn_event_bus',
+    packages=['purevpn_event_bus'],
+    version='0.0.3',
+    license='MIT',
+    description='',
+    author='Syed Hammad Ahmed',
+    author_email='syed.ahmed@purevpn.com',
+    url='https://github.com/sha-gaditek/purevpn_event_bus',
+    download_url='https://github.com/sha-gaditek/purevpn_event_bus/archive/refs/tags/0.0.3.tar.gz',
+    keywords=[
+        'DDD',
+        'Event Bus'
+    ],
+    install_requires=[
+        'shortuuid'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
