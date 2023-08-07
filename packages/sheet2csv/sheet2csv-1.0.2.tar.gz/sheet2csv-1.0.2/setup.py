3abecffd0cb68096d4a6312bc25c99e228f8cbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:00 +0000

    Add package sheet2csv/1.0.2/sheet2csv-1.0.2.tar.gz

---
+++ packages/sheet2csv/sheet2csv-1.0.2.tar.gz/sheet2csv-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+
+import pkg_resources
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
+setuptools.setup(
+    name="sheet2csv",
+    version=os.getenv("PACKAGE_VERSION", "1.0.2"),
+    author="",
+    author_email="",
+    description="A package to export Google Sheet to CSV via API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sledilnik/sheet2csv/",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
