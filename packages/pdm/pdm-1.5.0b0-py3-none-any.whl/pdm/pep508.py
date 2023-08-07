69dcee47375ce944be854ee3121334056430821c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:46 +0000

    Add package pdm/1.5.0b0/pdm-1.5.0b0-py3-none-any.whl

---
+++ packages/pdm/pdm-1.5.0b0-py3-none-any.whl/pdm/pep508.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import json
+import os
+import platform
+import sys
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    version_info = sys._version_info
+
+
+def format_full_version(info: version_info) -> str:
+    version = f"{info.major}.{info.minor}.{info.micro}"
+    kind = info.releaselevel
+    if kind != "final":
+        version += kind[0] + str(info.serial)
+    return version
+
+
+def default_environment() -> Dict[str, str]:
+    if hasattr(sys, "implementation"):
+        iver = format_full_version(sys.implementation.version)
+        implementation_name = sys.implementation.name
+    else:
+        iver = "0"
+        implementation_name = ""
+
+    return {
+        "implementation_name": implementation_name,
+        "implementation_version": iver,
+        "os_name": os.name,
+        "platform_machine": platform.machine(),
+        "platform_release": platform.release(),
+        "platform_system": platform.system(),
+        "platform_version": platform.version(),
+        "python_full_version": platform.python_version(),
+        "platform_python_implementation": platform.python_implementation(),
+        "python_version": ".".join(platform.python_version_tuple()[:2]),
+        "sys_platform": sys.platform,
+    }
+
+
+if __name__ == "__main__":
+    print(json.dumps(default_environment(), indent=2))
