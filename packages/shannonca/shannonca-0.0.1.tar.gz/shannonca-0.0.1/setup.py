5f6a5601161cae29e97c426ddacc13e840aa06dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:50 +0000

    Add package shannonca/0.0.1/shannonca-0.0.1.tar.gz

---
+++ packages/shannonca/shannonca-0.0.1.tar.gz/shannonca-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'shannonca', 
+  packages = ['shannonca'],  
+  version = '0.0.1',   
+  license='MIT',       
+  description = 'Informative Dimensionality Reduction via Shannon Component Analysis',   
+  author = 'Benjamin DeMeo',                   
+  author_email = 'bdemeo@g.harvard.edu',      
+  url = 'https://github.com/bdemeo/shannonca',  
+  download_url =  'https://github.com/bendemeo/shannonca/archive/refs/tags/0.0.1.tar.gz',
+  keywords = ['Shannon', 'Information', 'Dimensionality','reduction', 'single-cell','RNA'], 
+  install_requires=[           
+          'sklearn',
+          'scipy',
+          'numpy',
+          'warnings',
+          'matplotlib',
+          'pandas',
+          'seaborn',
+          'scanpy'
+      ]
+    )
