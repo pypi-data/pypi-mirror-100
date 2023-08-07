690ef0fe85716405e58bbd3c7896e5b87cb5d2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:32 +0000

    Add package NightWindDataLib/0.0.2/NightWindDataLib-0.0.2.tar.gz

---
+++ packages/NightWindDataLib/NightWindDataLib-0.0.2.tar.gz/NightWindDataLib-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools as st
+
+st.setup(
+    name="NightWindDataLib",
+    author="Nova_NightWind0311",
+    version="0.0.2",
+    author_email="",
+    include_package_data=True,
+    description="Little tools for web and data science development",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=st.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
