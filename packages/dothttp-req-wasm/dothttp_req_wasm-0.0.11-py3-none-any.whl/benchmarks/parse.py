7cdc1bd3f272b141f9f5b046799f5c8ddfe6fdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:31 +0000

    Add package dothttp-req-wasm/0.0.11/dothttp_req_wasm-0.0.11-py3-none-any.whl

---
+++ packages/dothttp-req-wasm/dothttp_req_wasm-0.0.11-py3-none-any.whl/benchmarks/parse.py
@@ -0,0 +1,6 @@
+import pyperf
+
+runner = pyperf.Runner()
+runner.timeit(name="sort a sorted list",
+              stmt="sorted(s, key=f)",
+              setup="f = lambda x: x; s = list(range(1000))")
