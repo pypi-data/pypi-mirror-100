40ccb4c811030daad2ea754b9c3a2efb91b02219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:41 +0000

    Add package PyIsEven/0.4.1/PyIsEven-0.4.1.tar.gz

---
+++ packages/PyIsEven/PyIsEven-0.4.1.tar.gz/PyIsEven-0.4.1/is_even/is_even.py
@@ -0,0 +1,36 @@
+import requests
+import functools
+from retry import retry
+
+
+@functools.lru_cache(maxsize=None)
+@retry(ConnectionError, tries=3, delay=2)
+def is_even(number):
+    n = int(number)
+    r = requests.get(f"https://api.isevenapi.xyz/api/iseven/{n}/")
+
+    if r.status_code == requests.codes.ok:
+        return r.json()["iseven"]
+    else:
+        raise Exception(r.json()["error"])
+
+
+def is_odd(number):
+    return not is_even(number)
+
+
+if __name__ == '__main__':
+
+    for i in range(10):
+        if is_even(i):
+            print(f"{i} is even")
+        else:
+            print(f"{i} is odd")
+
+    print("second run with cache memo")
+
+    for i in range(10):
+        if is_even(i):
+            print(f"{i} even")
+        else:
+            print(f"{i} is odd")
