1f061412d88072b73ae64aa022f7cfdffc6c039c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:15 +0000

    Add package em-sqltask/0.0.1a50/em-sqltask-0.0.1a50.tar.gz

---
+++ packages/em-sqltask/em-sqltask-0.0.1a50.tar.gz/em-sqltask-0.0.1a50/sql_gen/test/test_sysarg_parser.py
@@ -0,0 +1,24 @@
+import sys
+import pytest
+from ccdev.command_line_app import SysArgParser, CommandFactory
+from unittest.mock import Mock
+
+
+def class_name(instance):
+    return instance.__class__.__name__
+
+
+def test_not_args_creates_sqltask_ouput_to_console():
+    sys.argv = [".", "print-sql"]
+    mocked_factory = Mock()
+    parser = SysArgParser(mocked_factory)
+    command = parser.parse()
+    mocked_factory.make_print_sql_to_console_command.assert_called_once()
+
+
+def test_dir_args_creates_sqltask_ouput_to_file():
+    sys.argv = [".", "create-sql", "modules/my_module"]
+    mocked_factory = Mock()
+    parser = SysArgParser(mocked_factory)
+    command = parser.parse()
+    mocked_factory.make_create_sqltask_command.assert_called_once()
