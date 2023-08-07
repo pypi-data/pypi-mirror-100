3a62783336482f1d89ad20318df91136de5aa3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:17 +0000

    Add package fetch-embed/0.1.1/fetch-embed-0.1.1.tar.gz

---
+++ packages/fetch-embed/fetch-embed-0.1.1.tar.gz/fetch-embed-0.1.1/fetch_embed/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.1"
+
+from .fetch_embed import fetch_embed
+
+__all__ = ("fetch_embed",)
---
+++ packages/fetch-embed/fetch-embed-0.1.1.tar.gz/fetch-embed-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fetch_embed']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['alive-progress>=1.6.2,<2.0.0',
+ 'httpx>=0.17.1,<0.18.0',
+ 'logzero>=1.7.0,<2.0.0',
+ 'numpy>=1.20.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fetch-embed',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'freemt',
+    'author_email': 'yucongo+fmt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
