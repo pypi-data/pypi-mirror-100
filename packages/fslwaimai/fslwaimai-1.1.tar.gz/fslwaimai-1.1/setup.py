c2f4635ec1a9da8dac194665d3a67ae132416148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:30 +0000

    Add package fslwaimai/1.1/fslwaimai-1.1.tar.gz

---
+++ packages/fslwaimai/fslwaimai-1.1.tar.gz/fslwaimai-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="fslwaimai",  # 包名称
+      version="1.1",  # 版本号
+      description="随即点外卖",  # 代码描述
+      long_description="就是随机点外卖",  # 代码详细描述
+      author="matthews_Feng",  # 作者
+      author_email="bugaosuni@qq.com",  # 邮箱
+      py_modules=["waimai",]) 
