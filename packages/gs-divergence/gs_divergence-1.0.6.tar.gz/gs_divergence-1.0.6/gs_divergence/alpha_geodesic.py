87297ba1e8b5ddc4828739988408d54ac5b6800b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:14 +0000

    Add package gs-divergence/1.0.6/gs_divergence-1.0.6.tar.gz

---
+++ packages/gs-divergence/gs_divergence-1.0.6.tar.gz/gs_divergence-1.0.6/gs_divergence/alpha_geodesic.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def alpha_geodesic(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    alpha: float,
+    lmd: float
+) -> torch.Tensor:
+    r"""
+    $\alpha$-geodesic between two probability distributions
+    """
+
+    a_ = a + 1e-12
+    b_ = b + 1e-12
+    if alpha == 1:
+        return torch.exp((1 - lmd) * torch.log(a_) + lmd * torch.log(b_))
+    elif alpha >= 1e+9:
+        return torch.min(a, b)
+    elif alpha <= -1e+9:
+        return torch.max(a, b)
+    else:
+        p = (1 - alpha) / 2
+        lhs = a_ ** p
+        rhs = b_ ** p
+        g = ((1 - lmd) * lhs + lmd * rhs) ** (1/p)
+
+        if alpha > 0 and (g == 0).sum() > 0:
+            return torch.min(a, b)
+
+        return g
