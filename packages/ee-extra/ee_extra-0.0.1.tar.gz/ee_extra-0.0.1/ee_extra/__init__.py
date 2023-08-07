76f0f4ff0b839856fd9de029b5258224bf3001a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:29 +0000

    Add package ee-extra/0.0.1/ee_extra-0.0.1.tar.gz

---
+++ packages/ee-extra/ee_extra-0.0.1.tar.gz/ee_extra-0.0.1/ee_extra/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Miscellaneous functions for working with the Earth Engine Python API."""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
