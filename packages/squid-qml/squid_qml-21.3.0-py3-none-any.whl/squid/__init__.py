501f5792ac17724a624017561c2e22e89634d95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:15 +0000

    Add package squid-qml/21.3.0/squid_qml-21.3.0-py3-none-any.whl

---
+++ packages/squid-qml/squid_qml-21.3.0-py3-none-any.whl/squid/__init__.py
@@ -0,0 +1,10 @@
+from . import models  # isort:skip
+from .models import ClassicalModel, MainModel, QuantumModel, SingleModel
+
+__all__ = [
+    "models",
+    "ClassicalModel",
+    "MainModel",
+    "QuantumModel",
+    "SingleModel",
+]
