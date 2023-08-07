bd317d7685f4e0e06641379caacef8111416cc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:01 +0000

    Add package technical/1.2.2/technical-1.2.2.tar.gz

---
+++ packages/technical/technical-1.2.2.tar.gz/technical-1.2.2/technical/indicators/volume_indicators.py
@@ -0,0 +1,29 @@
+"""
+Volume indicators
+"""
+
+from numpy.core.records import ndarray
+
+
+########################################
+#
+# Volume Indicator Functions
+#
+
+# AD                   Chaikin A/D Line
+
+# ADOSC                Chaikin A/D Oscillator
+# OBV                  On Balance Volume
+
+# Other Volume Indicator Functions
+def chaikin_money_flow(dataframe, period=21):
+    mfm = (
+        (dataframe['close'] - dataframe['low']) - (dataframe['high'] - dataframe['close'])
+        / (dataframe['high'] - dataframe['low'])
+    )
+    mfv = mfm * dataframe['volume']
+    cmf = mfv.rolling(period).sum() / dataframe['volume'].rolling(period).sum()
+    return cmf
+
+
+cmf = chaikin_money_flow
